623ca55557d9ec062c0117d40456ae95622cadc5
devJCM <jesus.carrillo@tactos.com.mx>
2018-07-10 18:37:43 -0500

    Cuentas-EC-7-8-13

--- custom/Extension/application/Ext/Language/es_LA.sugar_tipo_registro_list.php
+++ custom/Extension/application/Ext/Language/es_LA.sugar_tipo_registro_list.php
@@ -2,6 +2,7 @@
  // created: 2018-01-10 14:11:42
 
 $app_list_strings['tipo_registro_list']=array (
+  'Lead' => 'Lead',
   'Prospecto' => 'Prospecto',
   'Cliente' => 'Cliente',
   'Persona' => 'Persona',
