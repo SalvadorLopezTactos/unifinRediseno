3e1755afab851955ab3cc8c17293c4f5cf7191a7
Adrian Arauz <adrian.arauz@tactos.com.mx>
2018-07-11 14:01:22 -0500

    Visibilidad activa para registro con base al regimen fiscal.

--- custom/Extension/modules/Accounts/Ext/Vardefs/sugarfield_pais_nacimiento_c.php
+++ custom/Extension/modules/Accounts/Ext/Vardefs/sugarfield_pais_nacimiento_c.php
@@ -1,7 +1,7 @@
 <?php
- // created: 2018-02-16 16:59:02
-$dictionary['Account']['fields']['pais_nacimiento_c']['labelValue'] = 'Pais de nacimiento';
-$dictionary['Account']['fields']['pais_nacimiento_c']['dependency'] = 'or(not(equal($tipo_registro_c,"Prospecto")),equal($estatus_c,"Interesado"))';
-$dictionary['Account']['fields']['pais_nacimiento_c']['visibility_grid'] = '';
-$dictionary['Account']['fields']['pais_nacimiento_c']['full_text_search']['boost'] = 1;
+ // created: 2018-07-11 13:38:13
+$dictionary['Account']['fields']['pais_nacimiento_c']['labelValue']='Pais de nacimiento';
+$dictionary['Account']['fields']['pais_nacimiento_c']['dependency']='and(or(not(equal($tipo_registro_c,"Prospecto")),equal($estatus_c,"Interesado")),not(equal($tipodepersona_c,"Persona Moral")))';
+$dictionary['Account']['fields']['pais_nacimiento_c']['visibility_grid']='';
 
+ ?>
---
+++ custom/Extension/modules/Leads/Ext/Language/es_LA.lang.php
@@ -0,0 +1,4 @@
+<?php
+// WARNING: The contents of this file are auto-generated.
+$mod_strings['LBL_REGIMEN_FISCAL'] = 'LBL_REGIMEN_FISCAL';
+$mod_strings['LBL_RECORDVIEW_PANEL1'] = 'Nuevo Panel 1';
