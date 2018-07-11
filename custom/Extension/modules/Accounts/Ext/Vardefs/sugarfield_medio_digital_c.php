7bb304988952737b6cd63a3347f2912badb11c53
Axel Flores <axelflores@MacBook-Air-de-Axel.local>
2018-07-11 14:40:09 -0500

    Cambio para habilitación de Origen AF

--- custom/Extension/modules/Accounts/Ext/Vardefs/sugarfield_medio_digital_c.php
+++ custom/Extension/modules/Accounts/Ext/Vardefs/sugarfield_medio_digital_c.php
@@ -1,7 +1,60 @@
 <?php
- // created: 2018-07-05 19:02:24
-$dictionary['Account']['fields']['medio_digital_c']['labelValue']='Medio Digital';
-$dictionary['Account']['fields']['medio_digital_c']['dependency']='equal($tct_detalle_origen_ddw_c,"Digital")';
-$dictionary['Account']['fields']['medio_digital_c']['visibility_grid']='';
+ // created: 2018-07-11 14:03:43
+$dictionary['Account']['fields']['medio_digital_c']['labelValue']='Medio';
+$dictionary['Account']['fields']['medio_digital_c']['visibility_grid']=array (
+  'trigger' => 'tct_detalle_origen_ddw_c',
+  'values' => 
+  array (
+    '' => 
+    array (
+    ),
+    'Bases de datos' => 
+    array (
+    ),
+    'Centro de Prospeccion' => 
+    array (
+    ),
+    'Digital' => 
+    array (
+      0 => '',
+      1 => 'Facebook',
+      2 => 'Twitter',
+      3 => 'Instagram',
+      4 => 'Web',
+      5 => 'LinkedIn',
+      6 => 'Radio Online',
+      7 => 'Prensa Online',
+      8 => 'TV Online',
+      9 => 'Revistas Online',
+    ),
+    'Campanas' => 
+    array (
+    ),
+    'Acciones Estrategicas' => 
+    array (
+    ),
+    'Afiliaciones' => 
+    array (
+    ),
+    'Llamadas Inbound' => 
+    array (
+    ),
+    'Parques Industriales' => 
+    array (
+    ),
+    'Offline' => 
+    array (
+      0 => '',
+      1 => 'TV',
+      2 => 'Radio',
+      3 => 'Prensa',
+      4 => 'Revistas',
+      5 => 'Espectaculares',
+    ),
+    'Cartera Promotores' => 
+    array (
+    ),
+  ),
+);
 
  ?>
