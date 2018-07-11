08b83a520e84ab2bd357cd1c0b0955085d08c8b8
SalvadorLopezTactos <38955870+SalvadorLopezTactos@users.noreply.github.com>
2018-07-11 16:46:40 -0500

    Se habilita país de nacimiento para búsqueda global

--- custom/Extension/modules/Accounts/Ext/Vardefs/sugarfield_pais_nacimiento_c.php
+++ custom/Extension/modules/Accounts/Ext/Vardefs/sugarfield_pais_nacimiento_c.php
@@ -3,5 +3,6 @@
 $dictionary['Account']['fields']['pais_nacimiento_c']['labelValue']='Pais de nacimiento';
 $dictionary['Account']['fields']['pais_nacimiento_c']['dependency']='and(or(not(equal($tipo_registro_c,"Prospecto")),equal($estatus_c,"Interesado")),not(equal($tipodepersona_c,"Persona Moral")))';
 $dictionary['Account']['fields']['pais_nacimiento_c']['visibility_grid']='';
+$dictionary['Account']['fields']['pais_nacimiento_c']['full_text_search']['boost'] = 1;
 
  ?>
