0496f3f13ee274503530c8ee4326610f65aeae11
Victor Martinez <victor.martinez@tactos.com.mx>
2018-07-16 16:17:37 -0500

    Cambiar dependencia de Régimen Patrimonial por Lista Desplegable padre

--- custom/Extension/modules/Accounts/Ext/Vardefs/sugarfield_regimenpatrimonial_c.php
+++ custom/Extension/modules/Accounts/Ext/Vardefs/sugarfield_regimenpatrimonial_c.php
@@ -1,7 +1,25 @@
 <?php
- // created: 2018-07-16 11:44:40
+ // created: 2018-07-16 16:12:54
 $dictionary['Account']['fields']['regimenpatrimonial_c']['labelValue']='Régimen Patrimonial';
-$dictionary['Account']['fields']['regimenpatrimonial_c']['dependency']='not(equal($tipodepersona_c,"Persona Moral"))';
-$dictionary['Account']['fields']['regimenpatrimonial_c']['visibility_grid']='';
+$dictionary['Account']['fields']['regimenpatrimonial_c']['visibility_grid']=array (
+  'trigger' => 'estadocivil_c',
+  'values' => 
+  array (
+    '' => 
+    array (
+    ),
+    'Casado' => 
+    array (
+      0 => '1',
+      1 => '2',
+    ),
+    'Soltero' => 
+    array (
+    ),
+    'Union Libre' => 
+    array (
+    ),
+  ),
+);
 
  ?>
