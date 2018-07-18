26a2dc7b7f4f132a8f07543861d4a986815ed51e
Victor Martinez <victor.martinez@tactos.com.mx>
2018-07-18 11:27:10 -0500

    eliminacion de listas

--- custom/Extension/modules/Opportunities/Ext/Vardefs/sugarfield_si_c.php
+++
@@ -1,7 +0,0 @@
-<?php
- // created: 2018-07-18 10:31:54
-$dictionary['Opportunity']['fields']['si_c']['labelValue']='Si';
-$dictionary['Opportunity']['fields']['si_c']['enforced']='';
-$dictionary['Opportunity']['fields']['si_c']['dependency']='';
-
- ?>
