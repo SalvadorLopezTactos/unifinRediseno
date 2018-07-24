1a72872ac4a783a10e733f9e173fc9e6032c97a6
devJCM <jesus.carrillo@tactos.com.mx>
2018-07-24 13:04:52 -0500

    branchJCM|Llamadas-JC-11|Correccion de campo

--- custom/Extension/modules/Calls/Ext/Vardefs/sugarfield_tct_resultado_llamada_ddw_c.php
+++ custom/Extension/modules/Calls/Ext/Vardefs/sugarfield_tct_resultado_llamada_ddw_c.php
@@ -1,6 +1,6 @@
 <?php
- // created: 2018-07-23 13:39:08
-$dictionary['Call']['fields']['tct_resultado_llamada_ddw_c']['labelValue']='x';
+ // created: 2018-07-24 12:49:50
+$dictionary['Call']['fields']['tct_resultado_llamada_ddw_c']['labelValue']='Resultado de la llamada';
 $dictionary['Call']['fields']['tct_resultado_llamada_ddw_c']['dependency']='';
 $dictionary['Call']['fields']['tct_resultado_llamada_ddw_c']['visibility_grid']='';
 
