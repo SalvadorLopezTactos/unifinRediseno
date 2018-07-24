69ffaa0c9ba6159a9da04d9c47fb6ea846c0756a
devJCM <jesus.carrillo@tactos.com.mx>
2018-07-24 12:06:18 -0500

    branchJCM|Llamadas-JC-11|Concluido

---
+++ custom/Extension/modules/Calls/Ext/Vardefs/sugarfield_tct_fecha_cita_dat_c.php
@@ -0,0 +1,6 @@
+<?php
+ // created: 2018-07-24 11:19:41
+$dictionary['Call']['fields']['tct_fecha_cita_dat_c']['labelValue']='Fecha de la cita';
+$dictionary['Call']['fields']['tct_fecha_cita_dat_c']['enforced']='';
+
+ ?>
---
+++ custom/Extension/modules/Calls/Ext/Vardefs/sugarfield_tct_resultado_llamada_ddw_c.php
@@ -0,0 +1,7 @@
+<?php
+ // created: 2018-07-23 13:39:08
+$dictionary['Call']['fields']['tct_resultado_llamada_ddw_c']['labelValue']='x';
+$dictionary['Call']['fields']['tct_resultado_llamada_ddw_c']['dependency']='';
+$dictionary['Call']['fields']['tct_resultado_llamada_ddw_c']['visibility_grid']='';
+
+ ?>
