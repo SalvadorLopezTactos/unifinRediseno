9bce1e4110bcfddb54f4fd81adf4c730b864f157
Javier Garcia solar <javier.garcia@tactos.com.mx>
2018-07-27 13:23:57 -0500

    Se creo Dashlte para mostrar las Notificaciones de los usuarios por equipo, asi como crear nuevas notificaciones

---
+++ custom/clients/base/views/dashlet-Feedback/dashlet-Feedback.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @author F. Javier G. Solar
+ * Date: 23/07/2018
+ */
+
+
+$viewdefs['base']['view']['dashlet-Feedback'] = array(
+    'dashlets' => array(
+        array(
+            'label' => 'FEEDBACK',
+            'description' => 'FEEDBACK',
+            'config' => array(),
+            'preview' => array(),
+            'filter' => array(),
+        ),
+    ),
+);
