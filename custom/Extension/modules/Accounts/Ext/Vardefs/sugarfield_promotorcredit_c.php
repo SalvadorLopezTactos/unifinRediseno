11f323219b265836a8f5451e916cd60385c1da7f
Axel Flores <axel.flores@tactos.com.mx>
2018-07-27 14:24:03 -0500

    Actualiza dependencia de visibilidad para Primotores en Cuentas

--- custom/Extension/modules/Accounts/Ext/Vardefs/sugarfield_promotorcredit_c.php
+++ custom/Extension/modules/Accounts/Ext/Vardefs/sugarfield_promotorcredit_c.php
@@ -1,5 +1,6 @@
 <?php
- // created: 2018-02-16 16:59:02
-$dictionary['Account']['fields']['promotorcredit_c']['labelValue'] = 'Promotor Credit';
-$dictionary['Account']['fields']['promotorcredit_c']['dependency'] = 'or(equal($tipo_registro_c,"Cliente"),equal($tipo_registro_c,"Prospecto"))';
+ // created: 2018-07-27 12:44:24
+$dictionary['Account']['fields']['promotorcredit_c']['labelValue']='Promotor Credit';
+$dictionary['Account']['fields']['promotorcredit_c']['dependency']='or(equal($tipo_registro_c,"Lead"),equal($tipo_registro_c,"Cliente"),equal($tipo_registro_c,"Prospecto"))';
 
+ ?>
