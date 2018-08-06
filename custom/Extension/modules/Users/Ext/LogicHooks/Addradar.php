e155f9fa7d334976ddd10d5e19abea85e27b3eaf
devJCM <jesus.carrillo@tactos.com.mx>
2018-08-06 14:26:37 -0500

    branchJCM|Logic_hook concluido para integracion de Radar automaticamente en los nuevos usuarios

---
+++ custom/Extension/modules/Users/Ext/LogicHooks/Addradar.php
@@ -0,0 +1,9 @@
+<?php
+$hook_array['after_save'][] = Array(
+    3,
+    'Agrega dash de radar al nuevo usuario',
+    'custom/modules/Users/AgregaRadar.php',
+    'AgregaRadar',
+    'agregardashboards'
+);
+?>
