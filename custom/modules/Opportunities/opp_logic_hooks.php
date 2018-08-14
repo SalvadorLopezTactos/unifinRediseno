85f04ed3ed5e29043e54cacb8ce85bb457a256dc
Axel Flores <axel.flores@tactos.com.mx>
2018-08-14 12:22:18 -0500

    Ajustes Crédito Automotriz

--- custom/modules/Opportunities/opp_logic_hooks.php
+++ custom/modules/Opportunities/opp_logic_hooks.php
@@ -153,7 +153,7 @@
         global $db, $current_user;
 
         $GLOBALS['log']->fatal('-Update: '. $args['isUpdate'] . '--Etapa: ' . $bean->tct_etapa_ddw_c);
-        if($args['isUpdate']==1 && $bean->tct_etapa_ddw_c=='SI'){//@jesus
+        if(($args['isUpdate']==1 && $bean->tct_etapa_ddw_c=='SI') || empty($bean->id_process_c)){//@jesus
             if (($bean->id_process_c == 0 || $bean->id_process_c == null || empty($bean->id_process_c))/* && $bean->estatus_c == 'P' */ && $bean->tipo_operacion_c == '1') {
             //Hay operaciones vigentes?
             // ** JSR INICIO
