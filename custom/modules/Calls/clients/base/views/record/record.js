fa87717bd7103668b67b5d747a867657a63a0e27
Javier Garcia solar <javier.garcia@tactos.com.mx>
2018-08-14 14:11:12 -0500

    Se agrega validación de la Fecha inicial, no se debe ser menor a la fecha actual. En los modulos de Llamadas y Reuniones

--- custom/modules/Calls/clients/base/views/record/record.js
+++ custom/modules/Calls/clients/base/views/record/record.js
@@ -6,6 +6,7 @@
             this._super("initialize", [options]);
 
             this.on('render',this.disableparentsfields,this);
+            this.model.addValidationTask('VaildaFechaPermitida', _.bind(this.validaFechaInicial, this));
 
         },
 
@@ -19,4 +20,28 @@
         disableparentsfields:function () {
                 this.$('[data-name="parent_name"]').attr('style', 'pointer-events:none;')
         },
+
+    /* @F. Javier G. Solar
+     * Valida que la Fecha Inicial no sea menor que la actual
+     * 14/08/2018
+     */
+    validaFechaInicial: function (fields, errors, callback) {
+        var hoy =Date.parse(new Date());
+        var fechaInicio =Date.parse(this.model.get("date_start"));
+
+        if(fechaInicio<hoy)
+        {
+            app.alert.show("Fecha no valida", {
+                level: "error",
+                title: "La Fecha Inicio no puede ser menor a la actual ",
+                autoClose: false
+            });
+            app.error.errorName2Keys['date_start'] = 'La fecha no puede ser menor a la actual';
+            errors['date_start'] = errors['date_start'] || {};
+            errors['date_start'].custom_message1 = true;
+        }
+        callback(null, fields, errors);
+    },
+
+
 })
