3afd3c5ec5d9735b5447dc08e97d36d18b56f461
Axel Flores <axel.flores@tactos.com.mx>
2018-08-22 11:56:52 -0500

    Razón perdida: Actualización de valor precio

--- custom/Extension/modules/Opportunities/Ext/Vardefs/sugarfield_tct_razon_op_perdida_ddw_c.php
+++ custom/Extension/modules/Opportunities/Ext/Vardefs/sugarfield_tct_razon_op_perdida_ddw_c.php
@@ -1,5 +1,5 @@
 <?php
- // created: 2018-07-18 12:15:20
+ // created: 2018-08-22 11:55:13
 $dictionary['Opportunity']['fields']['tct_razon_op_perdida_ddw_c']['labelValue']='Razón';
 $dictionary['Opportunity']['fields']['tct_razon_op_perdida_ddw_c']['dependency']='equal($tct_oportunidad_perdida_chk_c,true)';
 $dictionary['Opportunity']['fields']['tct_razon_op_perdida_ddw_c']['visibility_grid']='';
