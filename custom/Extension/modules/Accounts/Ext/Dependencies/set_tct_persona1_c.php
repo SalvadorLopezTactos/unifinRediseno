babe395dcba94af56cc7c1f1de9aa13e98e8d973
Jesus Carrillo <jesus.carrillo@tactos.com.mx>
2018-09-05 10:58:34 -0500

    branchJCM|Incidencia OBS171|Resuelta v2

--- custom/Extension/modules/Accounts/Ext/Dependencies/set_tct_persona1_c.php
+++ custom/Extension/modules/Accounts/Ext/Dependencies/set_tct_persona1_c.php
@@ -3,7 +3,6 @@
 $dependencies['Accounts']['tct_persona1_c'] = array(
     'hooks' => array("all"),
     'trigger' => 'true',
-    'triggerFields' => array('tct_persona1_c','tipodepersona_c','subtipo_cuenta_c','account_type'),
     'onload' => true,
     'actions' => array(
         array(
@@ -17,7 +16,7 @@ $dependencies['Accounts']['tct_persona1_c'] = array(
             'name' => 'SetRequired',
             'params' => array(
                 'target' => 'tct_persona1_c',
-                'value' => 'or(equal($subtipo_cuenta_c,"Integracion de Expediente"),equal($ account_type,"Customer"))',
+                'value' => 'or(equal($subtipo_cuenta_c,"Integracion de Expediente"),equal($tipo_registro_c,"Cliente"))',
             ),
         ),
     ),
