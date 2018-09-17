defbff95f4a1821f36a6a057c4e8143fe5b171b1
root <victor.martinez@tactos.com.mx>
2018-09-17 10:08:06 -0500

    El campo macro-sector es requerido para PF y PFCAE para crear una pre-solicitud

--- custom/clients/base/api/ValidaCamposSolicitud.php
+++ custom/clients/base/api/ValidaCamposSolicitud.php
@@ -41,13 +41,13 @@ class ValidaCamposSolicitud extends SugarApi
         $option = $args['caso'];
 
         $req_pm = "origendelprospecto_c,tipodepersona_c," .
-            "nombre_comercial_c,sectoreconomico_c," .
+            "nombre_comercial_c,sectoreconomico_c,tct_macro_sector_ddw_c," .
             "subsectoreconomico_c,actividadeconomica_c," .
             "empleados_c,promotorleasing_c,promotorfactoraje_c,promotorcredit_c";
 
         $req_pf_y_pfae = "origendelprospecto_c,tipodepersona_c," .
             "primernombre_c,apellidopaterno_c,apellidomaterno_c," .
-            "sectoreconomico_c,subsectoreconomico_c,actividadeconomica_c," .
+            "sectoreconomico_c,subsectoreconomico_c,tct_macro_sector_ddw_c,actividadeconomica_c," .
             "empleados_c," .
             "promotorleasing_c,promotorfactoraje_c,promotorcredit_c";
 
