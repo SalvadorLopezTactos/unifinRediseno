b8caa75d2c9ba9419d73b50331d18c1c1298992e
Javier Garcia <javier.garcia@tactos.com.mx>
2018-09-24 12:34:30 -0500

    Filtro de cuentas por promotor firmado v2

---
+++ custom/modules/Accounts/clients/base/filters/promotorAccounts/promotorAccounts.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * @author F. Javier G. Solar
+ * User: javier.garcia@tactos.com.mx
+ * Date: 21/09/2018
+ */
+
+global $current_user;
+$variable = 'user_id_c';
+
+$viewdefs['Accounts']['base']['filter']['promotorAccounts'] = array(
+    'create' => true,
+    'filters' => array(
+        array(
+
+            'id' => 'promotorAccounts',
+            'name' => 'Cuentas por Promotor',
+            'filter_definition' => array(
+
+                   /* array(
+                        'promotorleasing_c' => array( // c57e811e-b81a-cde4-d6b4-5626c9961772
+                            '$in' => array($current_user->id),
+                        ),
+
+                    ), */
+                    array(
+                        'user_id_c' => array( // c57e811e-b81a-cde4-d6b4-5626c9961772
+                            '$in' => array($current_user->id),
+                        ),
+                    ),
+            ),
+            'editable' => true,
+        )
+    )
+);
