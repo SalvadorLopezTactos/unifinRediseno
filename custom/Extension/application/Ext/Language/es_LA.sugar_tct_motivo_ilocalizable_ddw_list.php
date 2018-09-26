162fdbdc26b34047cbfa52fb6b1aadc689006bc5
Adrian Arauz <adrian.arauz@tactos.com.mx>
2018-09-26 09:33:51 -0500

    OBS 238

--- custom/Extension/application/Ext/Language/es_LA.sugar_tct_motivo_ilocalizable_ddw_list.php
+++ custom/Extension/application/Ext/Language/es_LA.sugar_tct_motivo_ilocalizable_ddw_list.php
@@ -1,7 +1,8 @@
 <?php
- // created: 2018-07-23 12:16:41
+ // created: 2018-09-25 17:02:46
 
 $app_list_strings['tct_motivo_ilocalizable_ddw_list']=array (
+  '' => '',
   'Datos_Incorrectos' => 'Datos Incorrectos',
   'La_Empresa_ya_no_existe' => 'La Empresa ya no existe',
 );
