1bd6350c9eafac0b623561790a64f03510b73abd
root <victor.martinez@tactos.com.mx>
2018-09-28 17:01:15 -0500

    modificación para que los valores del sea visibles si es realizada o no realizada

--- custom/Extension/modules/Meetings/Ext/Vardefs/sugarfield_objetivo_c.php
+++ custom/Extension/modules/Meetings/Ext/Vardefs/sugarfield_objetivo_c.php
@@ -1,5 +1,5 @@
 <?php
- // created: 2018-09-28 22:45:02
+ // created: 2018-09-28 23:55:02
 $dictionary['Meeting']['fields']['objetivo_c']['labelValue']='Objetivo';
 $dictionary['Meeting']['fields']['objetivo_c']['dependency']='';
 $dictionary['Meeting']['fields']['objetivo_c']['visibility_grid']=array (
@@ -23,9 +23,33 @@ $dictionary['Meeting']['fields']['objetivo_c']['visibility_grid']=array (
     ),
     'Held' => 
     array (
+      0 => '',
+      1 => '1',
+      2 => '2',
+      3 => '5',
+      4 => '6',
+      5 => '7',
+      6 => '8',
+      7 => '10',
+      8 => '4',
+      9 => '9',
+      10 => '11',
+      11 => '12',
     ),
     'Not Held' => 
     array (
+      0 => '',
+      1 => '1',
+      2 => '2',
+      3 => '5',
+      4 => '6',
+      5 => '7',
+      6 => '8',
+      7 => '10',
+      8 => '4',
+      9 => '9',
+      10 => '11',
+      11 => '12',
     ),
   ),
 );
