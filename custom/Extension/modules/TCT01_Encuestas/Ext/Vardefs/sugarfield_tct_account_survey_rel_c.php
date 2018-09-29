0df26bda106032e1416be34f3e34a4957f6981c5
devJCM <jesus.carrillo@tactos.com.mx>
2018-09-29 02:25:04 -0500

    branchJCMEncuestas v6.1

---
+++ custom/Extension/modules/TCT01_Encuestas/Ext/Vardefs/sugarfield_tct_account_survey_rel_c.php
@@ -0,0 +1,6 @@
+<?php
+ // created: 2018-09-28 21:54:55
+$dictionary['TCT01_Encuestas']['fields']['tct_account_survey_rel_c']['labelValue']='Cuenta';
+$dictionary['TCT01_Encuestas']['fields']['tct_account_survey_rel_c']['dependency']='';
+
+ ?>
