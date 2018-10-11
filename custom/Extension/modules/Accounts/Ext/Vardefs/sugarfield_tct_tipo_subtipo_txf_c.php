27b15c3e1d424cbcd3ca41329faece25d0b6263c
root <victor.martinez@tactos.com.mx>
2018-10-11 18:24:16 -0500

    Se quito el espacio de persona y proveedor

--- custom/Extension/modules/Accounts/Ext/Vardefs/sugarfield_tct_tipo_subtipo_txf_c.php
+++ custom/Extension/modules/Accounts/Ext/Vardefs/sugarfield_tct_tipo_subtipo_txf_c.php
@@ -1,5 +1,5 @@
 <?php
- // created: 2018-09-12 10:02:11
+ // created: 2018-10-12 01:19:21
 $dictionary['Account']['fields']['tct_tipo_subtipo_txf_c']['duplicate_merge_dom_value']=0;
 $dictionary['Account']['fields']['tct_tipo_subtipo_txf_c']['labelValue']='Tipo y Subtipo de la Cuenta';
 $dictionary['Account']['fields']['tct_tipo_subtipo_txf_c']['full_text_search']=array (
@@ -8,7 +8,15 @@ $dictionary['Account']['fields']['tct_tipo_subtipo_txf_c']['full_text_search']=a
   'searchable' => false,
 );
 $dictionary['Account']['fields']['tct_tipo_subtipo_txf_c']['calculated']='1';
-$dictionary['Account']['fields']['tct_tipo_subtipo_txf_c']['formula']='concat(getDropdownValue("tipo_registro_list",$tipo_registro_c)," ",getDropdownValue("subtipo_cuenta_list",$subtipo_cuenta_c))';
+$dictionary['Account']['fields']['tct_tipo_subtipo_txf_c']['formula']='ifElse(
+	equal($tipo_registro_c,"Persona"),
+	"PERSONA",
+	ifElse(
+		equal($tipo_registro_c,"Proveedor"),
+		"PROVEEDOR",
+		concat(getDropdownValue("tipo_registro_list",$tipo_registro_c)," ",getDropdownValue("subtipo_cuenta_list",$subtipo_cuenta_c))
+	)
+)';
 $dictionary['Account']['fields']['tct_tipo_subtipo_txf_c']['enforced']='1';
 $dictionary['Account']['fields']['tct_tipo_subtipo_txf_c']['dependency']='';
 
