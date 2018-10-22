722942048a7183939ba08ba165d5339c354dcd86
Salvador Lopez <salvador.lopez@enterconecta.com>
2018-10-22 12:03:27 -0500

    Dependencias Minuta (Studio)

--- custom/Extension/modules/minut_Minutas/Ext/Vardefs/sugarfield_tct_motivo_c.php
+++ custom/Extension/modules/minut_Minutas/Ext/Vardefs/sugarfield_tct_motivo_c.php
@@ -1,5 +1,6 @@
 <?php
- // created: 2018-10-22 11:49:35
+ // created: 2018-10-22 11:52:42
 $dictionary['minut_Minutas']['fields']['tct_motivo_c']['options']='tct_motivo_minuta_list';
+$dictionary['minut_Minutas']['fields']['tct_motivo_c']['dependency']='equal($tct_cliente_no_interesado_chk,1)';
 
  ?>
