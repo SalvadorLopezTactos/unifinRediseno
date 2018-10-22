7f8466b59b28b56c830cbf2ebb00b75d59378752
root <victor.martinez@tactos.com.mx>
2018-10-22 13:29:25 -0500

    Creación de minuta (fecha y usuario), creacion de campos Check-Out

---
+++ custom/Extension/modules/Meetings/Ext/Vardefs/sugarfield_check_out_latitude_c.php
@@ -0,0 +1,7 @@
+<?php
+ // created: 2018-10-22 17:38:28
+$dictionary['Meeting']['fields']['check_out_latitude_c']['labelValue']='Latitude Check-Out';
+$dictionary['Meeting']['fields']['check_out_latitude_c']['enforced']='';
+$dictionary['Meeting']['fields']['check_out_latitude_c']['dependency']='';
+
+ ?>
