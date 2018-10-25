749e73359558de76e8f8e6d1f212f34a8a202d45
devJCM <jesus.carrillo@tactos.com.mx>
2018-10-25 17:58:53 -0500

    branchJCM|Correcciones a CF minuta_compromisos|Realaizadas

--- custom/clients/base/fields/minuta_compromisos/minuta_compromisos.js
+++ custom/clients/base/fields/minuta_compromisos/minuta_compromisos.js
@@ -26,9 +26,18 @@
     loadData: function (options) {
       //Recupera data existente
       //myData = $.parseJSON( '{"myData":{"records":[{"compromiso":"1","id_resp":"82ec8bcc-cdb3-b56e-a472-573a06966424","responsable":"Carmen Velasco","fecha":"2018-10-24","deleted":0},{"compromiso":"2","id_resp":"bafb1018-7a44-11e8-bb52-00155d967407","responsable":"Adrian","fecha":"2018-10-30","deleted":0}]}}');
-      this.model.set('minuta_compromisos',[{"compromiso":"1","id_resp":"82ec8bcc-cdb3-b56e-a472-573a06966424","responsable":"Carmen Velasco","fecha":"2018-10-24","deleted":0},{"compromiso":"2","id_resp":"bafb1018-7a44-11e8-bb52-00155d967407","responsable":"Adrian","fecha":"2018-10-30","deleted":0}]);
+      this.model.set('minuta_compromisos',[{"compromiso":"1","id_resp":"716822f6-f2f9-3053-e052-5626c99305ac","responsable":"Carmen Velasco","fecha":"2018-10-24"},{"compromiso":"2","id_resp":"bafb1018-7a44-11e8-bb52-00155d967407","responsable":"Adrian","fecha":"2018-10-30"}]);
       myData = $.parseJSON( '{"myData":{"records":'+JSON.stringify(this.model.get('minuta_compromisos'))+'}}');
+
+      arr_responsables={"arr_responsables": {"responsables":[{"id_resp2":"716822f6-f2f9-3053-e052-5626c99305ac",
+                                            "responsable2":"Carmen Velasco"},
+                                                            {"id_resp2":"bafb1018-7a44-11e8-bb52-00155d967407",
+                                                  "responsable2":"Adrian"}
+                                                            ]
+                                            }
+                        };
       _.extend(this, myData);
+      _.extend(this, arr_responsables);
       this.render();
     },
 
@@ -43,12 +52,12 @@
 
 
       var item = {
-        "compromiso":valor1,"id_resp":valor2, "responsable":valor3, "fecha":valor4, "id":"", "deleted":0
+        "compromiso":valor1,"id_resp":valor2, "responsable":valor3, "fecha":valor4
       };
 
       this.myData.records.push(item);
       this.model.set('minuta_compromisos',this.myData.records);
-      this.model.save();
+      //this.model.save();
       this.render();
     },
 
@@ -57,10 +66,34 @@
 
         $('.removecompromiso').click(function(evt) {
             var row = $(this).closest(".compromisos");    // Find the row
-            self.myData.records[row.index()].deleted = 1;
+            self.myData.records.splice(row.index(),1);
+            self.render();
+        });
+
+        $('.existingresponsable').change(function(evt) {
+            var row = $(this).closest(".compromisos");    // Find the row
+            var val= row.find(".existingresponsable").context.value;
+            var text = row.find(".existingresponsable option[value="+val+"]").text();
+            self.myData.records[row.index()].responsable = text;
+            self.myData.records[row.index()].id_resp = val;
+            self.render();
+        });
+
+        $('.existingcompromiso').change(function(evt) {
+            var row = $(this).closest(".compromisos");    // Find the row
+            var val= row.find(".existingcompromiso").context.value;
+            self.myData.records[row.index()].compromiso = val;
+            self.render();
+        });
+
+        $('.existingdate').change(function(evt) {
+            var row = $(this).closest(".compromisos");    // Find the row
+            var val= row.find(".existingdate").context.value;
+            self.myData.records[row.index()].fecha = val;
             self.render();
         });
     },
+
 
     /**
      * When data changes, re-render the field only if it is not on edit (see MAR-1617).
