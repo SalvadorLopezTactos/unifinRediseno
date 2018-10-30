83b20e1278ab4d9ae8b252829f5416913b75dfc6
Salvador Lopez <salvador.lopez@tactos.com.mx>
2018-10-30 10:47:45 -0600

    Complemento de validación para objetivos específicos requeridos

--- custom/clients/base/fields/reunion_objetivos/reunion_objetivos.js
+++ custom/clients/base/fields/reunion_objetivos/reunion_objetivos.js
@@ -133,7 +133,7 @@
     },
 
     almacenaobjetivos:function(fields, errors, callback) {
-        this.model.set('reunion_objetivos', self.myobject);
+        this.model.set('reunion_objetivos', this.myobject);
         if (this.model.get('reunion_objetivos') == "" || this.model.get('reunion_objetivos') == null || this.model.get('reunion_objetivos').records.length==0) {
             errors['reunion_objetivos'] = "Al menos un objetivo es requerido.";
             errors['reunion_objetivos'].required = true;
