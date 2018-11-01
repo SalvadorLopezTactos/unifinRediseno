257331ee6dd876111df8ebaf114857e8cd9dd59a
Adrian Arauz <adrian.arauz@tactos.com.mx>
2018-11-01 14:26:54 -0600

    OBS314 9 sin gestor en credito automotriz

--- custom/modules/Accounts/clients/base/views/create/create.js
+++ custom/modules/Accounts/clients/base/views/create/create.js
@@ -491,16 +491,16 @@
                     this.model.set('promotorcredit_c', modelo.get('name'));
                     this.model.set('user_id2_c', modelo.get('id'));
                 } else {
-                    this.model.set('promotorcredit_c', 'Adriana Gayosso Cruz');
-                    this.model.set('user_id2_c', '7a83c151-6fc3-dc2b-b3a0-562a60aa3b74');
+                    this.model.set('promotorcredit_c', '9 - Sin Gestor');
+                    this.model.set('user_id2_c', '569246c7-da62-4664-ef2a-5628f649537e');
                 }
                 if (contains.call(modelo.get('productos_c'), "1") == false && contains.call(modelo.get('productos_c'), "3") == false && contains.call(modelo.get('productos_c'), "4") == false) {
                     this.model.set('promotorleasing_c', '9 - Sin Gestor');
                     this.model.set('user_id_c', '569246c7-da62-4664-ef2a-5628f649537e');
                     this.model.set('promotorfactoraje_c', 'Maria de Lourdes Campos Toca');
                     this.model.set('user_id1_c', 'a04540fc-e608-56a7-ad47-562a6078519d');
-                    this.model.set('promotorcredit_c', 'Adriana Gayosso Cruz');
-                    this.model.set('user_id2_c', '7a83c151-6fc3-dc2b-b3a0-562a60aa3b74');
+                    this.model.set('promotorcredit_c', '9 - Sin Gestor');
+                    this.model.set('user_id2_c', '569246c7-da62-4664-ef2a-5628f649537e');
                 }
             }, this)
         });
