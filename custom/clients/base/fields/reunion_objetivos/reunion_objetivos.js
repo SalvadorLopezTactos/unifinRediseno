303d937d2445899ca0ea1bdfee53094c218bc0b5
Axel Flores <axel.flores@tactos.com.mx>
2018-11-06 13:33:18 -0600

    Actualización Objetivos: OBS45, OBS50

--- custom/clients/base/fields/reunion_objetivos/reunion_objetivos.js
+++ custom/clients/base/fields/reunion_objetivos/reunion_objetivos.js
@@ -21,8 +21,15 @@
 
         //Carga datos
         this.model.on('sync', this.loadData, this);
+        //Data real
         this.myobject={};
         this.myobject.records=[];
+        //Data eliminada
+        this.myDeletedObj={};
+        this.myDeletedObj.records=[];
+        //Data original
+        this.myOriginal={};
+        this.myOriginal.records=[];
     },
 
 
@@ -34,7 +41,9 @@
             app.api.call('GET', app.api.buildURL('Meetings/' + this.model.get("id") + '/link/meetings_minut_objetivos_1'), null, {
                 success: function (data) {
                     selfvalue.myobject = data;
+                    selfvalue.myOriginal = data;
                     _.extend(this, selfvalue.myobject);
+                    _.extend(this, selfvalue.myOriginal);
                     selfvalue.render();
                 },
                 error: function (e) {
@@ -61,6 +70,12 @@
         $('.deleteRecord').click(function(evt) {
            var row = $(this).closest("tr");    // Find the row
            //self.myData.records[row.index()].deleted = 1;
+           if (self.myobject.records[row.index()].id) {
+             //Agrega a myDeletedObj
+             self.myobject.records[row.index()].deleted = 1;
+             self.myDeletedObj.records.push(self.myobject.records[row.index()]);
+           }
+
            self.myobject.records.splice(row.index(),1);
            self.render();
        });
@@ -134,11 +149,24 @@
     },
 
     almacenaobjetivos:function(fields, errors, callback) {
-        this.model.set('reunion_objetivos', this.myobject);
-        if (this.model.get('reunion_objetivos') == "" || this.model.get('reunion_objetivos') == null || this.model.get('reunion_objetivos').records.length==0) {
-            errors['reunion_objetivos'] = "Al menos un objetivo es requerido.";
-            errors['reunion_objetivos'].required = true;
-        }
+        myObjetivos={};
+        myObjetivos.records=[];
+
+        //Itera myobject
+        Object.keys(self.myobject.records).forEach(function(key) {
+            myObjetivos.records.push(self.myobject.records[key]);
+        });
+
+        //Itera myDeletedObj
+        Object.keys(self.myDeletedObj.records).forEach(function(key) {
+            myObjetivos.records.push(self.myDeletedObj.records[key]);
+        });
+
+        this.model.set('reunion_objetivos', myObjetivos);
+        // if (this.model.get('reunion_objetivos') == "" || this.model.get('reunion_objetivos') == null || this.model.get('reunion_objetivos').records.length==0) {
+        //     errors['reunion_objetivos'] = "Al menos un objetivo es requerido.";
+        //     errors['reunion_objetivos'].required = true;
+        // }
         callback(null, fields, errors);
     },
 
@@ -158,5 +186,6 @@
         });
         callback(null, fields, errors);
     },
+
 
 })
