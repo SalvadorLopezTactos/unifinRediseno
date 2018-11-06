b9e55ad53d8d516af1e825cd766ed56906c875ff
Salvador Lopez <salvador.lopez@tactos.com.mx>
2018-11-06 17:50:59 -0600

    Se omite búsqueda de duplicados para el campo name en minutas

---
+++ custom/Extension/modules/minut_Minutas/Ext/Vardefs/sugarfield_name.php
@@ -0,0 +1,17 @@
+<?php
+ // created: 2018-11-06 17:48:58
+$dictionary['minut_Minutas']['fields']['name']['len']='255';
+$dictionary['minut_Minutas']['fields']['name']['audited']=false;
+$dictionary['minut_Minutas']['fields']['name']['massupdate']=false;
+$dictionary['minut_Minutas']['fields']['name']['merge_filter']='disabled';
+$dictionary['minut_Minutas']['fields']['name']['unified_search']=false;
+$dictionary['minut_Minutas']['fields']['name']['full_text_search']=array (
+  'enabled' => true,
+  'boost' => '1.55',
+  'searchable' => true,
+);
+$dictionary['minut_Minutas']['fields']['name']['calculated']=false;
+$dictionary['minut_Minutas']['fields']['name']['duplicate_merge']='disabled';
+$dictionary['minut_Minutas']['fields']['name']['duplicate_merge_dom_value']='0';
+
+ ?>
