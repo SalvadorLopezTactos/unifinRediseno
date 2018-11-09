5bf7ef43923ae0581a60e041c6b780d5d441711e
Axel Flores <axel.flores@tactos.com.mx>
2018-11-08 19:00:23 -0600

    Actualización WARP valores y campos

--- custom/Extension/modules/tct02_Resumen/Ext/Vardefs/sugarfield_tct_datos_clave_txa_c.php
+++ custom/Extension/modules/tct02_Resumen/Ext/Vardefs/sugarfield_tct_datos_clave_txa_c.php
@@ -1,6 +1,6 @@
 <?php
- // created: 2018-09-20 16:58:01
-$dictionary['tct02_Resumen']['fields']['tct_datos_clave_txa_c']['labelValue']='Datos Clave';
+ // created: 2018-11-08 14:52:02
+$dictionary['tct02_Resumen']['fields']['tct_datos_clave_txa_c']['labelValue']='LBL_TCT_DATOS_CLAVE_TXA';
 $dictionary['tct02_Resumen']['fields']['tct_datos_clave_txa_c']['full_text_search']=array (
   'enabled' => '0',
   'boost' => '1',
