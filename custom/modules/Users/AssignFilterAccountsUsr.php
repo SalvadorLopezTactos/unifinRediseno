edf20f6f12656ef260574c879cc421c979e6f895
Javier Solar <javier.garcia@tactos.com.mx>
2019-01-08 13:20:47 -0600

    Ajuste de Filtro de usuario para la visualización de registros del modulo de Cuentas

--- custom/modules/Users/AssignFilterAccountsUsr.php
+++ custom/modules/Users/AssignFilterAccountsUsr.php
@@ -1,23 +1,42 @@
 <?php
 /**
- * Created by PhpStorm.
+ * Created by Javier G. Solar, Axel Flores
  * User: tactos
  * Date: 19/12/18
  * Time: 11:51 AM
+ */
+
+/**
+ * A) Funcionalidad: Genera a nivel Base de datos el Filtro de registros del Modulo de Cuentas
+ *    Afectando tambien a quienes Reporta el usuario modificado.
+ *    Existen dos listas principales.
+ *     1.- filterAccounts_PuestoUsr_list: almacena los Puestos excluyentes de filtro, se toma el id de la lista original
+ *         de puesto de usuario.
+ *     2.- filterAccounts_RolesUsr_list: contiene el nombre de los Roles excluyentes de filtro, estos nombres deben ser igual
+ *         que el listado de roles.
+ *
+ * B) Recupera al usuario a quien reportaba anteriormente y modifica sus filtros para que ya no puedan compartir registros
+ *    afectando a los usuarios a quienes le reportaban.
+ *    Despues toma al usuario al que le reportara actualmente y genera sus nuevos filtros afectando tambien a quienes
+ *    le reportan.
+ *
+ * C) La varieble $GLOBALS['fetched_reports_to_id'] almacena el bean antes de ser modificado, este es ejecutado en el
+ *    before_save
  */
 
 global $app_list_strings;
 
 $GLOBALS['puestosList'] = $app_list_strings['filterAccounts_PuestoUsr_list'];
 $GLOBALS['rolesList'] = $app_list_strings['filterAccounts_RolesUsr_list'];
+$GLOBALS['fetched_reports_to_id'] = "";
 
 
 class AssignFilterAccountsUsr
 {
 
-
     function AssignFilterAccounts_ByUsr($bean = null, $event = null, $args = null)
     {
+        $GLOBALS['log']->fatal("Estado: Actualizando Filtros");
         $puesto = $bean->puestousuario_c;
         $puestosListUsr = $GLOBALS['puestosList'];
         $AplicaFiltro = "";
@@ -67,21 +86,14 @@ null as description,
 u.default_team as team_id,
 u.team_set_id as team_set_id,
 null assigned_user_id,
-concat('[{"$campo":{"\$in":[',(select 
-CASE
-  WHEN (select GROUP_CONCAT(CONCAT(b.id)) from users b where b.reports_to_id = u.id) is null THEN concat('"',u.id,'"')
-  WHEN (select GROUP_CONCAT(CONCAT(b.id)) from users b where b.reports_to_id = u.id) is not null THEN concat('"',u.id,'",',(select GROUP_CONCAT(CONCAT('"',b.id,'"')) from users b where b.reports_to_id = u.id))
-END),']}}]') as filter_definition,
-concat('[{"$campo":{"\$in":[',(select 
-CASE
-  WHEN (select GROUP_CONCAT(CONCAT(b.id)) from users b where b.reports_to_id = u.id) is null THEN concat('"',u.id,'"')
-  WHEN (select GROUP_CONCAT(CONCAT(b.id)) from users b where b.reports_to_id = u.id) is not null THEN concat('"',u.id,'",',(select GROUP_CONCAT(CONCAT('"',b.id,'"')) from users b where b.reports_to_id = u.id))
-END),']}}]') as filter_template,
+concat('[{"$campo":{"\$in":["$idUsuario"]}}]') as filter_definition,
+concat('[{"$campo":{"\$in":["$idUsuario"]}}]') as filter_template,
 'Accounts' as module_name,
 null as acl_team_set_id
 from users u, users_cstm uc
 where u.id= uc.id_c and u.id='$idUsuario'
 and u.status = 'Active'
+
 
 SQL;
                         $GLOBALS['db']->query($queryInsertP);
@@ -156,19 +168,32 @@ SQL;
                     default:
                         $GLOBALS['log']->fatal("No se aplicara Filtro");
                 }
-
             }
 
+            if ($GLOBALS['fetched_reports_to_id']['reports_to_id'] != $bean->reports_to_id) {
+                if (!empty($GLOBALS['fetched_reports_to_id']['reports_to_id'])) {
 
-            $GLOBALS['log']->fatal("Puestos listas " . print_r($GLOBALS['puestosList'], true));
-            $GLOBALS['log']->fatal("Puesto Usuario " . $puesto);
-            $GLOBALS['log']->fatal("Nombre del filtro" . $AplicaFiltro);
+                    if ($GLOBALS['fetched_reports_to_id']['reports_to_id'] != 'NULL') {
+                        $sqlUpdate = "update users set reports_to_id='{$bean->reports_to_id}' where id='{$bean->id}'";
+                        $GLOBALS['db']->query($sqlUpdate);
+
+                        $newUser1 = BeanFactory::retrieveBean('Users', $GLOBALS['fetched_reports_to_id']['reports_to_id']);
+                        $newUser1->save();
+                    }
+                }
+            }
+
+            if (!empty($bean->reports_to_id)) {
+
+                if ($bean->reports_to_id != 'NULL') {
+                    $newUser = BeanFactory::retrieveBean('Users', $bean->reports_to_id);
+                    $newUser->save();
+                }
+            }
 
         } else {
-            $GLOBALS['log']->fatal("Estado: Fallo al Eliminar Filtro");
+            // $GLOBALS['log']->fatal("Estado: Fallo al Eliminar Filtro");
         }
-
-
     }
 
     function validateExcludingRoles($usr)
@@ -197,7 +222,7 @@ SQL;
         $result = array_intersect($rolesExcluyentes, $response);
         if (empty($result)) {
             $banderaRol = true;
-            $GLOBALS['log']->fatal("  nO hay Resultados de interseccion  VALIDA POR PUESTO" . print_r($result, true));
+           // $GLOBALS['log']->fatal("  nO hay Resultados de interseccion  VALIDA POR PUESTO" . print_r($result, true));
         }
 
 
@@ -227,5 +252,13 @@ SQL;
         // return true;
     }
 
+    function UpdateReportToUsr($bean = null, $event = null, $args = null)
+    {
 
+        $GLOBALS['fetched_reports_to_id'] = "";
+        if (!empty($bean->id)) {
+            $GLOBALS['fetched_reports_to_id'] = $bean->fetched_row;
+
+        }
+    }
 }
