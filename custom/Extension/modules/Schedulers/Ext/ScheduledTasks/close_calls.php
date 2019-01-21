061f5f835d0fdfa563a8c6f41d742af3999b1494
Axel Flores <axelflores@MacBook-Air-de-Axel.local>
2019-01-21 12:21:47 -0600

    Modifica proceso de actualización para eliminar llamadas Issabel

--- custom/Extension/modules/Schedulers/Ext/ScheduledTasks/close_calls.php
+++ custom/Extension/modules/Schedulers/Ext/ScheduledTasks/close_calls.php
@@ -7,17 +7,17 @@
     	// Busca las llamadas vencidas en status "planificada" y les cambia el estado a "no realizada"
         $GLOBALS['log']->fatal('>>>>>>COMIENZA JOB CLOSE_CALLS_ISSABEL:');//------------------------------------
 
-        $query="select calls.id from calls,calls_cstm where calls.id=calls_cstm.id_c and calls.status='Planned' and date_entered < curdate() and calls_cstm.tct_call_issabel_c=1;";
+        $query="select calls.id from calls,calls_cstm where calls.id=calls_cstm.id_c and calls.status='Planned' and date_entered < curdate() and calls_cstm.tct_call_issabel_c=1 and deleted=0;";
         $result = $GLOBALS['db']->query($query);
         $contador=0;
 
         while($row = $GLOBALS['db']->fetchByAssoc($result) )
         {
             $id = $row['id'];
-            $bean_call = BeanFactory::retrieveBean('Calls', $id);
-            //$bean_call->status='Not Held';
-            $bean_call->deleted=1;
-            $bean_call->save();
+            $queryUpdate="update calls
+              set deleted = 1
+              where id='{$id}';";
+            $resultUpdate = $GLOBALS['db']->query($queryUpdate);
             $contador++;
         }
         $GLOBALS['log']->fatal('>>>>>>TERMINA JOB CLOSE_CALLS_ISSABEL:'+$contador);//------------------------------------
