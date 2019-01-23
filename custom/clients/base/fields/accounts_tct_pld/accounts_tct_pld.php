ba176187634c4e97724d0061cd6311d0a14829f9
Javier Solar <javier.garcia@tactos.com.mx>
2019-01-23 11:58:45 -0600

    Vistas de los productos solo en edición

---
+++ custom/clients/base/fields/accounts_tct_pld/accounts_tct_pld.php
@@ -0,0 +1,18 @@
+<?php
+
+
+$viewdefs['base']['fields']['proveedor-Recursos-cstm'] = array(
+    'panels' => array(
+        array(
+            'fields' => array(
+                array(
+                    'name' => 'users_accounts_1_name',
+                    'label' => 'Proveedor de Recursos',
+                    'type' => 'relate',
+                    'view' => 'edit',
+                ),
+            ),
+        ),
+    ),
+
+);
