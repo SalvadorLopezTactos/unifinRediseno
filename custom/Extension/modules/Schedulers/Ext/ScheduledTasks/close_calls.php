17b8ae4c4a525b1ed902a97c9e62ef2f1c89e8dd
devJCM <jesus.carrillo@tactos.com.mx>
2019-01-28 18:35:25 -0600

    Modificacion de condiciones de fechas, ajustandolo a la hora de la db (UTC)

--- custom/Extension/modules/Schedulers/Ext/ScheduledTasks/close_calls.php
+++ custom/Extension/modules/Schedulers/Ext/ScheduledTasks/close_calls.php
@@ -7,7 +7,7 @@
     	// Busca las llamadas vencidas en status "planificada" y les cambia el estado a "no realizada"
         $GLOBALS['log']->fatal('>>>>>>COMIENZA JOB CLOSE_CALLS_ISSABEL:');//------------------------------------
 
-        $query="select calls.id from calls,calls_cstm where calls.id=calls_cstm.id_c and calls.status='Planned' and date_entered < curdate() and calls_cstm.tct_call_issabel_c=1 and deleted=0;";
+        $query="select calls.id from calls,calls_cstm where calls.id=calls_cstm.id_c and calls.status='Planned' and date_entered < UTC_TIMESTAMP() and calls_cstm.tct_call_issabel_c=1 and deleted=0;";
         $result = $GLOBALS['db']->query($query);
         $contador=0;
 
