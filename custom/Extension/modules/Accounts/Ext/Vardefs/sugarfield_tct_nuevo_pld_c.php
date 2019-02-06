48e7c3e3fd39cb9ee20a9540392fbe885e32e09d
Adrian Arauz <adrian.arauz@tactos.com.mx>
2019-02-06 10:22:28 -0600

    creacion campo nuevo_pld_c

---
+++ custom/Extension/modules/Accounts/Ext/Vardefs/sugarfield_tct_nuevo_pld_c.php
@@ -0,0 +1,12 @@
+<?php
+ // created: 2019-02-06 10:16:32
+$dictionary['Account']['fields']['tct_nuevo_pld_c']['labelValue']='Nuevo Pld';
+$dictionary['Account']['fields']['tct_nuevo_pld_c']['full_text_search']=array (
+  'enabled' => '0',
+  'boost' => '1',
+  'searchable' => false,
+);
+$dictionary['Account']['fields']['tct_nuevo_pld_c']['enforced']='';
+$dictionary['Account']['fields']['tct_nuevo_pld_c']['dependency']='';
+
+ ?>
