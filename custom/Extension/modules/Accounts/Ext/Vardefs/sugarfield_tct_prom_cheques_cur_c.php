f61c1a93296824e40262d47467069e6b261495e1
Adrian Arauz <adrian.arauz@tactos.com.mx>
2019-02-06 17:47:41 -0600

    cambio nombre campo Potencial

--- custom/Extension/modules/Accounts/Ext/Vardefs/sugarfield_tct_prom_cheques_cur_c.php
+++ custom/Extension/modules/Accounts/Ext/Vardefs/sugarfield_tct_prom_cheques_cur_c.php
@@ -1,6 +1,6 @@
 <?php
- // created: 2019-02-06 16:21:06
-$dictionary['Account']['fields']['tct_prom_cheques_cur_c']['labelValue']='Saldo de cheques de estados de cuenta';
+ // created: 2019-02-06 17:45:10
+$dictionary['Account']['fields']['tct_prom_cheques_cur_c']['labelValue']='Saldo de cheques promedio de estados de cuenta';
 $dictionary['Account']['fields']['tct_prom_cheques_cur_c']['enforced']='';
 $dictionary['Account']['fields']['tct_prom_cheques_cur_c']['dependency']='';
 $dictionary['Account']['fields']['tct_prom_cheques_cur_c']['related_fields']=array (
