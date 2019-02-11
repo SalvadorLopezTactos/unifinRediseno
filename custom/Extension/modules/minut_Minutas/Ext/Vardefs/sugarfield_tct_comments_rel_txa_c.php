85fac14164e643a143549acf47a84ca38a851ce5
Salvador Lopez <salvador.lopez@tactos.com.mx>
2019-02-11 12:45:14 -0600

    Nuevo campo de comentarios relvantes en Minuta

---
+++ custom/Extension/modules/minut_Minutas/Ext/Vardefs/sugarfield_tct_comments_rel_txa_c.php
@@ -0,0 +1,12 @@
+<?php
+ // created: 2019-02-11 12:42:46
+$dictionary['minut_Minutas']['fields']['tct_comments_rel_txa_c']['labelValue']='Comentarios relevantes de la reunión';
+$dictionary['minut_Minutas']['fields']['tct_comments_rel_txa_c']['full_text_search']=array (
+  'enabled' => '0',
+  'boost' => '1',
+  'searchable' => false,
+);
+$dictionary['minut_Minutas']['fields']['tct_comments_rel_txa_c']['enforced']='';
+$dictionary['minut_Minutas']['fields']['tct_comments_rel_txa_c']['dependency']='';
+
+ ?>
