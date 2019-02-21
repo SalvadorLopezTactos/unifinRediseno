dd4ba3fd5c3bdddd4c858e9a517fc6e927a1286d
Salvador Lopez <salvador.lopez@tactos.com.mx>
2019-02-21 16:49:21 -0600

    Corrección opciones de listas para tipo de dirección

--- custom/Extension/application/Ext/Language/es_LA.sugar_tipodedirecion_list.php
+++ custom/Extension/application/Ext/Language/es_LA.sugar_tipodedirecion_list.php
@@ -4,9 +4,9 @@
 $app_list_strings['tipodedirecion_list']=array (
   1 => 'PARTICULAR',
   2 => 'LABORAL',
+  3 => 'PARTICULAR Y LABORAL',
   4 => 'SUCURSAL',
-  5 => 'PARTICULAR Y LABORAL',
-  6 => 'PARTICULAR Y SUCURSAL',
-  7 => 'LABORAL Y SUCURSAL',
-  8 => 'PARTICULAR, LABORAL Y SUCURSAL',
+  5 => 'PARTICULAR Y SUCURSAL',
+  6 => 'LABORAL Y SUCURSAL',
+  7 => 'PARTICULAR, LABORAL Y SUCURSAL',
 );
