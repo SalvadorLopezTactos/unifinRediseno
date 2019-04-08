dc8330342fb2407af419cd2e32920bc6e4c7d713
Eduardo Carrasco <eduardo.carrasco@tactos.com.mx>
2019-04-08 16:36:52 -0500

    Usuarios-Nombre Completo

--- custom/Extension/modules/Users/Ext/Vardefs/sugarfield_nombre_completo_c.php
+++ custom/Extension/modules/Users/Ext/Vardefs/sugarfield_nombre_completo_c.php
@@ -1,7 +1,15 @@
 <?php
- // created: 2016-11-29 17:17:10
+ // created: 2019-04-08 16:21:37
+$dictionary['User']['fields']['nombre_completo_c']['duplicate_merge_dom_value']=0;
 $dictionary['User']['fields']['nombre_completo_c']['labelValue']='Nombre completo';
-$dictionary['User']['fields']['nombre_completo_c']['enforced']='';
+$dictionary['User']['fields']['nombre_completo_c']['full_text_search']=array (
+  'enabled' => '0',
+  'boost' => '1',
+  'searchable' => false,
+);
+$dictionary['User']['fields']['nombre_completo_c']['calculated']='true';
+$dictionary['User']['fields']['nombre_completo_c']['formula']='concat($first_name," ",$last_name)';
+$dictionary['User']['fields']['nombre_completo_c']['enforced']='true';
 $dictionary['User']['fields']['nombre_completo_c']['dependency']='';
 
  ?>
