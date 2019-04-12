8418f1ae2b242582b97d1f849da57eed024f2ee2
Axel Flores <axel.flores@tactos.com.mx>
2019-04-12 14:04:31 -0500

    Se agrega valor default Active en campo employee_status del módulo de Usuarios

---
+++ custom/Extension/modules/Users/Ext/Vardefs/sugarfield_employee_status.php
@@ -0,0 +1,13 @@
+<?php
+ // created: 2019-04-12 13:52:57
+$dictionary['User']['fields']['employee_status']['default']='Active';
+$dictionary['User']['fields']['employee_status']['audited']=false;
+$dictionary['User']['fields']['employee_status']['massupdate']=true;
+$dictionary['User']['fields']['employee_status']['duplicate_merge']='enabled';
+$dictionary['User']['fields']['employee_status']['duplicate_merge_dom_value']='1';
+$dictionary['User']['fields']['employee_status']['merge_filter']='disabled';
+$dictionary['User']['fields']['employee_status']['unified_search']=false;
+$dictionary['User']['fields']['employee_status']['calculated']=false;
+$dictionary['User']['fields']['employee_status']['dependency']=false;
+
+ ?>
