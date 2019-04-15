732ff3c3773401960f81376c6cf4411a4baa2d63
Adrian Arauz <adrian.arauz@tactos.com.mx>
2019-04-15 12:23:56 -0500

    Ajuste mayusculas en campo tct_tipo_subtipo_txf_c

--- custom/Extension/modules/Accounts/Ext/Vardefs/sugarfield_tct_tipo_subtipo_txf_c.php
+++ custom/Extension/modules/Accounts/Ext/Vardefs/sugarfield_tct_tipo_subtipo_txf_c.php
@@ -1,20 +1,23 @@
 <?php
- // created: 2018-12-05 18:17:34
-$dictionary['Account']['fields']['tct_tipo_subtipo_txf_c']['duplicate_merge_dom_value'] = 0;
-$dictionary['Account']['fields']['tct_tipo_subtipo_txf_c']['labelValue'] = 'Tipo y Subtipo de la Cuenta';
-$dictionary['Account']['fields']['tct_tipo_subtipo_txf_c']['full_text_search']['enabled'] = true;
-$dictionary['Account']['fields']['tct_tipo_subtipo_txf_c']['full_text_search']['searchable'] = false;
-$dictionary['Account']['fields']['tct_tipo_subtipo_txf_c']['full_text_search']['boost'] = 1;
-$dictionary['Account']['fields']['tct_tipo_subtipo_txf_c']['calculated'] = '1';
-$dictionary['Account']['fields']['tct_tipo_subtipo_txf_c']['formula'] = 'ifElse(
+ // created: 2019-04-15 12:03:48
+$dictionary['Account']['fields']['tct_tipo_subtipo_txf_c']['duplicate_merge_dom_value']=0;
+$dictionary['Account']['fields']['tct_tipo_subtipo_txf_c']['labelValue']='Tipo y Subtipo de la Cuenta';
+$dictionary['Account']['fields']['tct_tipo_subtipo_txf_c']['full_text_search']=array (
+  'enabled' => '0',
+  'boost' => '1',
+  'searchable' => false,
+);
+$dictionary['Account']['fields']['tct_tipo_subtipo_txf_c']['calculated']='1';
+$dictionary['Account']['fields']['tct_tipo_subtipo_txf_c']['formula']='ifElse(
 	equal($tipo_registro_c,"Persona"),
 	"PERSONA",
 	ifElse(
 		equal($tipo_registro_c,"Proveedor"),
 		"PROVEEDOR",
-		concat(getDropdownValue("tipo_registro_list",$tipo_registro_c)," ",getDropdownValue("subtipo_cuenta_list",$subtipo_cuenta_c))
+		strToUpper(concat(getDropdownValue("tipo_registro_list",$tipo_registro_c)," ",getDropdownValue("subtipo_cuenta_list",$subtipo_cuenta_c)))
 	)
 )';
-$dictionary['Account']['fields']['tct_tipo_subtipo_txf_c']['enforced'] = '1';
-$dictionary['Account']['fields']['tct_tipo_subtipo_txf_c']['dependency'] = '';
+$dictionary['Account']['fields']['tct_tipo_subtipo_txf_c']['enforced']='1';
+$dictionary['Account']['fields']['tct_tipo_subtipo_txf_c']['dependency']='';
 
+ ?>
