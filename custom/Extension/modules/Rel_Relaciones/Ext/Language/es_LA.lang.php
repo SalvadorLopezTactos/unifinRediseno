3431047d6155ef9b4990004b3b5d1f3642a5b2e9
Victor Martinez <victor.martinez@tactos.com.mx>
2019-04-22 09:44:21 -0500

    Etiqueta de Relación

--- custom/Extension/modules/Rel_Relaciones/Ext/Language/es_LA.lang.php
+++ custom/Extension/modules/Rel_Relaciones/Ext/Language/es_LA.lang.php
@@ -1,3 +1,4 @@
 <?php
 // WARNING: The contents of this file are auto-generated.
 $mod_strings['LBL_PORCENTAJE_PARTICIPACION'] = 'Porcentaje de Participación';
+$mod_strings['LBL_RELACION'] = 'Relación';
