d68166db4685fce0e9e145df80ce7de35d9ce725
Salvador Lopez <salvador.lopez@enterconecta.com>
2019-04-26 09:24:24 -0500

    Cambio de fórmula para calcular campo nombre_comercial_c

--- custom/Extension/modules/Accounts/Ext/Vardefs/sugarfield_nombre_comercial_c.php
+++ custom/Extension/modules/Accounts/Ext/Vardefs/sugarfield_nombre_comercial_c.php
@@ -1,15 +1,15 @@
 <?php
- // created: 2019-04-15 17:19:11
-$dictionary['Account']['fields']['nombre_comercial_c']['labelValue'] = 'Nombre Comercial';
-$dictionary['Account']['fields']['nombre_comercial_c']['full_text_search']['enabled'] = true;
-$dictionary['Account']['fields']['nombre_comercial_c']['full_text_search']['searchable'] = true;
-$dictionary['Account']['fields']['nombre_comercial_c']['full_text_search']['boost'] = 1;
-$dictionary['Account']['fields']['nombre_comercial_c']['calculated'] = '1';
-$dictionary['Account']['fields']['nombre_comercial_c']['formula'] = 'ifElse(
-equal($nombre_comercial_c,""),
-$name,
-$nombre_comercial_c
-)';
-$dictionary['Account']['fields']['nombre_comercial_c']['enforced'] = '';
-$dictionary['Account']['fields']['nombre_comercial_c']['dependency'] = 'not(equal($tipodepersona_c,"Persona Fisica"))';
+ // created: 2019-04-26 09:11:00
+$dictionary['Account']['fields']['nombre_comercial_c']['duplicate_merge_dom_value']=0;
+$dictionary['Account']['fields']['nombre_comercial_c']['labelValue']='Nombre Comercial';
+$dictionary['Account']['fields']['nombre_comercial_c']['full_text_search']=array (
+  'enabled' => true,
+  'boost' => '1',
+  'searchable' => true,
+);
+$dictionary['Account']['fields']['nombre_comercial_c']['calculated']='true';
+$dictionary['Account']['fields']['nombre_comercial_c']['formula']='$name';
+$dictionary['Account']['fields']['nombre_comercial_c']['enforced']='';
+$dictionary['Account']['fields']['nombre_comercial_c']['dependency']='not(equal($tipodepersona_c,"Persona Fisica"))';
 
+ ?>
