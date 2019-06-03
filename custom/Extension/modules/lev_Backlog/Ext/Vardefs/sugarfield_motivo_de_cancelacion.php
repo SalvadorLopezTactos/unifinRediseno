ccd8cf6cf9c64667cf9ad0e8e02e484531901fa9
Adrian Arauz <adrian.arauz@tactos.com.mx>
2019-06-03 12:39:24 -0500

    Motivos cancelacion-Backlog

--- custom/Extension/modules/lev_Backlog/Ext/Vardefs/sugarfield_motivo_de_cancelacion.php
+++ custom/Extension/modules/lev_Backlog/Ext/Vardefs/sugarfield_motivo_de_cancelacion.php
@@ -1,6 +1,7 @@
 <?php
- // created: 2019-04-15 17:19:11
-$dictionary['lev_Backlog']['fields']['motivo_de_cancelacion']['audited'] = true;
-$dictionary['lev_Backlog']['fields']['motivo_de_cancelacion']['dependency'] = 'or(equal($estatus_de_la_operacion,"Cancelada"),equal($estatus_de_la_operacion,"Cancelada por cliente"))';
-$dictionary['lev_Backlog']['fields']['motivo_de_cancelacion']['full_text_search']['boost'] = 1;
+ // created: 2019-06-03 12:22:25
+$dictionary['lev_Backlog']['fields']['motivo_de_cancelacion']['audited']=true;
+$dictionary['lev_Backlog']['fields']['motivo_de_cancelacion']['dependency']='or(equal($estatus_de_la_operacion,"Cancelada"),equal($estatus_de_la_operacion,"Cancelada por cliente"))';
+$dictionary['lev_Backlog']['fields']['motivo_de_cancelacion']['default']='';
 
+ ?>
