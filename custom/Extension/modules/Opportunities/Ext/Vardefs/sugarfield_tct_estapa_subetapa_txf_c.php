a3b58e5adc0c394648c7d3d5fe656bc12fb426f9
Salvador Lopez <salvador.lopez@enterconecta.com>
2019-06-17 11:22:23 -0500

    Ajuste a valor calculado del campo tct_estapa_subetapa_txf_c

--- custom/Extension/modules/Opportunities/Ext/Vardefs/sugarfield_tct_estapa_subetapa_txf_c.php
+++ custom/Extension/modules/Opportunities/Ext/Vardefs/sugarfield_tct_estapa_subetapa_txf_c.php
@@ -1,12 +1,15 @@
 <?php
- // created: 2019-04-15 17:19:11
-$dictionary['Opportunity']['fields']['tct_estapa_subetapa_txf_c']['duplicate_merge_dom_value'] = 0;
-$dictionary['Opportunity']['fields']['tct_estapa_subetapa_txf_c']['labelValue'] = 'Etapa y Subetapa de la Solicitud';
-$dictionary['Opportunity']['fields']['tct_estapa_subetapa_txf_c']['full_text_search']['enabled'] = true;
-$dictionary['Opportunity']['fields']['tct_estapa_subetapa_txf_c']['full_text_search']['searchable'] = true;
-$dictionary['Opportunity']['fields']['tct_estapa_subetapa_txf_c']['full_text_search']['boost'] = 1;
-$dictionary['Opportunity']['fields']['tct_estapa_subetapa_txf_c']['calculated'] = '1';
-$dictionary['Opportunity']['fields']['tct_estapa_subetapa_txf_c']['formula'] = 'concat(getDropdownValue("tct_etapa_ddw_c_list",$tct_etapa_ddw_c)," ",getDropdownValue("estatus_c_operacion_list",$estatus_c))';
-$dictionary['Opportunity']['fields']['tct_estapa_subetapa_txf_c']['enforced'] = '1';
-$dictionary['Opportunity']['fields']['tct_estapa_subetapa_txf_c']['dependency'] = '';
+ // created: 2019-06-17 11:20:25
+$dictionary['Opportunity']['fields']['tct_estapa_subetapa_txf_c']['duplicate_merge_dom_value']=0;
+$dictionary['Opportunity']['fields']['tct_estapa_subetapa_txf_c']['labelValue']='Etapa y Subetapa de la Solicitud';
+$dictionary['Opportunity']['fields']['tct_estapa_subetapa_txf_c']['full_text_search']=array (
+  'enabled' => true,
+  'boost' => '1',
+  'searchable' => true,
+);
+$dictionary['Opportunity']['fields']['tct_estapa_subetapa_txf_c']['calculated']='true';
+$dictionary['Opportunity']['fields']['tct_estapa_subetapa_txf_c']['formula']='$tct_estapa_subetapa_txf_c';
+$dictionary['Opportunity']['fields']['tct_estapa_subetapa_txf_c']['enforced']='true';
+$dictionary['Opportunity']['fields']['tct_estapa_subetapa_txf_c']['dependency']='';
 
+ ?>
