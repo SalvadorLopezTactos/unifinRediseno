44a8ae51b8abcf4574733b15a73013cb7304d688
Salvador Lopez <salvador.lopez@enterconecta.com>
2019-06-21 10:38:11 -0500

    Modificación lista de valores de resultado de llamada

--- custom/Extension/application/Ext/Language/es_LA.sugar_tct_resultado_llamada_ddw_list.php
+++ custom/Extension/application/Ext/Language/es_LA.sugar_tct_resultado_llamada_ddw_list.php
@@ -1,12 +1,11 @@
 <?php
- // created: 2018-08-01 20:49:02
+ // created: 2019-06-21 10:37:01
 
 $app_list_strings['tct_resultado_llamada_ddw_list']=array (
   '' => '',
   'Ilocalizable' => 'Ilocalizable',
   'Cita' => 'Cita',
-  'Seguimiento' => 'Seguimiento',
-  'Conferencia' => 'Conferencia',
+  'Nueva_llamada' => 'Se agendó nueva llamada',
   'No_esta_Interesado' => 'No está Interesado',
   'Fuera_de_Perfil' => 'Fuera de Perfil',
 );
