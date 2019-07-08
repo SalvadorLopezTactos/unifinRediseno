b9b5b5bb213d9be0c5f724027d3a32d112791169
Salvador Lopez <salvador.lopez@enterconecta.com>
2019-07-08 10:02:34 -0500

    Nuevo sub resultado de Resultado de la llamada Ilocalizable

--- custom/Extension/application/Ext/Language/es_LA.sugar_tct_motivo_ilocalizable_ddw_list.php
+++ custom/Extension/application/Ext/Language/es_LA.sugar_tct_motivo_ilocalizable_ddw_list.php
@@ -1,8 +1,9 @@
 <?php
- // created: 2018-09-25 17:02:46
+ // created: 2019-07-08 10:00:58
 
 $app_list_strings['tct_motivo_ilocalizable_ddw_list']=array (
   '' => '',
   'Datos_Incorrectos' => 'Datos Incorrectos',
   'La_Empresa_ya_no_existe' => 'La Empresa ya no existe',
+  'No fue posible contactarlo' => 'No fue posible contactarlo',
 );
