1ca09f66593e3207649a40cb6852536f860d23e8
Adrian Arauz <adrian.arauz@tactos.com.mx>
2019-07-15 18:37:28 -0500

    Requerimiento campo Probabilidad de Conversion-Backlog

--- custom/Extension/modules/lev_Backlog/Ext/Language/es_LA.lang.php
+++ custom/Extension/modules/lev_Backlog/Ext/Language/es_LA.lang.php
@@ -66,3 +66,4 @@ $mod_strings['LBL_TCT_QUE_PRODUCTO_TXF'] = '¿Qué producto? ';
 $mod_strings['LBL_TCT_TIPO_OP_LEASING_MLS'] = 'Tipo Operación Leasing';
 $mod_strings['LBL_TCT_CARGA_MASIVA_CHK'] = 'Carga Masiva';
 $mod_strings['LBL_TCT_BLOQUEO_TXF'] = 'Bloqueo';
+$mod_strings['LBL_TCT_CONVERSION'] = 'Probabilidad de Conversión %';
