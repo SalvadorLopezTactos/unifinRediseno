8c74f13d6d58e38490f7f6e7b8595036ad25260f
Adrian Arauz <adrian.arauz@tactos.com.mx>
2019-08-22 17:35:22 -0500

    Ajuste Cuenta - Venta Anuales 145912

--- custom/Extension/modules/Accounts/Ext/Vardefs/sugarfield_tct_ano_ventas_ddw_c.php
+++ custom/Extension/modules/Accounts/Ext/Vardefs/sugarfield_tct_ano_ventas_ddw_c.php
@@ -1,5 +1,5 @@
 <?php
- // created: 2019-08-22 16:38:10
+ // created: 2019-08-22 17:28:37
 $dictionary['Account']['fields']['tct_ano_ventas_ddw_c']['labelValue']='Año de Ventas Anuales';
 $dictionary['Account']['fields']['tct_ano_ventas_ddw_c']['dependency']='';
 $dictionary['Account']['fields']['tct_ano_ventas_ddw_c']['visibility_grid']='';
