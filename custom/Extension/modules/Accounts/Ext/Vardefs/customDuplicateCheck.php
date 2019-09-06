043b71e6b7f0acebed2696e5ef47bee7d2ca8d9a
Axel Flores <axel.flores@tactos.com.mx>
2019-09-06 15:45:54 -0500

    RFC - Validación creación - 145825

--- custom/Extension/modules/Accounts/Ext/Vardefs/customDuplicateCheck.php
+++ custom/Extension/modules/Accounts/Ext/Vardefs/customDuplicateCheck.php
@@ -5,17 +5,22 @@
  * Date: 6/9/2015
  * Time: 10:42 AM
  */
-
 $dictionary['Account']['duplicate_check'] = array(
     'enabled' => true,
     'FilterDuplicateCheck' => array(
         'filter_template' => array(
             array(
                 '$or' => array(
-                    array('rfc_c' => array('$equals' => '$rfc_c')),
                     array('clean_name' => array('$equals' => '$clean_name')),
+                    array(
+                        '$and' => array(
+                            array('rfc_c' => array('$equals' => '$rfc_c')),
+                            array('rfc_c' => array('$not_equals' => 'XXX010101XXX')),
+                            array('rfc_c' => array('$not_equals' => 'XXXX010101XXX')),
+                        ),
+                    )
                 )
-            ),
+            )
         ),
         'ranking_fields' => array(
             array('in_field_name' => 'rfc_c', 'dupe_field_name' => 'rfc_c'),
