1c50fbb7d4fb8bd53c3cbf3e08db27678f463f40
Axel Flores <axel.flores@tactos.com.mx>
2019-09-11 17:06:32 -0500

    RFC - Validación creación - 145825 - Ajuste creación sin RFC

--- custom/Extension/modules/Accounts/Ext/Vardefs/customDuplicateCheck.php
+++ custom/Extension/modules/Accounts/Ext/Vardefs/customDuplicateCheck.php
@@ -7,24 +7,9 @@
  */
 $dictionary['Account']['duplicate_check'] = array(
     'enabled' => true,
-    'FilterDuplicateCheck' => array(
-        'filter_template' => array(
-            array(
-                '$or' => array(
-                    array('clean_name' => array('$equals' => '$clean_name')),
-                    array(
-                        '$and' => array(
-                            array('rfc_c' => array('$equals' => '$rfc_c')),
-                            array('rfc_c' => array('$not_equals' => 'XXX010101XXX')),
-                            array('rfc_c' => array('$not_equals' => 'XXXX010101XXX')),
-                        ),
-                    )
-                )
-            )
-        ),
-        'ranking_fields' => array(
-            array('in_field_name' => 'rfc_c', 'dupe_field_name' => 'rfc_c'),
-            array('in_field_name' => 'clean_name', 'dupe_field_name' => 'clean_name'),
-        )
+    'UnifinFieldDuplicateCheck' => array(
+        'field1' => 'clean_name',
+        'field2' => 'rfc',
+        'field3' => 'name'
     )
 );
