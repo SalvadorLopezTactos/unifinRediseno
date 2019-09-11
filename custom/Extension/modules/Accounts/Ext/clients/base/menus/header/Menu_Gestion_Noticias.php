11565c6b13ad403a638413abead36096a9edf187
Axel Flores <axel.flores@tactos.com.mx>
2019-09-11 09:58:34 -0500

    Vista 360 - Noticias generales 146090 - Front

---
+++ custom/Extension/modules/Accounts/Ext/clients/base/menus/header/Menu_Gestion_Noticias.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: AF. Tactos
+ * Date: 2019/09/09
+ */
+$viewdefs['Accounts']['base']['menu']['header'][] = array(
+    'route'=>'#Accounts/layout/Gestion_Noticias_Layout',
+    'label' =>'LNK_GESTION_NOTICIAS',
+    'acl_module'=>'Accounts',
+    'icon' => 'fa-edit',
+);
