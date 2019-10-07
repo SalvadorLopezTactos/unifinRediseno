9cfed4b7806cf1de233b05615db27c3ed1229b26
Salvador Lopez <salvador.lopez@enterconecta.com>
2019-10-07 18:31:04 -0500

    Nuevo valor en lista de productos de Usuarios

--- custom/Extension/application/Ext/Language/es_LA.sugar_tipo_producto_list.php
+++ custom/Extension/application/Ext/Language/es_LA.sugar_tipo_producto_list.php
@@ -1,5 +1,5 @@
 <?php
- // created: 2018-01-10 14:12:00
+ // created: 2019-10-07 18:25:10
 
 $app_list_strings['tipo_producto_list']=array (
   1 => 'LEASING',
@@ -7,4 +7,5 @@ $app_list_strings['tipo_producto_list']=array (
   3 => 'CREDITO AUTOMOTRIZ',
   4 => 'FACTORAJE',
   5 => 'LINEA CREDITO SIMPLE',
+  6 => 'FLEET',
 );
