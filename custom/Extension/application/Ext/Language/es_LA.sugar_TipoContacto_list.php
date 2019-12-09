f70348ba51ac72d37949eb315d67f6cefbd8d625
Salvador Lopez <salvador.lopez@enterconecta.com>
2019-12-09 12:48:52 -0600

    Nuevos catálogos (RelacionesActivas_list,TipoContacto_list)

---
+++ custom/Extension/application/Ext/Language/es_LA.sugar_TipoContacto_list.php
@@ -0,0 +1,12 @@
+<?php
+ // created: 2018-10-31 11:53:17
+
+$app_list_strings['TipoContacto_list']=array (
+  '0' => '',
+  '1' => 'Promoción',
+  '2' => 'Cobranza',
+  '3' => 'Administración',
+  '4' => 'Entrega de bienes',
+  '5' => 'Factoraje',
+  '6' => 'Firmante VR',
+);
