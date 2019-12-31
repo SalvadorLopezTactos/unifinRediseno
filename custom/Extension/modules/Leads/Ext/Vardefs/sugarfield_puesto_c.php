0ee0b06d510bf75c08e83321359f3656936bc7a9
IRVING DELGADO <47790645+devai92@users.noreply.github.com>
2019-12-31 13:11:38 -0600

    Update sugarfield_puesto_c.php
    update field formula

--- custom/Extension/modules/Leads/Ext/Vardefs/sugarfield_puesto_c.php
+++ custom/Extension/modules/Leads/Ext/Vardefs/sugarfield_puesto_c.php
@@ -1,6 +1,8 @@
 <?php
  // created: 2019-12-31 16:08:06
 $dictionary['Lead']['fields']['puesto_c']['labelValue']='Puesto';
-$dictionary['Lead']['fields']['puesto_c']['visibility_grid']='';
-
+$dictionary['Lead']['fields']['puesto_c']['dependency']='or(
+equal($regimen_fiscal_c,"Persona Fisica"),
+equal($regimen_fiscal_c,"Persona Fisica con Actividad Empresarial")
+)';
  ?>
