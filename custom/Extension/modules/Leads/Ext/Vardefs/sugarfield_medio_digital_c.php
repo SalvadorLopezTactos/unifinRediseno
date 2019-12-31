08391e655138f4b09d6d9183941799038d04e7fb
IRVING DELGADO <47790645+devai92@users.noreply.github.com>
2019-12-31 13:09:20 -0600

    Update sugarfield_medio_digital_c.php
    update field formula

--- custom/Extension/modules/Leads/Ext/Vardefs/sugarfield_medio_digital_c.php
+++ custom/Extension/modules/Leads/Ext/Vardefs/sugarfield_medio_digital_c.php
@@ -1,6 +1,63 @@
 <?php
  // created: 2019-12-31 17:09:21
 $dictionary['Lead']['fields']['medio_digital_c']['labelValue']='Medio';
-$dictionary['Lead']['fields']['medio_digital_c']['visibility_grid']='';
+$dictionary['Lead']['fields']['medio_digital_c']['visibility_grid']=array (
+  'trigger' => 'detalle_origen_c',
+  'values' => 
+  array (
+    '' => 
+    array (
+    ),
+    'Bases de datos' => 
+    array (
+    ),
+    'Centro de Prospeccion' => 
+    array (
+    ),
+    'Digital' => 
+    array (
+      0 => '',
+      1 => 'Facebook',
+      2 => 'Twitter',
+      3 => 'Instagram',
+      4 => 'Web',
+      5 => 'LinkedIn',
+      6 => 'Radio Online',
+      7 => 'Prensa Online',
+      8 => 'TV Online',
+      9 => 'Revistas Online',
+    ),
+    'Campanas' => 
+    array (
+    ),
+    'Acciones Estrategicas' => 
+    array (
+    ),
+    'Afiliaciones' => 
+    array (
+    ),
+    'Llamadas Inbound' => 
+    array (
+    ),
+    'Parques Industriales' => 
+    array (
+    ),
+    'Offline' => 
+    array (
+      0 => '',
+      1 => 'TV',
+      2 => 'Radio',
+      3 => 'Prensa',
+      4 => 'Revistas',
+      5 => 'Espectaculares',
+    ),
+    'Cartera Promotores' => 
+    array (
+    ),
+    'Recomendacion' => 
+    array (
+    ),
+  ),
+);
 
  ?>
