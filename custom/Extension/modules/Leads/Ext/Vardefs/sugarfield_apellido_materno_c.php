28a7957cdbd12c233b43abd988bab9d11152f9ae
IRVING DELGADO <47790645+devai92@users.noreply.github.com>
2019-12-31 13:13:19 -0600

    Update sugarfield_apellido_materno_c.php
    update field

--- custom/Extension/modules/Leads/Ext/Vardefs/sugarfield_apellido_materno_c.php
+++ custom/Extension/modules/Leads/Ext/Vardefs/sugarfield_apellido_materno_c.php
@@ -7,5 +7,9 @@ $dictionary['Lead']['fields']['apellido_materno_c']['full_text_search']=array (
   'searchable' => false,
 );
 $dictionary['Lead']['fields']['apellido_materno_c']['enforced']='';
+$dictionary['Lead']['fields']['apellido_materno_c']['dependency']='or(
+equal($regimen_fiscal_c,"Persona Fisica"),
+equal($regimen_fiscal_c,"Persona Fisica con Actividad Empresarial")
+)';
 
  ?>
