1785556db1f1a99aeababd05c917cd7af126267b
IRVING DELGADO <47790645+devai92@users.noreply.github.com>
2019-12-31 13:15:33 -0600

    Update sugarfield_nombre_empresa_c.php
    update field

--- custom/Extension/modules/Leads/Ext/Vardefs/sugarfield_nombre_empresa_c.php
+++ custom/Extension/modules/Leads/Ext/Vardefs/sugarfield_nombre_empresa_c.php
@@ -7,5 +7,5 @@ $dictionary['Lead']['fields']['nombre_empresa_c']['full_text_search']=array (
   'searchable' => false,
 );
 $dictionary['Lead']['fields']['nombre_empresa_c']['enforced']='';
-
+$dictionary['Lead']['fields']['nombre_empresa_c']['dependency']='equal($regimen_fiscal_c,"Persona Moral")';
  ?>
