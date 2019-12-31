4e2a7441d3d52e5d05fb45ba147e69d3660c2224
IRVING DELGADO <47790645+devai92@users.noreply.github.com>
2019-12-31 13:16:16 -0600

    Update sugarfield_origen_busqueda_c.php
    update field

--- custom/Extension/modules/Leads/Ext/Vardefs/sugarfield_origen_busqueda_c.php
+++ custom/Extension/modules/Leads/Ext/Vardefs/sugarfield_origen_busqueda_c.php
@@ -7,5 +7,5 @@ $dictionary['Lead']['fields']['origen_busqueda_c']['full_text_search']=array (
   'searchable' => false,
 );
 $dictionary['Lead']['fields']['origen_busqueda_c']['enforced']='';
-
+$dictionary['Lead']['fields']['origen_busqueda_c']['dependency']='equal($detalle_origen_c,"Bases de datos")';
  ?>
