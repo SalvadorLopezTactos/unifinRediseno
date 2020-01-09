9332882400b721158e62fc378baaaee6b31e6b15
Eduardo Carrasco <eduardo.carrasco@tactos.com.mx>
2020-01-09 15:44:56 -0600

    156382-Gestión Agentes

---
+++ custom/clients/base/layouts/agentes-telefonicos/agentes-telefonicos.php
@@ -0,0 +1,10 @@
+<?php
+$viewdefs['base']['layout']['agentes-telefonicos'] = array(
+    'type' => 'simple',
+    'components' => array(
+        array(
+            'view' => 'agentes-telefonicos',
+        ),
+    ),
+);
+
