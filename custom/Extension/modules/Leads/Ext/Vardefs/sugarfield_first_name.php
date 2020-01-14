e21bfc2797f0d5911c408921d274dd9ba80badc3
IRVING DELGADO <irving.delgado@tactos.com.mx>
2020-01-14 11:54:24 -0600

    148691 CP - Asignación de nombre lead a key last_name

--- custom/Extension/modules/Leads/Ext/Vardefs/sugarfield_first_name.php
+++ custom/Extension/modules/Leads/Ext/Vardefs/sugarfield_first_name.php
@@ -1,18 +1,16 @@
 <?php
- // created: 2020-01-06 16:41:26
+ // created: 2020-01-14 16:31:19
 $dictionary['Lead']['fields']['first_name']['massupdate']=false;
 $dictionary['Lead']['fields']['first_name']['comments']='';
 $dictionary['Lead']['fields']['first_name']['duplicate_merge']='disabled';
-$dictionary['Lead']['fields']['first_name']['duplicate_merge_dom_value']=0;
+$dictionary['Lead']['fields']['first_name']['duplicate_merge_dom_value']='0';
 $dictionary['Lead']['fields']['first_name']['merge_filter']='disabled';
 $dictionary['Lead']['fields']['first_name']['full_text_search']=array (
-  'enabled' => true,
+  'enabled' => '0',
   'boost' => '1.87',
-  'searchable' => true,
+  'searchable' => false,
 );
-$dictionary['Lead']['fields']['first_name']['calculated']='true';
+$dictionary['Lead']['fields']['first_name']['calculated']=false;
 $dictionary['Lead']['fields']['first_name']['importable']='false';
-$dictionary['Lead']['fields']['first_name']['formula']='$name_c';
-$dictionary['Lead']['fields']['first_name']['enforced']=true;
 
  ?>
--- custom/Extension/modules/Leads/Ext/Vardefs/sugarfield_last_name.php
+++ custom/Extension/modules/Leads/Ext/Vardefs/sugarfield_last_name.php
@@ -1,16 +1,19 @@
 <?php
- // created: 2020-01-07 16:39:37
+ // created: 2020-01-14 17:06:40
 $dictionary['Lead']['fields']['last_name']['massupdate']=false;
 $dictionary['Lead']['fields']['last_name']['comments']='Last name of the contact';
 $dictionary['Lead']['fields']['last_name']['duplicate_merge']='disabled';
-$dictionary['Lead']['fields']['last_name']['duplicate_merge_dom_value']='0';
+$dictionary['Lead']['fields']['last_name']['duplicate_merge_dom_value']=0;
 $dictionary['Lead']['fields']['last_name']['merge_filter']='disabled';
 $dictionary['Lead']['fields']['last_name']['full_text_search']=array (
   'enabled' => '0',
   'boost' => '1.85',
   'searchable' => false,
 );
-$dictionary['Lead']['fields']['last_name']['calculated']=false;
+$dictionary['Lead']['fields']['last_name']['calculated']='true';
 $dictionary['Lead']['fields']['last_name']['required']=false;
+$dictionary['Lead']['fields']['last_name']['importable']='false';
+$dictionary['Lead']['fields']['last_name']['formula']='$name_c';
+$dictionary['Lead']['fields']['last_name']['enforced']=true;
 
  ?>
