5dd307c44f3881d379ab9cdb4f56a8189b016b2c
Eduardo Carrasco <eduardo.carrasco@tactos.com.mx>
2020-01-14 16:56:10 -0600

    156425-Resultado Confirmado

--- custom/Extension/application/Ext/Language/es_LA.sugar_resultado_confirmado_c_list.php
+++ custom/Extension/application/Ext/Language/es_LA.sugar_resultado_confirmado_c_list.php
@@ -1,7 +1,8 @@
 <?php
- // created: 2019-12-30 13:50:11
+ // created: 2020-01-14 16:31:50
 
 $app_list_strings['resultado_confirmado_c_list']=array (
   1 => 'El resultado coincide',
   2 => 'El prospecto indica lo contrario',
+  '' => '',
 );
--- custom/Extension/modules/Meetings/Ext/Vardefs/sugarfield_resultado_confirmado_c.php
+++ custom/Extension/modules/Meetings/Ext/Vardefs/sugarfield_resultado_confirmado_c.php
@@ -1,5 +1,5 @@
 <?php
- // created: 2019-12-30 13:50:32
+ // created: 2020-01-14 16:32:20
 $dictionary['Meeting']['fields']['resultado_confirmado_c']['labelValue']='Resultado Confirmado';
 $dictionary['Meeting']['fields']['resultado_confirmado_c']['dependency']='';
 $dictionary['Meeting']['fields']['resultado_confirmado_c']['visibility_grid']='';
