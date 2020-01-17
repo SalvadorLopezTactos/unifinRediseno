7bb67abeb31f2ed3b06ee689bc5e6a5c26cbbaeb
Eduardo Carrasco <eduardo.carrasco@tactos.com.mx>
2020-01-17 15:18:21 -0600

    156382-Gestión Agentes

--- custom/clients/base/views/agentes-telefonicos/agentes-telefonicos.js
+++ custom/clients/base/views/agentes-telefonicos/agentes-telefonicos.js
@@ -11,7 +11,7 @@
     initialize: function(options){
         this._super("initialize", [options]);
         this.loadView = false;
-        if(app.user.attributes.agente_telefonico_c == 1 || app.user.attributes.type == "admin"){
+        if(app.user.attributes.agente_telefonico_c == 1){
           this.loadView = true;
           agentes = this;
           agentes.filtros = {
@@ -46,61 +46,69 @@
         var AgenteN = $('#AgenteN').val().trim();
         var AgenteA = $('#AgenteA').val().trim();
         var equipoA = $("#Equipos").val();
-        agentes.filtros = {
-            "Nombre": AgenteN,
-            "Apellidos": AgenteA,
-            "Equipo": equipoA,
-            "Informa": Informa
-        };
-        $('#successful').hide();
-        var strUrl = 'Users?fields=id,nombre_completo_c,puestousuario_c,reports_to_id,reports_to_name,equipo_c&max_num=-1&filter[][status]=Active&filter[1][$or][0][puestousuario_c]=27&filter[1][$or][1][puestousuario_c]=31&order_by=nombre_completo_c:asc';
-        if(AgenteN != "" && AgenteN != null) {
-          strUrl = strUrl + '&filter[][first_name][$contains]=' + AgenteN;
-        }
-        if(AgenteA != "" && AgenteA != null) {
-          strUrl = strUrl + '&filter[][last_name][$contains]=' + AgenteA;
-        }
-        if(equipoA != "0" && equipoA != null) {
-          strUrl = strUrl + '&filter[][equipo_c][$equals]=' + equipoA;
-        }
-        if(Informa != "" && Informa != null) {
-          strUrl = strUrl + '&filter[][reports_to_id][$equals]=' + Informa;
-        }
-        $('#processing').show();
-        app.api.call("GET", app.api.buildURL(strUrl), null, {
-           success: _.bind(function (data) {
-              if(data.records.length > 0) {
-                agentes.listausuarios = [];
-                agentes.listausuarios_previo = [];
-                for(var i = 0; i < data.records.length; i++) {
-                   var actual = {
-                       "id": data.records[i].id,
-                       "reports_to_id": data.records[i].reports_to_id,
-                       "equipo_c": data.records[i].equipo_c,
-                       "nombre_completo_c": data.records[i].nombre_completo_c,
-                       "reports_to_name": data.records[i].reports_to_name
-                   };
-                   var previo = {
-                       "id": data.records[i].id,
-                       "reports_to_id": data.records[i].reports_to_id,
-                       "equipo_c": data.records[i].equipo_c,
-                       "nombre_completo_c": data.records[i].nombre_completo_c,
-                       "reports_to_name": data.records[i].reports_to_name
-                   };
-                   agentes.listausuarios.push(actual);
-                   agentes.listausuarios_previo.push(previo);
-                   agentes.filtros.Total = data.records.length;
+        if(AgenteN!="" || AgenteA !="" || equipoA!="0" || Informa!=undefined || Informa!=null || Informa!="") {
+          agentes.filtros = {
+              "Nombre": AgenteN,
+              "Apellidos": AgenteA,
+              "Equipo": equipoA,
+              "Informa": Informa
+          };
+          $('#successful').hide();
+          var strUrl = 'Users?fields=id,nombre_completo_c,puestousuario_c,reports_to_id,reports_to_name,equipo_c&max_num=-1&filter[][status]=Active&filter[1][$or][0][puestousuario_c]=27&filter[1][$or][1][puestousuario_c]=31&order_by=nombre_completo_c:asc';
+          if(AgenteN != "" && AgenteN != null) {
+            strUrl = strUrl + '&filter[][first_name][$contains]=' + AgenteN;
+          }
+          if(AgenteA != "" && AgenteA != null) {
+            strUrl = strUrl + '&filter[][last_name][$contains]=' + AgenteA;
+          }
+          if(equipoA != "0" && equipoA != null) {
+            strUrl = strUrl + '&filter[][equipo_c][$equals]=' + equipoA;
+          }
+          if(Informa != "" && Informa != null) {
+            strUrl = strUrl + '&filter[][reports_to_id][$equals]=' + Informa;
+          }
+          $('#processing').show();
+          app.api.call("GET", app.api.buildURL(strUrl), null, {
+             success: _.bind(function (data) {
+                if(data.records.length > 0) {
+                  agentes.listausuarios = [];
+                  agentes.listausuarios_previo = [];
+                  for(var i = 0; i < data.records.length; i++) {
+                     var actual = {
+                         "id": data.records[i].id,
+                         "reports_to_id": data.records[i].reports_to_id,
+                         "equipo_c": data.records[i].equipo_c,
+                         "nombre_completo_c": data.records[i].nombre_completo_c,
+                         "reports_to_name": data.records[i].reports_to_name
+                     };
+                     var previo = {
+                         "id": data.records[i].id,
+                         "reports_to_id": data.records[i].reports_to_id,
+                         "equipo_c": data.records[i].equipo_c,
+                         "nombre_completo_c": data.records[i].nombre_completo_c,
+                         "reports_to_name": data.records[i].reports_to_name
+                     };
+                     agentes.listausuarios.push(actual);
+                     agentes.listausuarios_previo.push(previo);
+                     agentes.filtros.Total = data.records.length;
+                  }
+                } else {
+                  agentes.listausuarios = [];
+                  agentes.listausuarios_previo = [];
+                  agentes.filtros.Total = 0;
                 }
-              } else {
-                agentes.listausuarios = [];
-                agentes.listausuarios_previo = [];
-                agentes.filtros.Total = 0;
-              }
-              $('#processing').hide();
-              agentes.render();
-              $('#btn_guardar').attr('style', 'pointer-events:none;');
-           }, this)
-        });
+                $('#processing').hide();
+                agentes.render();
+                $('#btn_guardar').attr('style', 'pointer-events:none;');
+             }, this)
+          });
+        }else{
+            app.alert.show("Campos faltantes para búsqueda", {
+                level: "error",
+                messages: 'Ingrese algún criterio de búsqueda',
+                autoClose: false
+            });
+        }
     },
 
     actualizaUsuarios: function(evt) {
