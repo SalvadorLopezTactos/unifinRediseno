9f4fd7d3cb72c71b715af92ae07308888ce493ae
erickdjcruz <47753132+erickdjcruz@users.noreply.github.com>
2020-02-04 13:21:45 -0600

    #148691_CP_OB055_PFAE_CP201
    Edición columna contactos en tareas

---
+++ custom/Extension/modules/Leads/Ext/clients/base/layouts/subpanels/_overridesubpanel-for-leads-tasks.php
@@ -0,0 +1,6 @@
+<?php
+//auto-generated file DO NOT EDIT
+$viewdefs['Leads']['base']['layout']['subpanels']['components'][]['override_subpanel_list_view'] = array (
+  'link' => 'tasks',
+  'view' => 'subpanel-for-leads-tasks',
+);
