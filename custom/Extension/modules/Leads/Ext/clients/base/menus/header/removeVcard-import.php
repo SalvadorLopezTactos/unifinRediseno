cbc963a1cb969738640cb536145c569648414e39
Javier García <javier.garcia@tactos.com.mx>
2020-02-06 12:55:11 -0600

    # 148691 Se realiza ajuste para quitar el boton crear desde vcard V2

--- custom/Extension/modules/Leads/Ext/clients/base/menus/header/removeVcard-import.php
+++ custom/Extension/modules/Leads/Ext/clients/base/menus/header/removeVcard-import.php
@@ -6,9 +6,11 @@
  * Time: 11:24 AM
  */
 
+if (isset($viewdefs['Leads']['base']['menu']['header'])) {
     foreach ($viewdefs['Leads']['base']['menu']['header'] as $key => $moduleAction) {
         //remove the link by label key
         if (in_array($moduleAction['label'], array('LNK_IMPORT_VCARD'))) {
             unset($viewdefs['Leads']['base']['menu']['header'][$key]);
         }
     }
+}
