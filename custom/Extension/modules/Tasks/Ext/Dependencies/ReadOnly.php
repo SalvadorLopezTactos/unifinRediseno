05815abf041ddccb59adbef46346532c9e2d3755
erickdjcruz <47753132+erickdjcruz@users.noreply.github.com>
2020-02-06 23:53:24 -0600

    #148691_CP_Check_Lista_Tasks
    Lista dinamica para status con ayuda CP

--- custom/Extension/modules/Tasks/Ext/Dependencies/ReadOnly.php
+++ custom/Extension/modules/Tasks/Ext/Dependencies/ReadOnly.php
@@ -28,6 +28,24 @@ $dependencies['Tasks']['readonly_fields'] = array
 	),
 );
 
+$dependencies['Tasks']['setoptions_status_ayuda_c'] = array(
+   'hooks' => array("all"),
+   'trigger' => 'true',
+   'triggerFields' => array('ayuda_asesor_cp_c','status'),
+   'onload' => true,
+   'actions' => array(
+     array(
+       'name' => 'SetOptions',
+       'params' => array(
+         'target' => 'status',
+         'keys' => 'ifElse(equal($ayuda_asesor_cp_c,"1"),getDropdownKeySet("task_status_cp_list"),getDropdownKeySet("task_status_dom"))',
+         'labels' => 'ifElse(equal($ayuda_asesor_cp_c,"1"),getDropdownKeySet("task_status_cp_list"),getDropdownValueSet("task_status_dom"))',
+       ),
+     ),
+   ),
+);
+
+
 
 //Dependencia para ocultar en Tareas la cuenta y así asignar una única a la relación Adrian Arauz 20/707/18
 /*$dependencies['Tasks']['TareasNo'] = array(
