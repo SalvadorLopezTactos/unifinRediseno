c5b318028a8156c468471c92503b4fa23fa93a50
Axel Flores <axel.flores@tactos.com.mx>
2020-02-11 17:12:34 -0600

    Ajuste Origen: Referenciado Unifin

--- custom/Extension/application/Ext/Language/es_LA.sugar_origendelprospecto_list.php
+++ custom/Extension/application/Ext/Language/es_LA.sugar_origendelprospecto_list.php
@@ -1,5 +1,5 @@
 <?php
- // created: 2020-02-11 11:54:04
+ // created: 2020-02-06 13:15:57
 
 $app_list_strings['origendelprospecto_list']=array (
   '' => '',
@@ -7,8 +7,10 @@ $app_list_strings['origendelprospecto_list']=array (
   'Referido Cliente' => 'Referenciado Cliente',
   'Referido Proveedor' => 'Referenciado Proveedor',
   'Referenciador' => 'Referenciado Socio Comercial',
-  'Referido Director' => 'Referenciado Unifin',
+  'Referido Director' => 'Referenciado Director',
   'Referenciado Vendor' => 'Referenciado Vendor',
   'Marketing' => 'Marketing',
   'Inteligencia de Negocio' => 'Inteligencia de Negocio',
+  'Portal Uniclick' => 'Portal Uniclick',
+  'Whatsapp' => 'Whatsapp',
 );
