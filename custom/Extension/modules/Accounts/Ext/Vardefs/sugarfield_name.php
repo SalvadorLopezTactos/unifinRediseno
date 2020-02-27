31582d0519fcb21dbfd9432b2eddfa4b3062de11
IRVING DELGADO <irving.delgado@tactos.com.mx>
2020-02-27 11:46:40 -0600

    Modificar validación de tipo de relación, al crear cuenta tipo persona.

--- custom/Extension/modules/Accounts/Ext/Vardefs/sugarfield_name.php
+++ custom/Extension/modules/Accounts/Ext/Vardefs/sugarfield_name.php
@@ -7,8 +7,7 @@ $dictionary['Account']['fields']['name']['duplicate_merge']='disabled';
 $dictionary['Account']['fields']['name']['duplicate_merge_dom_value']=0;
 $dictionary['Account']['fields']['name']['merge_filter']='disabled';
 $dictionary['Account']['fields']['name']['calculated']='1';
-$dictionary['Account']['fields']['name']['formula']='ifElse(equal($tipodepersona_c,"Persona Moral"),$razonsocial_c,
-concat($primernombre_c," ",$segundonombre_c,ifElse(equal($segundonombre_c,""),""," "),$apellidopaterno_c,ifElse(equal($apellidomaterno_c,""),""," "),$apellidomaterno_c))';
+$dictionary['Account']['fields']['name']['formula']='ifElse(equal($tipodepersona_c,"Persona Moral"),$razonsocial_c,concat($primernombre_c," ",$segundonombre_c,ifElse(equal($segundonombre_c,""),""," "),$apellidopaterno_c,ifElse(equal($apellidomaterno_c,""),""," "),$apellidomaterno_c))';
 $dictionary['Account']['fields']['name']['enforced']=true;
 $dictionary['Account']['fields']['name']['comments']='Name of the Company';
 $dictionary['Account']['fields']['name']['full_text_search']=array (
--- custom/Extension/modules/Opportunities/Ext/Vardefs/sugarfield_tct_competencia_porque_txf_c.php
+++ custom/Extension/modules/Opportunities/Ext/Vardefs/sugarfield_tct_competencia_porque_txf_c.php
@@ -5,8 +5,5 @@ $dictionary['Opportunity']['fields']['tct_competencia_porque_txf_c']['full_text_
 $dictionary['Opportunity']['fields']['tct_competencia_porque_txf_c']['full_text_search']['searchable'] = true;
 $dictionary['Opportunity']['fields']['tct_competencia_porque_txf_c']['full_text_search']['boost'] = 1;
 $dictionary['Opportunity']['fields']['tct_competencia_porque_txf_c']['enforced'] = '';
-$dictionary['Opportunity']['fields']['tct_competencia_porque_txf_c']['dependency'] = 'and(
-equal($tct_razon_op_perdida_ddw_c,"C"),
-equal($tct_oportunidad_perdida_chk_c,true)
-)';
+$dictionary['Opportunity']['fields']['tct_competencia_porque_txf_c']['dependency'] = 'and(equal($tct_razon_op_perdida_ddw_c,"C"),equal($tct_oportunidad_perdida_chk_c,true))';
 
