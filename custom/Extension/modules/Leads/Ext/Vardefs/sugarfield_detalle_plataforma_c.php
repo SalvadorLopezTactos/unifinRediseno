6c3d51ad17e72f99e90f4f84ac105bac569b02b7
Adrian Arauz <adrian.arauz@tactos.com.mx>
2020-02-27 17:29:08 -0600

    Nuevo campo GCLID - Ticket# 17830

---
+++ custom/Extension/modules/Leads/Ext/Vardefs/sugarfield_detalle_plataforma_c.php
@@ -0,0 +1,12 @@
+<?php
+ // created: 2020-02-27 17:08:26
+$dictionary['Lead']['fields']['detalle_plataforma_c']['labelValue']='GCLID';
+$dictionary['Lead']['fields']['detalle_plataforma_c']['full_text_search']=array (
+  'enabled' => '0',
+  'boost' => '1',
+  'searchable' => false,
+);
+$dictionary['Lead']['fields']['detalle_plataforma_c']['enforced']='';
+$dictionary['Lead']['fields']['detalle_plataforma_c']['dependency']='';
+
+ ?>
