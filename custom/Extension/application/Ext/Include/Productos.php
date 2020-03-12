d7b3e765e337d57903ac0c26f19da4c4d0a94f42
IRVING DELGADO <irving.delgado@tactos.com.mx>
2020-03-12 12:15:40 -0600

    Nuevo Modulo Productos

---
+++ custom/Extension/application/Ext/Include/Productos.php
@@ -0,0 +1,7 @@
+<?php 
+ //WARNING: The contents of this file are auto-generated
+$beanList['uni_Productos'] = 'uni_Productos';
+$beanFiles['uni_Productos'] = 'modules/uni_Productos/uni_Productos.php';
+$moduleList[] = 'uni_Productos';
+
+?>
---
+++ custom/Extension/modules/Accounts/Ext/Vardefs/accounts_uni_productos_1_Accounts.php
@@ -0,0 +1,14 @@
+<?php
+// created: 2020-03-12 15:55:02
+$dictionary["Account"]["fields"]["accounts_uni_productos_1"] = array (
+  'name' => 'accounts_uni_productos_1',
+  'type' => 'link',
+  'relationship' => 'accounts_uni_productos_1',
+  'source' => 'non-db',
+  'module' => 'uni_Productos',
+  'bean_name' => 'uni_Productos',
+  'vname' => 'LBL_ACCOUNTS_UNI_PRODUCTOS_1_FROM_ACCOUNTS_TITLE',
+  'id_name' => 'accounts_uni_productos_1accounts_ida',
+  'link-type' => 'many',
+  'side' => 'left',
+);
