a417aa25125c19ec046417b65a46ea2b070a4243
IRVING DELGADO <irving.delgado@tactos.com.mx>
2020-03-12 13:12:27 -0600

    Lista desplegable es_LA.sugar_fuera_de_perfil_ddw_list.php de No viable

---
+++ custom/Extension/application/Ext/Language/es_LA.sugar_fuera_de_perfil_ddw_list.php
@@ -0,0 +1,15 @@
+<?php
+
+$app_list_strings['fuera_de_perfil_ddw_list']=array (
+  '' => '',
+  '0' => '',
+  '1' => 'Por Ventas',
+  '2' => 'No maneja activo fijo',
+  '3' => 'No tiene dos años  operando',
+  '4' => 'Broker / Vendor',
+  '5' => 'Giro no acreditable',
+  '6' => 'Ya está sobre apalancado',
+  '7' => 'Quiebra Técnica',
+  '8' => 'Pérdidas',
+  '9' => 'Mal buro de crédito',
+);
