02c4037ae3a1b149f6342d214660afed513e1863
Eduardo Carrasco <eduardo.carrasco@tactos.com.mx>
2020-03-13 13:12:14 -0600

    Productos

--- custom/Extension/application/Ext/Language/es_LA.sugar_fuera_de_perfil_ddw_list.php
+++ custom/Extension/application/Ext/Language/es_LA.sugar_fuera_de_perfil_ddw_list.php
@@ -5,11 +5,7 @@ $app_list_strings['fuera_de_perfil_ddw_list']=array (
   '0' => '',
   '1' => 'Por Ventas',
   '2' => 'No maneja activo fijo',
-<<<<<<< HEAD
-  '3' => 'No tiene dos años operando',
-=======
   '3' => 'No tiene dos años  operando',
->>>>>>> 3d22fa77937e44b87676a7137c8af58c68be2b34
   '4' => 'Broker / Vendor',
   '5' => 'Giro no acreditable',
   '6' => 'Ya está sobre apalancado',
