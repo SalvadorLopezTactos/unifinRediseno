cff2b3922f0e3ea7b3dae3c21dec27655041bbc3
Salvador Lopez <salvador.lopez@enterconecta.com>
2020-03-16 17:00:06 -0600

    Borrando archivos innecesarios

--- custom/Extension/modules/Accounts/Ext/Language/es_ES.recordsAssignedToMe.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: salvadorlopez
- * Date: 06/03/20
- * Time: 10:30
- */
-$mod_strings['LBL_FILTER_RECORDS_ASSIGNER_TO_ME'] = 'Mis registros asignados cstm';
--- custom/Extension/modules/Accounts/Ext/clients/mobile/filters/default/default.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: salvadorlopez
- * Date: 06/03/20
- * Time: 10:02
- */
