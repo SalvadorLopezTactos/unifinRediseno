7721f4a0fa1245fe6458485de38508f68d209102
Adrian Arauz <adrian.arauz@tactos.com.mx>
2020-03-24 17:05:21 -0600

    Ajuste Actualizacion lista valores Portal Analizate - Ticket #10334

--- custom/Extension/application/Ext/Language/es_LA.sugar_analizate_url_list.php
+++ custom/Extension/application/Ext/Language/es_LA.sugar_analizate_url_list.php
@@ -1,5 +1,5 @@
 <?php
- // created: 2020-03-24 15:10:56
+ // created: 2020-03-24 17:02:28
 
 $app_list_strings['analizate_url_list']=array (
   1 => 'https://ciecv3-desarrollo.azurewebsites.net/Ciec/Registro?data=lEyQn0XsXscPVuCW/JcTehEej3s74Bsme/joJz6BuO7+f5WhP13Zd/vOzgl62rUIJtoC6FxDr+fcvfmNsFm64xiHjbtCkVNWgPGIqcDqQKqmBm2p6wQAihABdatSPIyDXRBl2ulo8n/JwspwDZVSiGa0bYZEkJrTQ5zUpUmaAJ7JZBYDGQcUrXFvyTuOZckuJz8zQ41NFAe12Joa/3eseuXMi0XP6mVD',
