d05d86f67f1bc535be502b6ff8bc60425d77b5d3
Adrian Arauz <adrian.arauz@tactos.com.mx>
2020-03-25 18:09:25 -0600

    Ajuste2 Auditar Informa A - Ticket #21615

--- custom/clients/base/api/AgentesTelefonicos.php
+++ custom/clients/base/api/AgentesTelefonicos.php
@@ -29,11 +29,11 @@ class AgentesTelefonicos extends SugarApi
         //Traer el bean de users_audit
         $bean_Audit = BeanFactory::getBean('Users',$user_id);
         $audit_last=$bean_Audit->reports_to_id;
-        $GLOBALS['log']->fatal('>>>>Trae Bean de users_audit<<<<<<<');
+        //$GLOBALS['log']->fatal('>>>>Trae Bean de users_audit<<<<<<<');
         //Condicion para insertar registro, si el reports_to_id actual no es igual al del registro
 
         if ($reports_to_id != $audit_last){
-            $GLOBALS['log']->fatal('>>>>Genera Insert en users_audit<<<<<<<');
+            //$GLOBALS['log']->fatal('>>>>Genera Insert en users_audit<<<<<<<');
             $sqlInsert="insert into users_audit (id, parent_id, date_created, created_by, field_name, data_type, before_value_string, after_value_string, before_value_text, after_value_text, event_id, date_updated)
                   VALUES ('{$id_u_audit}', '{$user_id}', '{$date}', '{$current_user->id}', 'reports_to_id', 'id', '{$audit_last}', '{$reports_to_id}', '', '', '1', '{$date}')";
             $GLOBALS['db']->query($sqlInsert);
@@ -43,10 +43,6 @@ class AgentesTelefonicos extends SugarApi
         $query = "update users a, users_cstm b set a.reports_to_id = '{$reports_to_id}', b.equipo_c = '{$equipo_c}' where a.id = b.id_c and a.id = '{$user_id}'";
         $result = $db->query($query);
 		    return $result;
-
-        $GLOBALS['log']->fatal('Realiza Update en users_cstm');
-
-
-
+        //$GLOBALS['log']->fatal('Realiza Update en users_cstm');
     }
 }
