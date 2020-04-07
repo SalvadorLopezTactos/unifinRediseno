71cfe336cd316348078d111e78a1186d998b12b2
Eduardo Carrasco <eduardo.carrasco@tactos.com.mx>
2020-04-07 12:18:47 -0500

    23280-Campos Inbound

---
+++ custom/Extension/modules/Leads/Ext/Vardefs/sugarfield_campana_c.php
@@ -0,0 +1,12 @@
+<?php
+ // created: 2020-04-06 14:58:17
+$dictionary['Lead']['fields']['campana_c']['labelValue']='Campaña';
+$dictionary['Lead']['fields']['campana_c']['full_text_search']=array (
+  'enabled' => '0',
+  'boost' => '1',
+  'searchable' => false,
+);
+$dictionary['Lead']['fields']['campana_c']['enforced']='';
+$dictionary['Lead']['fields']['campana_c']['dependency']='';
+
+ ?>
---
+++ custom/Extension/modules/Leads/Ext/Vardefs/sugarfield_id_landing_c.php
@@ -0,0 +1,12 @@
+<?php
+ // created: 2020-04-06 12:49:07
+$dictionary['Lead']['fields']['id_landing_c']['labelValue']='ID Landing';
+$dictionary['Lead']['fields']['id_landing_c']['full_text_search']=array (
+  'enabled' => '0',
+  'boost' => '1',
+  'searchable' => false,
+);
+$dictionary['Lead']['fields']['id_landing_c']['enforced']='';
+$dictionary['Lead']['fields']['id_landing_c']['dependency']='';
+
+ ?>
