9a04b946039656681ed035554c3097e3bf8a47f4
Eduardo Carrasco <eduardo.carrasco@tactos.com.mx>
2020-04-08 10:48:53 -0500

    10651-Fecha Primer Ingreso

---
+++ custom/Extension/modules/Opportunities/Ext/Vardefs/sugarfield_fecha_ingreso_estudio_c.php
@@ -0,0 +1,7 @@
+<?php
+ // created: 2020-04-08 10:15:35
+$dictionary['Opportunity']['fields']['fecha_ingreso_estudio_c']['labelValue']='Fecha de Primer Ingreso Estudio';
+$dictionary['Opportunity']['fields']['fecha_ingreso_estudio_c']['enforced']='';
+$dictionary['Opportunity']['fields']['fecha_ingreso_estudio_c']['dependency']='';
+
+ ?>
