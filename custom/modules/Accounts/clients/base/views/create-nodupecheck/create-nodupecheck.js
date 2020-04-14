a42fc7308df4b9a0de603bf6cf20c565fa20cf7d
erickdjcruz <47753132+erickdjcruz@users.noreply.github.com>
2020-04-14 17:11:21 -0500

    Ticket #10650 - Existencia dominio - 2020/04/14
    Validaciones de javascript para la validación de existencia de una página web

--- custom/modules/Accounts/clients/base/views/create-nodupecheck/create-nodupecheck.js
+++ custom/modules/Accounts/clients/base/views/create-nodupecheck/create-nodupecheck.js
@@ -1869,14 +1869,42 @@
 
             if (!expreg.test(webSite)) {
 
-                app.alert.show('error-web-site', {
+                app.alert.show('error-website', {
                     level: 'error',
                     autoClose: false,
                     messages: "El formato de <b>Página Web</b> no es valido."
                 });
                 errors['website'] = errors['website'] || {};
                 errors['website'].required = true;
-            }
+
+            }else{
+				app.api.call('GET', app.api.buildURL('validacion_sitio_web/?website=' +webSite) ,null, {
+					success: _.bind(function (data) {
+						//console.log(data);
+						if (data == "02") {
+							app.alert.show("error-website", {
+								level: "error",
+								autoClose: false,
+								messages: "La <b>Página Web</b> no existe."
+							});
+							errors['website'] = errors['website'] || {};
+							errors['website'].required = true;
+							//callback(null, fields, errors);
+						}
+						if (data == "01" ) {
+							app.alert.show("error-website", {
+								level: "error",
+								autoClose: false,
+								messages: "La <b>Página Web</b> no existe o no esta activa."
+							});
+							errors['website'] = errors['website'] || {};
+							errors['website'].required = true;
+							//callback(null, fields, errors);
+						}
+						callback(null, fields, errors);
+					}, this),
+				});
+			}
         }
         callback(null, fields, errors);
     },
