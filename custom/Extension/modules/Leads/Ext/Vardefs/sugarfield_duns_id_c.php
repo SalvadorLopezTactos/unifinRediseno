819b70096fe93b2d290c3e87f7e92abcb48eda65
IRVING DELGADO <irving.delgado@tactos.com.mx>
2020-04-17 14:42:14 -0500

    Ticket No Viable: #14040, CP-637 - Ajuste de account_uni_productos js error de dedo

---
+++ custom/Extension/modules/Leads/Ext/Vardefs/sugarfield_duns_id_c.php
@@ -0,0 +1,12 @@
+<?php
+ // created: 2020-04-17 11:42:16
+$dictionary['Lead']['fields']['duns_id_c']['labelValue']='DUNS ID';
+$dictionary['Lead']['fields']['duns_id_c']['full_text_search']=array (
+  'enabled' => '0',
+  'boost' => '1',
+  'searchable' => false,
+);
+$dictionary['Lead']['fields']['duns_id_c']['enforced']='';
+$dictionary['Lead']['fields']['duns_id_c']['dependency']='';
+
+ ?>
