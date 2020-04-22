4e7c95719e08e1d84bb22d07d3a859269386d5ff
Axel Flores <axel.flores@tactos.com.mx>
2020-04-22 14:31:08 -0500

    Chattigo Ticket #155151 , CP-492 - renombra directorio clients

---
+++ custom/Extension/modules/C5515_uni_chattigo/Ext/clients/base/views/panel-top/panel-top.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by Tactos.
+ * User: EJC
+ */
+ 
+$viewdefs['C5515_uni_chattigo']['base']['view']['panel-top']['buttons'] = array(
+    array(
+        'type' => 'actiondropdown',
+        'name' => 'panel_dropdown',
+        'css_class' => 'pull-right',
+        
+    ),
+);
