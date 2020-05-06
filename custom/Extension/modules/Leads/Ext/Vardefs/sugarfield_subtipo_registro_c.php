6bc4e487d6de955199c18510513665593c762fd1
Adrian Arauz <adrian.arauz@tactos.com.mx>
2020-05-05 20:48:30 -0500

    listas - Leads - Cambio catálogos numéricos - Ticket #10792, CP-841

--- custom/Extension/modules/Leads/Ext/Vardefs/sugarfield_subtipo_registro_c.php
+++ custom/Extension/modules/Leads/Ext/Vardefs/sugarfield_subtipo_registro_c.php
@@ -1,7 +1,33 @@
 <?php
- // created: 2020-01-02 23:41:12
+ // created: 2020-05-05 20:33:00
 $dictionary['Lead']['fields']['subtipo_registro_c']['labelValue']='Subtipo de Lead';
-$dictionary['Lead']['fields']['subtipo_registro_c']['dependency']='';
-$dictionary['Lead']['fields']['subtipo_registro_c']['visibility_grid']='';
+$dictionary['Lead']['fields']['subtipo_registro_c']['visibility_grid']=array (
+  'trigger' => 'tipo_registro_c',
+  'values' => 
+  array (
+    1 => 
+    array (
+      0 => '1',
+      1 => '2',
+      2 => '3',
+      3 => '4',
+    ),
+    2 => 
+    array (
+    ),
+    3 => 
+    array (
+    ),
+    4 => 
+    array (
+    ),
+    5 => 
+    array (
+    ),
+    '' => 
+    array (
+    ),
+  ),
+);
 
  ?>
--- custom/Extension/modules/Leads/Ext/Vardefs/sugarfield_tipo_subtipo_registro_c.php
+++ custom/Extension/modules/Leads/Ext/Vardefs/sugarfield_tipo_subtipo_registro_c.php
@@ -1,5 +1,5 @@
 <?php
- // created: 2019-12-19 13:02:01
+ // created: 2020-05-05 20:39:37
 $dictionary['Lead']['fields']['tipo_subtipo_registro_c']['duplicate_merge_dom_value']=0;
 $dictionary['Lead']['fields']['tipo_subtipo_registro_c']['labelValue']='Tipo y Subtipo de Lead';
 $dictionary['Lead']['fields']['tipo_subtipo_registro_c']['full_text_search']=array (
@@ -7,9 +7,9 @@ $dictionary['Lead']['fields']['tipo_subtipo_registro_c']['full_text_search']=arr
   'boost' => '1',
   'searchable' => false,
 );
-$dictionary['Lead']['fields']['tipo_subtipo_registro_c']['calculated']='true';
-$dictionary['Lead']['fields']['tipo_subtipo_registro_c']['formula']='ifElse(	equal($tipo_registro_c,"Persona"),	"PERSONA",	ifElse(		equal($tipo_registro_c,"Proveedor"),		"PROVEEDOR",		strToUpper(concat(getDropdownValue("tipo_registro_list",$tipo_registro_c)," ",getDropdownValue("subtipo_cuenta_list",$subtipo_registro_c)))	))';
-$dictionary['Lead']['fields']['tipo_subtipo_registro_c']['enforced']='true';
+$dictionary['Lead']['fields']['tipo_subtipo_registro_c']['calculated']='1';
+$dictionary['Lead']['fields']['tipo_subtipo_registro_c']['formula']='ifElse(	equal($tipo_registro_c,"Persona"),	"PERSONA",	ifElse(		equal($tipo_registro_c,"Proveedor"),		"PROVEEDOR",		strToUpper(concat(getDropdownValue("tipo_registro_cuenta_list",$tipo_registro_c)," ",getDropdownValue("subtipo_registro_cuenta_list",$subtipo_registro_c)))	))';
+$dictionary['Lead']['fields']['tipo_subtipo_registro_c']['enforced']='1';
 $dictionary['Lead']['fields']['tipo_subtipo_registro_c']['dependency']='';
 
  ?>
