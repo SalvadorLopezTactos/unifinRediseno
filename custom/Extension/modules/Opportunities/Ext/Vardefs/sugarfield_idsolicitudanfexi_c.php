f720617efbb6ffb238ffc3cc1d70b490707c0905
javierSolar <javier@garcia.com.mx>
2020-06-02 11:16:57 -0500

    #11003, CP-986 Mambu Integración Anfexi Creación campo solicitud de Anfexi

---
+++ custom/Extension/modules/Opportunities/Ext/Vardefs/sugarfield_idsolicitudanfexi_c.php
@@ -0,0 +1,12 @@
+<?php
+ // created: 2020-06-02 11:03:13
+$dictionary['Opportunity']['fields']['idsolicitudanfexi_c']['labelValue']='Solicitud de Anfexi';
+$dictionary['Opportunity']['fields']['idsolicitudanfexi_c']['full_text_search']=array (
+  'enabled' => '0',
+  'boost' => '1',
+  'searchable' => false,
+);
+$dictionary['Opportunity']['fields']['idsolicitudanfexi_c']['enforced']='';
+$dictionary['Opportunity']['fields']['idsolicitudanfexi_c']['dependency']='';
+
+ ?>
