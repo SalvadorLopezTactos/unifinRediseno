958c1ab8f2b6ffbdd8d8fafd55a4b1756a78c0a2
IRVING DELGADO <irving.delgado@tactos.com.mx>
2020-06-03 14:53:34 -0500

    Ticket#11003,CP-986-CRM SP3-Ajuste Recuperación del asesor Uniclick

--- custom/modules/Opportunities/opp_anfexi_class.php
+++ custom/modules/Opportunities/opp_anfexi_class.php
@@ -4,12 +4,12 @@ class anfexi_class
 {
     function anfexi_function($bean = null, $event = null, $args = null)
     {        
-        /**********Funcion para asignar asesor uniclick de cuenta, cuando la solicitud no tenga asesor**********/
-        $beanCuenta = BeanFactory::retrieveBean('Accounts', $bean->account_id, array('disable_row_level_security' => true));
-        
-        if ($bean->assigned_user_id == '' || $bean->assigned_user_id == null){
-            // $GLOBALS['log']->fatal("Asigna Asesor Uniclick en la Solicitud: ". $beanCuenta->user_id7_c);
+        /**********Funcion para asignar asesor uniclick de cuenta, cuando la solicitud no tenga asesor y sea tipo producto uniclick**********/
+        if ($bean->tipo_producto_c == '8' && ($bean->assigned_user_id == '' || $bean->assigned_user_id == null)){
+            
+            $beanCuenta = BeanFactory::retrieveBean('Accounts', $bean->account_id, array('disable_row_level_security' => true));
             $bean->assigned_user_id = $beanCuenta->user_id7_c; //Se asigna al Asesor Uniclick en la Solicitud
+            // $GLOBALS['log']->fatal("Asigna Asesor Uniclick en la Solicitud: ". $beanCuenta->user_id7_c);
             
         }
     }
