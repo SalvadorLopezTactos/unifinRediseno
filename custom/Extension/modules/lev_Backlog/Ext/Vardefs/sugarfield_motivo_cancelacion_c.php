c69376e95946c870cddc567d679f5c3a0910a804
Eduardo Carrasco <eduardo.carrasco@tactos.com.mx>
2020-06-22 12:12:56 -0500

    11112-Backlog Nuevas Listas

--- custom/Extension/modules/lev_Backlog/Ext/Vardefs/sugarfield_motivo_cancelacion_c.php
+++ custom/Extension/modules/lev_Backlog/Ext/Vardefs/sugarfield_motivo_cancelacion_c.php
@@ -1,7 +1,7 @@
 <?php
- // created: 2020-06-19 08:31:27
+ // created: 2020-06-22 11:09:03
 $dictionary['lev_Backlog']['fields']['motivo_cancelacion_c']['labelValue']='Motivo de cancelación';
-$dictionary['lev_Backlog']['fields']['motivo_cancelacion_c']['dependency']='';
+$dictionary['lev_Backlog']['fields']['motivo_cancelacion_c']['dependency']='or(equal($estatus_operacion_c,"1"),equal($estatus_operacion_c,"Cancelada por cliente"))';
 $dictionary['lev_Backlog']['fields']['motivo_cancelacion_c']['visibility_grid']='';
 
  ?>
