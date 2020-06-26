a3f1e9022e31f8f50bd5b664f286b1d2cb03466a
devai92 <irving.delgado@tactos.com.mx>
2020-06-26 11:59:21 -0500

    Ticket#11138, CP-1058 - Ajuste en Agente Tel para Empleados

--- custom/Extension/modules/Accounts/Ext/Vardefs/sugarfield_tct_origen_ag_tel_rel_c.php
+++ custom/Extension/modules/Accounts/Ext/Vardefs/sugarfield_tct_origen_ag_tel_rel_c.php
@@ -1,5 +1,13 @@
 <?php
  // created: 2020-05-26 10:15:53
-$dictionary['Account']['fields']['tct_origen_ag_tel_rel_c']['labelValue']='Agente Telefónico';
-$dictionary['Account']['fields']['tct_origen_ag_tel_rel_c']['dependency']='or(equal($detalle_origen_c,"2"),equal($detalle_origen_c,"8"),equal($detalle_origen_c,"6"),equal($detalle_origen_c,"5"),equal($detalle_origen_c,"4"),equal($detalle_origen_c,"3"),equal($detalle_origen_c,"9"),equal($detalle_origen_c,"1"),equal($detalle_origen_c,"10"))';
+// $dictionary['Account']['fields']['tct_origen_ag_tel_rel_c']['labelValue']='Agente Telefónico';
+// $dictionary['Account']['fields']['tct_origen_ag_tel_rel_c']['dependency']='or(equal($detalle_origen_c,"2"),equal($detalle_origen_c,"8"),equal($detalle_origen_c,"6"),equal($detalle_origen_c,"5"),equal($detalle_origen_c,"4"),equal($detalle_origen_c,"3"),equal($detalle_origen_c,"9"),equal($detalle_origen_c,"1"),equal($detalle_origen_c,"10"))';
+
+$dictionary['Account']['fields']['tct_origen_ag_tel_rel_c'] = array (
+    'labelValue' => 'Agente Telefónico',
+    'dependency' => 'or(equal($detalle_origen_c,"2"),equal($detalle_origen_c,"8"),equal($detalle_origen_c,"6"),equal($detalle_origen_c,"5"),equal($detalle_origen_c,"4"),equal($detalle_origen_c,"3"),equal($detalle_origen_c,"9"),equal($detalle_origen_c,"1"),equal($detalle_origen_c,"10"))',
+    'table' => 'users',
+    'module' => 'Employees'
+);
+
 ?>
