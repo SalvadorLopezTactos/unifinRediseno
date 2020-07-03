e8317e303334a2e6c845e385201cbeb70c827488
Eduardo Carrasco <eduardo.carrasco@tactos.com.mx>
2020-07-03 08:54:53 -0500

    11055-Seguros

---
+++ custom/modules/S_seguros/clients/base/views/list/list.php
@@ -0,0 +1,97 @@
+<?php
+$module_name = 'S_seguros';
+$viewdefs[$module_name] = 
+array (
+  'base' => 
+  array (
+    'view' => 
+    array (
+      'list' => 
+      array (
+        'panels' => 
+        array (
+          0 => 
+          array (
+            'label' => 'LBL_PANEL_1',
+            'fields' => 
+            array (
+              0 => 
+              array (
+                'name' => 'name',
+                'label' => 'LBL_NAME',
+                'default' => true,
+                'enabled' => true,
+                'link' => true,
+              ),
+              1 => 
+              array (
+                'name' => 'etapa',
+                'label' => 'LBL_ETAPA',
+                'enabled' => true,
+                'default' => true,
+              ),
+              2 => 
+              array (
+                'name' => 'prima_obj',
+                'label' => 'LBL_PRIMA_OBJ',
+                'enabled' => true,
+                'related_fields' => 
+                array (
+                  0 => 'currency_id',
+                  1 => 'base_rate',
+                ),
+                'currency_format' => true,
+                'default' => true,
+              ),
+              3 => 
+              array (
+                'name' => 'prima_neta',
+                'label' => 'LBL_PRIMA_NETA',
+                'enabled' => true,
+                'related_fields' => 
+                array (
+                  0 => 'currency_id',
+                  1 => 'base_rate',
+                ),
+                'currency_format' => true,
+                'default' => true,
+              ),
+              4 => 
+              array (
+                'name' => 'assigned_user_name',
+                'label' => 'LBL_ASSIGNED_TO_NAME',
+                'default' => true,
+                'enabled' => true,
+                'link' => true,
+              ),
+              5 => 
+              array (
+                'name' => 'date_entered',
+                'enabled' => true,
+                'default' => true,
+              ),
+              6 => 
+              array (
+                'name' => 'date_modified',
+                'enabled' => true,
+                'default' => true,
+              ),
+              7 => 
+              array (
+                'name' => 'team_name',
+                'label' => 'LBL_TEAM',
+                'default' => false,
+                'enabled' => true,
+              ),
+            ),
+          ),
+        ),
+        'orderBy' => 
+        array (
+          'field' => 'date_modified',
+          'direction' => 'desc',
+        ),
+      ),
+    ),
+  ),
+);
