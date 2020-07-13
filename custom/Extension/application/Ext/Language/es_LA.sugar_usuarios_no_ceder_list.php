4e0c23998ec6114d9c7465f75718eee018172a60
erickdjcruz <47753132+erickdjcruz@users.noreply.github.com>
2020-07-13 14:31:14 -0500

    #11296 - Reasignar Cuentas CP - 13/07/2020
    lista de valores- cambio

--- custom/Extension/application/Ext/Language/es_LA.sugar_usuarios_no_ceder_list.php
+++ custom/Extension/application/Ext/Language/es_LA.sugar_usuarios_no_ceder_list.php
@@ -5,7 +5,7 @@ $app_list_strings['usuarios_no_ceder_list']=array (
   1 => '405cc6b7-fc4a-7cae-552f-5628f61fd849',
   2 => '36af9462-37e6-11ea-baed-a44e314beb18',
   3 => '569246c7-da62-4664-ef2a-5628f649537e',
-  4 => 'Perdido',
+  4 => '28f5b8b8-ab06-6bfd-dc85-5628f6e9f411',
   5 => '42ee17c4-f67a-11e9-9711-00155d96730d',
-  6 => 'cc736f7a-4f5f-11e9-856a-a0481cdf89ebNo Viable',
+  6 => 'cc736f7a-4f5f-11e9-856a-a0481cdf89eb',
 );
