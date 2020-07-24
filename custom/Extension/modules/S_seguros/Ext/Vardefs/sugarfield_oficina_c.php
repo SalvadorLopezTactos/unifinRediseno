ef492f0acfe133a41b2516eef1c5c7682ebd87ca
arauz07 <adrian.arauz@tactos.com.mx>
2020-07-24 10:38:14 -0500

    Detalle vardef oficina Seguros

--- custom/Extension/modules/S_seguros/Ext/Vardefs/sugarfield_oficina_c.php
+++ custom/Extension/modules/S_seguros/Ext/Vardefs/sugarfield_oficina_c.php
@@ -1,5 +1,5 @@
 <?php
- // created: 2020-07-20 14:34:22
+ // created: 2020-07-21 10:48:28
 $dictionary['S_seguros']['fields']['oficina_c']['labelValue']='Oficina';
 $dictionary['S_seguros']['fields']['oficina_c']['dependency']='';
 $dictionary['S_seguros']['fields']['oficina_c']['visibility_grid']='';
