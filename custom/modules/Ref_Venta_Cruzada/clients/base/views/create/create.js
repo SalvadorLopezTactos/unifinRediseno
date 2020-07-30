70e676df19dc445069e9849164bf4300af4c9aee
erickdjcruz <47753132+erickdjcruz@users.noreply.github.com>
2020-07-30 14:32:50 -0500

    #32400_Venta Cruzada_30-07-2020_2
    VAlidacion, producto ya referenciado

--- custom/modules/Ref_Venta_Cruzada/clients/base/views/create/create.js
+++ custom/modules/Ref_Venta_Cruzada/clients/base/views/create/create.js
@@ -5,7 +5,6 @@
         self = this;
         this._super("initialize", [options]);
 		this.model.addValidationTask('producto_repetido', _.bind(this.producto_repetido, this));
-        
     },
 	
 	_renderHtml: function(){      
@@ -31,39 +30,35 @@
         app.alert.dismiss('Error_validacion_Campos');
         var sameproductref = 0;
         
-		app.api.call("read", app.api.buildURL("Accounts/" + this.model.get('accounts_ref_venta_cruzada_1accounts_ida') + "/link/accounts_ref_venta_cruzada_1", null, null,null), null, {
+		app.api.call("read", app.api.buildURL("Accounts/" + this.model.get('accounts_ref_venta_cruzada_1accounts_ida') + "/link/accounts_ref_venta_cruzada_1", null, null,
+			null), null, {
             success: _.bind(function (data) {
 				if (data.records.length > 0) {
                     for(var i=0;i<data.records.length;i++){
                         //Validar si es activa
                         if(data.records[i].estatus=='1' && data.records[i].producto_referenciado == this.model.get('producto_referenciado')){
-                            //errors['account_ref_valida'] = errors['account_ref_valida'] || {};
+                            errors['account_ref_valida'] = errors['account_ref_valida'] || {};
 							sameproductref = sameproductref+1;
-							app.alert.show("Error_validacion_Referencias", {
+							
+                        }
+					}
+					if(sameproductref > 0){
+						app.alert.show("Error_validacion_Referencias", {
 							   level: "error",
 							   messages: 'El producto seleccionado, tiene una referencia activa',
 							   autoClose: false
 							});
-							errors['account_ref_valida'] = errors['account_ref_valida'] || {};
-							errors['producto_referenciado'] = errors['producto_referenciado'] || {};
-							errors['producto_referenciado'].required = true;
-                        }
+						errors['account_ref_valida'] = errors['account_ref_valida'] || {};
+						errors['producto_referenciado'] = errors['producto_referenciado'] || {};
+						errors['producto_referenciado'].required = true;
 					}
                 }
+				callback(null,fields,errors);
 			}, this)
         });
 		
-		//if(sameproductref > 0){
-		//	app.alert.show("Error_validacion_Referencias", {
-        //        level: "error",
-        //        messages: 'El producto seleccionado, tiene una referencia activa',
-        //        autoClose: false
-        //    });
-		//	errors['producto_referenciado'] = errors['producto_referenciado'] || {};
-		//	errors['producto_referenciado'].required = true;
-		//}
-        
-        callback(null, fields, errors);
+		//callback(null, fields, errors);
+		
     },
 
 })
