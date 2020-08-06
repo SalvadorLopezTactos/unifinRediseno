11e98127aab58f2c16c1cd78aed3b6b5c1cb1abe
Axel Flores <axel.flores@tactos.com.mx>
2020-08-06 18:16:36 -0500

    Ticket#33893, CP-1190 - Modificación WF Notificaciones

--- custom/modules/TCT2_Notificaciones/workflow/actions_array.php
+++ custom/modules/TCT2_Notificaciones/workflow/actions_array.php
@@ -2,6 +2,29 @@
 //Workflow Action Meta Data Arrays 
 $action_meta_array = array ( 
 
+'TCT2_Notificaciones1_action0' => 
+
+array ( 
+
+		 'action_type' => 'update', 
+		 'action_module' => '', 
+		 'rel_module' => '', 
+		 'rel_module_type' => 'all', 
+	 'basic' => array ( 
+
+		 'description' => stripslashes('ENTRO EL JOB DESDE REASIGNACION'),
+	 ), 
+
+	 'basic_ext' => array ( 
+
+	 ), 
+
+	 'advanced' => array ( 
+
+	 ), 
+
+), 
+
 ); 
 
  
