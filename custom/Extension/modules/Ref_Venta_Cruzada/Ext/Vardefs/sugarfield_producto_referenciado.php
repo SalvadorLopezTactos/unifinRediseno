1faae535c21b2dda8ee4a6a03e83886ba354e6d2
erickdjcruz <47753132+erickdjcruz@users.noreply.github.com>
2020-08-25 19:28:56 -0500

    #32400_Venta Cruzada_25-08-2020

--- custom/Extension/modules/Ref_Venta_Cruzada/Ext/Vardefs/sugarfield_producto_referenciado.php
+++ custom/Extension/modules/Ref_Venta_Cruzada/Ext/Vardefs/sugarfield_producto_referenciado.php
@@ -1,6 +1,6 @@
 <?php
- // created: 2020-08-12 12:41:14
-$dictionary['Ref_Venta_Cruzada']['fields']['producto_referenciado']['default']='1';
+ // created: 2020-08-25 11:32:40
+$dictionary['Ref_Venta_Cruzada']['fields']['producto_referenciado']['default']='';
 $dictionary['Ref_Venta_Cruzada']['fields']['producto_referenciado']['options']='producto_referenciado_list';
 
  ?>
