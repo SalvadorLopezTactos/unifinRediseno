be5c69f938b92659b6d247de6a64f1079cf73799
erickdjcruz <47753132+erickdjcruz@users.noreply.github.com>
2020-08-28 12:08:54 -0500

    #1359_Venta Cruzada_28-08-2020

--- custom/Extension/modules/Ref_Venta_Cruzada/Ext/Vardefs/sugarfield_producto_referenciado.php
+++ custom/Extension/modules/Ref_Venta_Cruzada/Ext/Vardefs/sugarfield_producto_referenciado.php
@@ -1,6 +1,67 @@
 <?php
- // created: 2020-08-25 11:32:40
-$dictionary['Ref_Venta_Cruzada']['fields']['producto_referenciado']['default']='';
+ // created: 2020-08-28 11:03:37
+$dictionary['Ref_Venta_Cruzada']['fields']['producto_referenciado']['default']='1';
 $dictionary['Ref_Venta_Cruzada']['fields']['producto_referenciado']['options']='producto_referenciado_list';
+$dictionary['Ref_Venta_Cruzada']['fields']['producto_referenciado']['visibility_grid']=array (
+  'trigger' => 'producto_origen',
+  'values' => 
+  array (
+    1 => 
+    array (
+      0 => '2',
+      1 => '3',
+      2 => '4',
+      3 => '7',
+    ),
+    2 => 
+    array (
+      0 => '1',
+      1 => '3',
+      2 => '4',
+      3 => '7',
+    ),
+    3 => 
+    array (
+      0 => '1',
+      1 => '2',
+      2 => '4',
+      3 => '7',
+    ),
+    4 => 
+    array (
+      0 => '1',
+      1 => '3',
+      2 => '2',
+      3 => '7',
+    ),
+    5 => 
+    array (
+    ),
+    6 => 
+    array (
+    ),
+    7 => 
+    array (
+      0 => '1',
+      1 => '3',
+      2 => '2',
+      3 => '4',
+    ),
+    8 => 
+    array (
+      0 => '9',
+    ),
+    9 => 
+    array (
+      0 => '8',
+    ),
+    11 => 
+    array (
+    ),
+    '' => 
+    array (
+    ),
+  ),
+);
 
  ?>
