3e47699a8229e0990c3b74d85746e22e16034bc3
Axel Flores <axelflores@MacBook-Pro-de-Axel.local>
2020-09-02 21:54:07 -0500

    Ajuste campo custom opportunities_directores.js

--- custom/clients/base/fields/opportunities_directores/opportunities_directores.js
+++ custom/clients/base/fields/opportunities_directores/opportunities_directores.js
@@ -86,7 +86,11 @@
             if(this.model.get('tipo_producto_c')!='1'){
                 $('[data-type="opportunities_directores"]').hide();
             }else{
-                $('[data-type="opportunities_directores"]').show();
+                if (this.model.get('tct_etapa_ddw_c')=="SI" && this.model.get('estatus_c')=="") {
+                  $('[data-type="opportunities_directores"]').hide();
+                }else{
+                  $('[data-type="opportunities_directores"]').show();
+                }
             }
         }
 
