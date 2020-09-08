ad0fd615b39d52cc80445118e2441f7ed8301e81
erickdjcruz <47753132+erickdjcruz@users.noreply.github.com>
2020-09-08 18:53:13 -0500

    #38738_notificacion_fiscal_08-09-2020

---
+++ custom/Extension/modules/Accounts/Ext/LogicHooks/NotiFiscal_Hook.php
@@ -0,0 +1,9 @@
+<?php
+
+$hook_array['after_save'][] = Array(
+    21,
+    'Notificacion Proveedor',
+    'custom/modules/Accounts/Notificacion_Fiscal_class.php',
+    'Notificacion_Fiscal_class',
+    'notificacionF'
+);
