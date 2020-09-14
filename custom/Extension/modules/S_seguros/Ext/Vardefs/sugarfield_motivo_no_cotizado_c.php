827faccae3b38d62b0504def4ab80e5dd0b98f8a
Eduardo Carrasco <eduardo.carrasco@tactos.com.mx>
2020-09-14 12:10:41 -0500

    11599-SegurosF2

---
+++ custom/Extension/modules/S_seguros/Ext/Vardefs/sugarfield_motivo_no_cotizado_c.php
@@ -0,0 +1,7 @@
+<?php
+ // created: 2020-09-14 10:41:52
+$dictionary['S_seguros']['fields']['motivo_no_cotizado_c']['labelValue']='Motivo No Cotizado';
+$dictionary['S_seguros']['fields']['motivo_no_cotizado_c']['dependency']='equal($etapa,5)';
+$dictionary['S_seguros']['fields']['motivo_no_cotizado_c']['visibility_grid']='';
+
+ ?>
