1f02da5bb386df4e7095cd3100a0bf868efd214c
arauz07 <adrian.arauz@tactos.com.mx>
2020-09-21 17:53:10 -0500

    Ticket#11681-NoAuditables

---
+++ custom/Extension/modules/uni_Productos/Ext/Vardefs/sugarfield_no_viable.php
@@ -0,0 +1,5 @@
+<?php
+ // created: 2020-09-21 17:24:35
+$dictionary['uni_Productos']['fields']['no_viable']['audited']=true;
+
+ ?>
---
+++ custom/Extension/modules/uni_Productos/Ext/Vardefs/sugarfield_no_viable_porque.php
@@ -0,0 +1,5 @@
+<?php
+ // created: 2020-09-21 17:32:54
+$dictionary['uni_Productos']['fields']['no_viable_porque']['audited']=true;
+
+ ?>
