b8965bb17d689ea0dc40a5de7c7fa2b3054ba192
Salvador Lopez <salvador.lopez@tactos.com.mx>
2020-10-01 10:52:45 -0500

    Ajuste para mostrar u ocultar campo de Director de solicitud

--- custom/clients/base/fields/opportunities_directores/opportunities_directores.js
+++ custom/clients/base/fields/opportunities_directores/opportunities_directores.js
@@ -90,7 +90,7 @@
                 }else{
                     if (this.model.get('tct_etapa_ddw_c')=="SI" && this.model.get('estatus_c')=="") {
                         $('[data-type="opportunities_directores"]').hide();
-                    }else if(banderaExcluye.check.incudes(1)){
+                    }else if(banderaExcluye.check.includes(1)){
                         $('[data-type="opportunities_directores"]').hide();
                     }
                     else{
