d37b9d1560d82a5f6590b0c8c906da0bda6ac807
Salvador Lopez <salvador.lopez@tactos.com.mx>
2020-09-30 21:15:14 -0500

    Ocultar campo de director al dar click en editar solicitud

--- custom/clients/base/fields/opportunities_directores/opportunities_directores.js
+++ custom/clients/base/fields/opportunities_directores/opportunities_directores.js
@@ -82,20 +82,25 @@
             this.directorSolicitud="";
         }
 
-        if(this.model.get('tipo_producto_c')!=undefined){
-            if(this.model.get('tipo_producto_c')!='1'){ //Tipo 1 = LEASING
-                $('[data-type="opportunities_directores"]').hide();
-            }else{
-                if (this.model.get('tct_etapa_ddw_c')=="SI" && this.model.get('estatus_c')=="") {
-                  $('[data-type="opportunities_directores"]').hide();
-                }else if(banderaExcluye.check.incudes(1)){
+
+        if(this.view.action!="create"){
+            if(this.model.get('tipo_producto_c')!=undefined){
+                if(this.model.get('tipo_producto_c')!='1'){ //Tipo 1 = LEASING
                     $('[data-type="opportunities_directores"]').hide();
-                }
-                else{
-                  $('[data-type="opportunities_directores"]').show();
+                }else{
+                    if (this.model.get('tct_etapa_ddw_c')=="SI" && this.model.get('estatus_c')=="") {
+                        $('[data-type="opportunities_directores"]').hide();
+                    }else if(banderaExcluye.check.incudes(1)){
+                        $('[data-type="opportunities_directores"]').hide();
+                    }
+                    else{
+                        $('[data-type="opportunities_directores"]').show();
+                    }
                 }
             }
+
         }
+
 
         if(this.model.get("vobo_dir_c")==true){
             $('[data-type="opportunities_directores"]').attr('style', 'pointer-events:none;');
