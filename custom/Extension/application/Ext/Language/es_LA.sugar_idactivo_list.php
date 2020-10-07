69fca83f9ed326ad7319305ac53649463808ea7f
Javier García <javier.garcia@tactos.com.mx>
2020-10-07 18:10:28 -0500

    11766 CondicionesFinancieras ajuste de orden de activos

--- custom/Extension/application/Ext/Language/es_LA.sugar_idactivo_list.php
+++ custom/Extension/application/Ext/Language/es_LA.sugar_idactivo_list.php
@@ -1,15 +1,15 @@
 <?php
- // created: 2020-10-06 14:41:45
+ // created: 2020-10-07 17:58:31
 
 $app_list_strings['idactivo_list']=array (
   '' => '',
   '000100030001' => 'AUTOS',
   '0001' => 'EQUIPO DE TRANSPORTE / FLOTILLA',
   '000100030002' => 'CAMIONES',
-  '000100020001' => 'BARCOS',
   '0002' => 'MAQUINARIA / EQUIPO',
-  '000100030005' => 'MOTOS',
-  '000100010001' => 'AVIONES',
   '00030006' => 'EQUIPO DE COMPUTO',
+  '000100020001' => 'BARCOS',
+  '000100010001' => 'AVIONES',
+  '000100030005' => 'MOTOS',
   '0003' => 'EQUIPO',
 );
