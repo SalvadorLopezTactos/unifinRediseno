47c2e8ff23c8ac248350a3838ffaf37cf339fa47
arauz07 <adrian.arauz@tactos.com.mx>
2020-11-06 09:41:11 -0600

    CP1009-Ajuste OBS Acento Ag Telefonico

--- custom/Extension/modules/Users/Ext/Vardefs/sugarfield_agente_telefonico_c.php
+++ custom/Extension/modules/Users/Ext/Vardefs/sugarfield_agente_telefonico_c.php
@@ -1,6 +1,6 @@
 <?php
- // created: 2020-01-16 11:23:19
-$dictionary['User']['fields']['agente_telefonico_c']['labelValue']='Gestión Agente telefonico';
+ // created: 2020-11-06 09:35:11
+$dictionary['User']['fields']['agente_telefonico_c']['labelValue']='Gestión Agente telefónico';
 $dictionary['User']['fields']['agente_telefonico_c']['enforced']='';
 $dictionary['User']['fields']['agente_telefonico_c']['dependency']='';
 
