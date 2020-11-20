e0f3f1ef25219a6e2d42822e8a65d2cefa8077e2
IRVING DELGADO <irving.delgado@tactos.com.mx>
2020-11-19 18:14:41 -0600

    Ticket#34914,CP-1198, Ajuste combinacion en vacio

--- custom/Extension/application/Ext/Language/es_LA.sugar_actividad_list.php
+++ custom/Extension/application/Ext/Language/es_LA.sugar_actividad_list.php
@@ -2,6 +2,7 @@
 // created: 2020-11-03 22:04:51
 
 $app_list_strings['actividad_list'] = array(
+  '0' => '',
   '0111013' => 'Cultivo de Alpiste',
   '0111021' => 'Cultivo de Arroz',
   '0111039' => 'Cultivo de Avena',
@@ -1194,6 +1195,5 @@ $app_list_strings['actividad_list'] = array(
   '9911018' => 'Instituciones Financieras del Extranjero',
   '9912016' => 'Consulado',
   '9912024' => 'Gobierno Extranjero',
-  '9919012' => 'Servicios de Oficinas Y Representaciones de Otros Paises Que Gozan de Extraterritorialidad',
-  '' => '',
+  '9919012' => 'Servicios de Oficinas Y Representaciones de Otros Paises Que Gozan de Extraterritorialidad',  
 );
