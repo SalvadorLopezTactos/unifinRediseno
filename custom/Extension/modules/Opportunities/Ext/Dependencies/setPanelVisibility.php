c8e09cdaefb9376a92a9e6646405a1e5cf0cba5f
Eduardo Carrasco <eduardo.carrasco@tactos.com.mx>
2020-11-27 14:40:02 -0600

    11960-CE

---
+++ custom/Extension/modules/Opportunities/Ext/Dependencies/setPanelVisibility.php
@@ -0,0 +1,17 @@
+<?php
+$dependencies['Opportunities']['credito_estructurado']=array(
+    'hooks' => array("edit","view"),
+    'trigger' => 'true',
+    'triggerFields' => array('producto_financiero_c'),
+    'onload' => true,
+    'actions' => array(
+        array(
+            'name' => 'SetPanelVisibility',
+            'params' => array(
+                'target' => 'LBL_RECORDVIEW_PANEL4',
+                'value' => 'equal($producto_financiero_c,43)',
+            ),
+        ),
+    ),
+   	'notActions' => array(),
+);
