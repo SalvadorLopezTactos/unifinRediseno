6621cff65ee9edc74a7d31260a653afdf52ce243
Javier García <javier.garcia@tactos.com.mx>
2020-11-29 14:05:15 -0600

    44674 AltaLeadsServicio Ajuste de servicios y validaciones

---
+++ custom/Extension/modules/Leads/Ext/Vardefs/sugarfield_contacto_telefono_c.php
@@ -0,0 +1,12 @@
+<?php
+ // created: 2020-11-29 11:40:06
+$dictionary['Lead']['fields']['contacto_telefono_c']['labelValue']='Teléfono Contacto';
+$dictionary['Lead']['fields']['contacto_telefono_c']['full_text_search']=array (
+  'enabled' => '0',
+  'boost' => '1',
+  'searchable' => false,
+);
+$dictionary['Lead']['fields']['contacto_telefono_c']['enforced']='';
+$dictionary['Lead']['fields']['contacto_telefono_c']['dependency']='';
+
+ ?>
