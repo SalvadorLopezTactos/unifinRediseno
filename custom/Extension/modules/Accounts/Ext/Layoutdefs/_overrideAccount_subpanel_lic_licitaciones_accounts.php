cfd41445d89c2527fef950df3c4a07e4e39c212c
arauz07 <adrian.arauz@tactos.com.mx>
2020-12-14 21:55:48 -0600

    Ajuste subpanel ticket44660-3

---
+++ custom/Extension/modules/Accounts/Ext/Layoutdefs/_overrideAccount_subpanel_lic_licitaciones_accounts.php
@@ -0,0 +1,4 @@
+<?php
+//auto-generated file DO NOT EDIT
+$layout_defs['Accounts']['subpanel_setup']['lic_licitaciones_accounts']['override_subpanel_name'] = 'Account_subpanel_lic_licitaciones_accounts';
+?>
---
+++ custom/modules/Lic_Licitaciones/clients/base/views/subpanel-for-accounts-lic_licitaciones_accounts/subpanel-for-accounts-lic_licitaciones_accounts.php
@@ -0,0 +1,72 @@
+<?php
+// created: 2020-12-15 03:49:50
+$viewdefs['Lic_Licitaciones']['base']['view']['subpanel-for-accounts-lic_licitaciones_accounts'] = array (
+  'panels' => 
+  array (
+    0 => 
+    array (
+      'name' => 'panel_header',
+      'label' => 'LBL_PANEL_1',
+      'fields' => 
+      array (
+        0 => 
+        array (
+          'label' => 'LBL_NAME',
+          'enabled' => true,
+          'default' => true,
+          'name' => 'name',
+          'link' => true,
+        ),
+        1 => 
+        array (
+          'name' => 'institucion',
+          'label' => 'LBL_INSTITUCION',
+          'enabled' => true,
+          'default' => true,
+        ),
+        2 => 
+        array (
+          'name' => 'description',
+          'label' => 'LBL_DESCRIPTION',
+          'enabled' => true,
+          'sortable' => false,
+          'default' => true,
+        ),
+        3 => 
+        array (
+          'name' => 'monto_total',
+          'label' => 'LBL_MONTO_TOTAL',
+          'enabled' => true,
+          'related_fields' => 
+          array (
+            0 => 'currency_id',
+            1 => 'base_rate',
+          ),
+          'currency_format' => true,
+          'default' => true,
+        ),
+        4 => 
+        array (
+          'name' => 'date_entered',
+          'label' => 'LBL_DATE_ENTERED',
+          'enabled' => true,
+          'readonly' => true,
+          'default' => true,
+        ),
+        5 => 
+        array (
+          'label' => 'LBL_DATE_MODIFIED',
+          'enabled' => true,
+          'default' => true,
+          'name' => 'date_modified',
+        ),
+      ),
+    ),
+  ),
+  'orderBy' => 
+  array (
+    'field' => 'date_modified',
+    'direction' => 'desc',
+  ),
+  'type' => 'subpanel-list',
+);
