65410c641fbefb6d77b1f2519b827a2ddac5e189
Salvador Lopez <salvador.lopez@tactos.com.mx>
2020-12-20 22:19:32 -0600

    Dashlet Protocolo de Reasignación de Leads

---
+++ custom/clients/base/views/dashlet_protocolo_leads/dashlet_protocolo_leads.js
@@ -0,0 +1,54 @@
+({
+    plugins: ['Dashlet'],
+
+    events: {
+        'click #assign_cp': 'asignarCP',
+        'click #assign_asesor': 'asignarPorAsesor',
+        'click #assign_bd': 'asignarPorBD',
+        'click #assign_cancel': 'asignarPorCancelados',
+    },
+
+    initialize: function (options) {
+        this._super("initialize", [options]);
+    },
+
+    asignarCP: function (evt) {
+
+    	app.alert.show('navigateToNotificationCP', {
+    		level: 'confirmation',
+    		messages: 'Se le enviará una notificación al Centro de Prospección.<br>¿Está seguro?',
+    		autoClose: false,
+    		onConfirm: function(){
+    			alert("Enviando notificación...");
+    		},
+    		onCancel: function(){
+    			alert("Cancelando operación!");
+    		}
+    	});
+    },
+
+    asignarPorAsesor:function(evt){
+
+    	var objLead = {
+            action: 'edit',
+            copy: true,
+            create: true,
+            layout: 'create',
+            module: 'Leads',
+            dataFromProtocolo:'1'
+        };
+
+    	app.controller.loadView(objLead);
+
+    	// update the browser URL with the proper
+    	app.router.navigate('#Leads/create', {trigger: false});
+
+    },
+
+    asignarPorBD:function(){
+
+    	//Obtener registros para asignar automáticamente
+    	alert("Asignación por base de datos");
+
+    },
+})
