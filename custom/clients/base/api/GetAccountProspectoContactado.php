0add76a328b6a93cb8d1a8a3a46c9966d70c71ab
IRVING DELGADO <irving.delgado@tactos.com.mx>
2020-12-24 12:13:04 -0600

    Ticket#45221,CP-1552- Dashlet Prospecto Contactado Activos y Aplazados

--- custom/clients/base/api/GetAccountProspectoContactado.php
+++ custom/clients/base/api/GetAccountProspectoContactado.php
@@ -2,50 +2,49 @@
 
 if (!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point');
 
-class GetLeadsProspectoInteresado extends SugarApi
+class GetAccountProspectoContactado extends SugarApi
 {
     public function registerApiRest()
     {
         return array(
-            'GETLeadProspectoInteresadoAPI' => array(
+            'GETAProspectoContactadoAPI' => array(
                 'reqType' => 'GET',
                 'noLoginRequired' => false,
-                'path' => array('GetLeadsProspectoInteresado', '?'),
-                'pathVars' => array('module', 'statusProduct'),
-                'method' => 'getcstmGetLeadsProspectoInteresado',
-                'shortHelp' => 'Obtiene todas las Cuentas de tipo Prospecto Interesado con sus Solicitudes en estatus inicial',
+                'path' => array('GetAccountProspectoContactado', '?'),
+                'pathVars' => array('module', 'sproducto'),
+                'method' => 'getcstmGetAccountProspectoContactado',
+                'shortHelp' => 'Obtiene todas las Cuentas de tipo Prospecto Contactado que no tienen solicitudes',
             ),
         );
     }
-    public function getcstmGetLeadsProspectoInteresado($api, $args)
+    public function getcstmGetAccountProspectoContactado($api, $args)
     {
 
         try {
 
             global $current_user;
             $id_user = $current_user->id;
-            $statusProduct = $args['statusProduct'];
+            $estadoProducto = $args['sproducto'];
             $records_in = [];
 
-            if ($statusProduct != 3) {
+            if ($estadoProducto != 3) {
 
                 $query = "SELECT a.id as idCuenta, a.name as nombreCuenta, a.assigned_user_id, ac.user_id_c, ac.tipo_registro_c, ac.subtipo_cuenta_c, 
-                ac.tipo_registro_cuenta_c as tipoCuenta, ac.subtipo_registro_cuenta_c as subtipoCuenta, opp.id as idOpp, opp.name as oppNombre, 
-                opp.date_entered, opp.assigned_user_id, oppcstm.tct_etapa_ddw_c, oppcstm.tct_estapa_subetapa_txf_c as oppEtapa,
+                ac.tipo_registro_cuenta_c as tipoCuenta, ac.subtipo_registro_cuenta_c as subtipoCuenta, 
                 up.name, upc.status_management_c as EstatusProducto, up.tipo_producto
                 FROM accounts a
                 INNER JOIN accounts_cstm ac on ac.id_c = a.id
-                INNER JOIN accounts_opportunities app on app.account_id = ac.id_c
-                INNER JOIN opportunities opp on opp.id = app.opportunity_id
-                INNER JOIN opportunities_cstm oppcstm on oppcstm.id_c = opp.id
+                LEFT JOIN accounts_opportunities app on app.account_id = ac.id_c
                 INNER JOIN accounts_uni_productos_1_c aup on aup.accounts_uni_productos_1accounts_ida = ac.id_c
                 INNER JOIN uni_productos up on up.id = aup.accounts_uni_productos_1uni_productos_idb
                 INNER JOIN uni_productos_cstm upc on upc.id_c = up.id
-                WHERE ac.tipo_registro_cuenta_c = '2' and ac.subtipo_registro_cuenta_c = '7' and ac.user_id_c= '{$id_user}'
-                and oppcstm.tct_etapa_ddw_c = 'SI' 
-                and opp.date_entered < DATE_SUB(now(), INTERVAL 5 DAY)
+                WHERE app.id IS NULL 
+                and ac.tipo_registro_cuenta_c = '2' 
+                and ac.subtipo_registro_cuenta_c = '2' 
+                and ac.user_id_c = '{$id_user}'
+                and upc.fecha_asignacion_c < DATE_SUB(now(), INTERVAL 5 DAY)
                 and up.tipo_producto = '1'
-                and upc.status_management_c = '{$statusProduct}'
+                and upc.status_management_c = '{$estadoProducto}'
                 and a.deleted = 0 and up.deleted = 0";
 
                 $result = $GLOBALS['db']->query($query);
@@ -54,7 +53,7 @@ class GetLeadsProspectoInteresado extends SugarApi
                     
                     $records_in['records'][] = array(
                         'idCuenta' => $row['idCuenta'], 'nombreCuenta' => $row['nombreCuenta'], 'tipoCuenta' => $row['tipoCuenta'],
-                        'subtipoCuenta' => $row['subtipoCuenta'], 'idOpp' => $row['idOpp'], 'oppNombre' => $row['oppNombre'], 'oppEtapa' => $row['oppEtapa'], 'EstatusProducto' => $row['EstatusProducto']
+                        'subtipoCuenta' => $row['subtipoCuenta'], 'EstatusProducto' => $row['EstatusProducto']
                     );
                 }
 
---
+++ custom/clients/base/views/dashlet-pc-aplazados/dashlet-pc-aplazados.js
@@ -0,0 +1,88 @@
+({
+    plugins: ['Dashlet'],
+
+    events: {
+        'click #btnCancelPCAplazado': 'noViablePCAplazado',
+        'click #btnPresolicitudAplazado': 'preSolicitudPCAplazado',
+    },
+
+    dataAcProspectoContactadoAplazado: [],
+
+    initialize: function (options) {
+        this._super("initialize", [options]);
+        self_pc_aplazado = this;
+        this.cuentasProspectContactadoAplazado();
+    },
+
+
+    cuentasProspectContactadoAplazado: function () {
+        //API para obtener los Cuentas prospecto contactado sin solicitudes
+        estPCAplazado = "2";
+
+        app.api.call('GET', app.api.buildURL('GetAccountProspectoContactado/' + estPCAplazado), null, {
+            success: function (data) {
+
+                self_pc_aplazado.dataAcProspectoContactadoAplazado = data.records;
+
+                self_pc_aplazado.render();
+            },
+            error: function (e) {
+                throw e;
+            }
+        });
+    },
+
+    preSolicitudPCAplazado: function (events) {
+        
+        idCuentaAplazado = $(events.currentTarget).attr('title');
+
+        app.alert.show('sol-pc-aplazado', {
+            level: 'process',
+            title: 'Cargando...',
+        });
+        
+        app.api.call("read", app.api.buildURL("Accounts/" + idCuentaAplazado, null, null, {
+            fields: "name",
+        }), null, {
+            success: _.bind(function (data) {
+
+                app.alert.dismiss('sol-pc-aplazado');
+
+                var objOpp = {
+                    action: 'edit',
+                    copy: true,
+                    create: true,
+                    layout: 'create',
+                    module: 'Opportunities',
+                    idAccount: idCuentaAplazado,
+                    idNameAccount: data.name
+                };
+                app.controller.loadView(objOpp);
+                // update the browser URL with the proper
+                app.router.navigate('#Opportunities/create', { trigger: false });
+            }, this)
+        });
+    },
+
+    noViablePCAplazado: function (events) {
+
+        btnIdCuentaPCAplazado = $(events.currentTarget).attr('title');
+
+        var quickCreateView = null;
+        if (!quickCreateView) {
+
+            quickCreateView = app.view.createView({
+                context: this.context,
+                name: 'ModalNoViableCuentas',
+                layout: this.layout,
+                module: 'Accounts',
+                contextIdCuenta: btnIdCuentaPCAplazado
+            });
+
+            this.layout._components.push(quickCreateView);
+            this.layout.$el.append(quickCreateView.$el);
+
+        }
+        this.layout.trigger("app:view:ModalNoViableCuentas");
+    },
+})
