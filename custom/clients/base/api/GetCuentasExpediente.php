f63472d635629a4e0d84330d39dba301277d81ca
erickdjcruz <47753132+erickdjcruz@users.noreply.github.com>
2020-12-30 14:42:17 -0600

    #45221_lead_management-2020-12-30
    Dashlets expediente erick

---
+++ custom/clients/base/api/GetCuentasExpediente.php
@@ -0,0 +1,77 @@
+<?php
+
+if (!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point');
+
+class GetCuentasExpediente extends SugarApi
+{
+    public function registerApiRest()
+    {
+        return array(
+            'GETLeadNoAtendidoAPI' => array(
+                'reqType' => 'GET',
+                'noLoginRequired' => false,
+                'path' => array('GetCuentasExpediente', '?'),
+                'pathVars' => array('module', 'statusProduct'),
+                'method' => 'getcstmGetCuentasExpediente',
+                'shortHelp' => 'Obtiene todas las Cuentas con estatus "Prospecto en Integración de Expediente"',
+            ),
+        );
+    }
+    public function getcstmGetCuentasExpediente($api, $args)
+    {
+
+        try {
+
+            global $current_user;
+            $id_user = $current_user->id;
+            $statusProduct = $args['statusProduct'];
+            $records_in = [];
+
+            if ($statusProduct != 3) {
+
+                $query = "SELECT a.id as idCuenta, a.name as nombreCuenta, a.assigned_user_id, ac.user_id_c, ac.tipo_registro_c, ac.subtipo_cuenta_c, 
+ac.tipo_registro_cuenta_c as tipoCuenta, ac.subtipo_registro_cuenta_c as subtipoCuenta,
+ opp.id as idOpp, opp.name as oppNombre, opp.id, opp.amount as monto,
+ opp.date_entered, opp.assigned_user_id, oppcstm.tct_etapa_ddw_c,
+ oppcstm.tct_estapa_subetapa_txf_c as oppEtapa, up.name , upc.fecha_asignacion_c ,
+ up.tipo_producto, upc.status_management_c as EstatusProducto
+FROM accounts a 
+INNER JOIN accounts_cstm ac on ac.id_c = a.id
+INNER JOIN accounts_opportunities app on app.account_id = ac.id_c
+INNER JOIN opportunities opp on opp.id = app.opportunity_id
+INNER JOIN opportunities_cstm oppcstm on oppcstm.id_c = opp.id
+INNER JOIN accounts_uni_productos_1_c aup on aup.accounts_uni_productos_1accounts_ida = ac.id_c
+INNER JOIN uni_productos up on up.id = aup.accounts_uni_productos_1uni_productos_idb
+INNER JOIN uni_productos_cstm upc on upc.id_c = up.id
+WHERE ac.tipo_registro_cuenta_c = '2' and ac.subtipo_registro_cuenta_c = '8'
+ and ac.user_id_c=  '{$id_user}'
+ and upc.status_management_c = '{$statusProduct}'
+and a.deleted = 0 and up.deleted = 0 
+and ((upc.fecha_asignacion_c > DATE_SUB(now(), INTERVAL 10 DAY) and opp.amount <= 10000000) OR
+(upc.fecha_asignacion_c > DATE_SUB(now(), INTERVAL 20 DAY) and opp.amount > 10000000))";
+               //$GLOBALS['log']->fatal('query',$query);
+                $result = $GLOBALS['db']->query($query);
+				
+                while ($row = $GLOBALS['db']->fetchByAssoc($result)){                    
+                    $records_in['records'][] = array(
+                        'idCuenta' => $row['idCuenta'], 'nombreCuenta' => $row['nombreCuenta'], 'tipoCuenta' => $row['tipoCuenta'],
+                        'subtipoCuenta' => $row['subtipoCuenta'], 'idOpp' => $row['idOpp'], 'oppNombre' => $row['oppNombre'], 
+						'oppEtapa' => $row['oppEtapa'], 'EstatusProducto' => $row['EstatusProducto'],
+						'fecha_asignacion' => $row['fecha_asignacion_c'],'Monto' => '$ '.round($row['monto'], 2)
+                    );					
+                }
+
+            } else {
+
+                $records_in['status'] = '200';
+                $records_in['message'] = 'Validar que el estatus del Producto sea Activo o Aplazado';
+            }
+			$GLOBALS['log']->fatal('records_in',$records_in);
+            return $records_in;
+
+        } catch (Exception $e) {
+
+            $GLOBALS['log']->fatal("Error: " . $e->getMessage());
+        }
+    }
+}
