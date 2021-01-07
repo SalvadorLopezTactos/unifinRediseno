ec4cc915dfbc6d572be887ec4e23212da1723bbd
Eduardo Carrasco <eduardo.carrasco@tactos.com.mx>
2021-01-06 19:15:22 -0600

    48434-Seguros

--- custom/Extension/modules/S_seguros/Ext/Vardefs/sugarfield_ejecutivo_c.php
+++ custom/Extension/modules/S_seguros/Ext/Vardefs/sugarfield_ejecutivo_c.php
@@ -1,17 +1,19 @@
 <?php
- // created: 2021-01-06 18:27:30
+ // created: 2021-01-06 19:08:08
 $dictionary['S_seguros']['fields']['ejecutivo_c']['labelValue']='Ejecutivo Asignado o KAM';
+$dictionary['S_seguros']['fields']['ejecutivo_c']['dependency']='';
 $dictionary['S_seguros']['fields']['ejecutivo_c']['visibility_grid']=array (
   'trigger' => 'tipo_registro_sf_c',
   'values' => 
   array (
     1 => 
     array (
-      0 => '2',
+      0 => '3',
     ),
     2 => 
     array (
       0 => '1',
+      1 => '2',
     ),
   ),
 );
