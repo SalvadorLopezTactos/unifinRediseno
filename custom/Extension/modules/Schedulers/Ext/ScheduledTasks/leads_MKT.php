6b7749a0953303625cec895cdbfbbe3a72caaf44
Javier García <javier.garcia@tactos.com.mx>
2021-01-07 21:09:04 -0600

    44937 LeadsUnifinGate Se creo logica para asignar a 9.- MKT los lead si no hay usuarios disponibles

---
+++ custom/Extension/modules/Schedulers/Ext/ScheduledTasks/leads_MKT.php
@@ -0,0 +1,112 @@
+<?php
+array_push($job_strings, 'assignLeadMktToUser');
+
+function assignLeadMktToUser()
+{
+    $GLOBALS['log']->fatal("INICIA JOB REASIGNAR LEADS CON USR GRUPO 9.- MKT A USR AGENTE TELEFONICO" );
+
+    global $db;
+
+    /* Obetenemos el id del usuario de grupo de 9.- MKT*/
+    $QueryId = "SELECT id from users
+WHERE first_name LIKE '%9.-%' AND last_name LIKE 'MKT'";
+    $queryResultId = $db->query($QueryId);
+    $row = $db->fetchByAssoc($queryResultId);
+    $idMKT = $row['id'];
+
+    /** Buscamos los Leads que tengan asignados el usuario de grupo 9.- MKT */
+    $getLeads = "select * from leads where assigned_user_id='{$idMKT}'";
+    $ResultLeads = $db->query($getLeads);
+
+    while ($row = $GLOBALS['db']->fetchByAssoc($ResultLeads)) {
+
+        $usrEnable = GetUserMKT();
+
+        $indices = $usrEnable['indice'];
+        if (!empty($usrEnable['id'])) {
+            $update_assigne_user = "UPDATE leads SET  assigned_user_id ='{$usrEnable['id']}'  WHERE id ='{$row['id']}' ";
+            $db->query($update_assigne_user);
+
+            if ($indices > -1) {
+                $update_assigne_user = "UPDATE config SET value = $indices  WHERE category = 'AltaLeadsServices' AND name = 'last_assigned_user'";
+                $db->query($update_assigne_user);
+            }
+        }
+    }
+    $GLOBALS['log']->fatal("tERMINA JOB REASIGNAR LEADS CON USR GRUPO 9.- MKT A USR AGENTE TELEFONICO" );
+
+    return true;
+}
+
+function GetUserMKT()
+{
+    global $db;
+    $users = [];
+    $new_assigned_user = "";
+
+    /* Obtiene  el dia y la hora actual*/
+    $queryFEcha = "SELECT date_format(NOW(),'%W %H %i') AS Fecha,UTC_TIMESTAMP()";
+    $queryResult = $db->query($queryFEcha);
+    $row = $db->fetchByAssoc($queryResult);
+    $date_Hoy = $row['Fecha'];
+    $array_date = explode(" ", $date_Hoy);
+    $dia_semana = $array_date[0];
+    $horaDia = $array_date[1] . ":" . $array_date[2];
+    $dateInput = date('H:i', strtotime($horaDia));
+
+    /* Obtiene el ultimo  usuario asignado y registrado en el config*/
+    $query = "Select value from config  where name='last_assigned_user' ";
+    $result = $db->query($query);
+    $row = $db->fetchByAssoc($result);
+    $last_indice = $row['value'];
+
+    /** Obtenemos los usuarios disponibles */
+    $query_asesores = "SELECT
+  user.id,
+  user.date_entered,
+  count(lead.assigned_user_id) AS total_asignados,
+  uc.access_hours_c
+FROM users user
+  INNER JOIN users_cstm uc
+    ON uc.id_c = user.id
+  INNER JOIN leads lead
+    ON lead.assigned_user_id = user.id
+where puestousuario_c='27' AND subpuesto_c='3'
+GROUP BY lead.assigned_user_id ORDER BY total_asignados,date_entered ASC";
+    $result_usr = $db->query($query_asesores);
+
+    while ($row = $db->fetchByAssoc($result_usr)) {
+        $hours = json_decode($row['access_hours_c'], true);
+        $hoursIn = !empty($hours) ? $hours[$dia_semana]['entrada'] : "";
+        $hoursOut = !empty($hours) ? $hours[$dia_semana]['salida'] : "";
+        if ($hoursIn != "" && $hoursOut != "") {
+            if (($hoursIn != "Bloqueado" && $hoursOut != "Bloqueado") && ($hoursIn != "Libre" && $hoursOut != "Libre")) {
+                $enable = accessHours($hoursIn, $hoursOut, $dateInput);
+                if ($enable) {
+                    $users[] = $row['id'];
+                }
+            } elseif ($hoursIn == "Libre" && $hoursOut == "Libre") {
+                $users[] = $row['id'];
+            }
+        }
+    }
+    //$GLOBALS['log']->fatal("Usuarios disponibles MKT en JOB " . print_r($users, true));
+
+    if (count($users) > 0) {
+        $new_indice = $last_indice >= count($users) - 1 ? 0 : $last_indice + 1;
+
+        $new_assigned_user = $users[$new_indice];
+    }
+    return array("id" => $new_assigned_user, "indice" => $new_indice);
+}
+
+function accessHours($from, $to, $login)
+{
+    $GLOBALS['log']->fatal('FRom ' . $from . "  " . $to . "  " . $login);
+    $dateFrom = date("H:i", strtotime($from));
+    $dateTo = date("H:i", strtotime($to));
+    $dateLogin = date("H:i", strtotime($login));
+    return ($dateFrom <= $dateLogin && $dateLogin <= $dateTo);
+}
+
+
