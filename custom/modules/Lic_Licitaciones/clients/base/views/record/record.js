3a9661961c58cf975dff58ef89c6833867dd7f06
arauz07 <adrian.arauz@tactos.com.mx>
2021-02-15 18:37:12 -0600

    Ajustes Ticket 51291- Licitaciones3

--- custom/modules/Lic_Licitaciones/clients/base/views/record/record.js
+++ custom/modules/Lic_Licitaciones/clients/base/views/record/record.js
@@ -17,6 +17,10 @@
         $('[name="create_pre"]').hide();
         //Evento para mostrar botones dependiendo de opcion elegida
         this.model.on("change:resultado_licitacion_c",_.bind(this.ocultarBotones, this));
+        //Validacion para impedir que se guarde registro sin cuenta asociada
+        this.model.addValidationTask('Valida_cuenta', _.bind(this.validacuenta, this));
+        this.model.addValidationTask('Valida_noViable', _.bind(this.validaNoViable, this));
+
     },
 
     _render: function (options) {
@@ -96,6 +100,36 @@
         }else{
             $('[name="create_pre"]').hide();
         }
+    },
+
+    validacuenta: function (fields, errors, callback) {
+        var cuenta=this.model.get('lic_licitaciones_accounts_name');
+        if (cuenta==""|| cuenta==null) {
+            app.alert.show("cuentaFaltante", {
+                level: "error",
+                title: "No se puede guardar el registro sin una cuenta asociada. Favor de verificar.",
+                autoClose: false
+            });
+            errors['lic_licitaciones_accounts_name'] = errors['lic_licitaciones_accounts_name'] || {};
+            errors['lic_licitaciones_accounts_name'].required = true;
+        }
+        callback(null, fields, errors);
+    },
+
+    validaNoViable: function (fields, errors, callback) {
+        var resultado=this.model.get('resultado_licitacion_c');
+        var razon=this.model.get('razon_no_viable_c');
+        if (resultado=="2" && razon=="") {
+            app.alert.show("noViableFaltante", {
+                level: "error",
+                title: "Hace falta seleccionar una razón de no viable.",
+                autoClose: false
+            });
+            errors['razon_no_viable_c'] = errors['razon_no_viable_c'] || {};
+            errors['razon_no_viable_c'].required = true;
+        }
+        callback(null, fields, errors);
+
     },
  
     
