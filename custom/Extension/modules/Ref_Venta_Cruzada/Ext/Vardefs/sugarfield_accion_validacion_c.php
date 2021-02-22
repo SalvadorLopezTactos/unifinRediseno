c92b4d256ea4a8bd2d6946087ce3e705894e757c
erickdjcruz <erick.cruz@tactos.com.mx>
2021-02-22 12:51:21 -0600

    #49473_cruzada_uniclick_20210222
    venta cruzada corregida v2

---
+++ custom/Extension/modules/Ref_Venta_Cruzada/Ext/Vardefs/sugarfield_accion_validacion_c.php
@@ -0,0 +1,7 @@
+<?php
+ // created: 2021-02-17 00:00:28
+$dictionary['Ref_Venta_Cruzada']['fields']['accion_validacion_c']['labelValue']='Acción de validación';
+$dictionary['Ref_Venta_Cruzada']['fields']['accion_validacion_c']['dependency']='';
+$dictionary['Ref_Venta_Cruzada']['fields']['accion_validacion_c']['visibility_grid']='';
+ ?>
+ 
