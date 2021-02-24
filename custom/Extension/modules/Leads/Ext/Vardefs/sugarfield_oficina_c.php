76f66b31cdc632eb42d10bf715db16bc8f3aaf25
Salvador Lopez <salvador.lopez@tactos.com.mx>
2021-02-23 20:21:38 -0600

    Nuevo campo oficina y ajuste a reasignación de base de datos automática

---
+++ custom/Extension/modules/Leads/Ext/Vardefs/sugarfield_oficina_c.php
@@ -0,0 +1,7 @@
+<?php
+ // created: 2021-02-23 19:24:02
+$dictionary['Lead']['fields']['oficina_c']['labelValue']='Oficina';
+$dictionary['Lead']['fields']['oficina_c']['dependency']='';
+$dictionary['Lead']['fields']['oficina_c']['visibility_grid']='';
+
+ ?>
