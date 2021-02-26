dc3e69a06584811dffd16b197cfc922e8b412e92
IRVING DELGADO <irving.delgado@tactos.com.mx>
2021-02-25 18:11:55 -0600

    Ticket#12453, CP-1764- Ajuste modal motivo cancelacion calls

--- custom/modules/Calls/clients/base/views/MotivoCancelModal/MotivoCancelModal.js
+++ custom/modules/Calls/clients/base/views/MotivoCancelModal/MotivoCancelModal.js
@@ -6,15 +6,19 @@
 ({
     extendsFrom: 'BaseeditmodalView',
     fallbackFieldTemplate: 'edit',
+    context_Call: null,
     motivo_list: null,
+    submotivo_list: null,
+
     events: {
         'click #btn-cancela': 'closeModal',
         'click #btn-asigna': 'assignedAccount',
+        'change #motivocancelacion': 'dependenciasMSC',
     },
 
-    context_Call: null,
     initialize: function (options) {
         self_modal_get = this;
+        this.showSubmotivo = 0;
 
         app.view.View.prototype.initialize.call(this, options);
         if (this.layout) {
@@ -22,12 +26,15 @@
 
                 var temp_array_get = [];
                 var newContext = options.context.get('model');
+                
+                //MOTIVO DE CANCELACION
                 var motivos = app.lang.getAppListStrings('motivo_cancelacion_list');
                 var list_html = '<option value="" >  </option>';
                 _.each(motivos, function (value, key) {
                     list_html += '<option value="' + key + '">' + motivos[key] + '</option>';
                 });
                 this.motivo_list = list_html;
+
                 this.context_Call = options;
                 this.render();
                 this.$('.modal').modal({
@@ -59,39 +66,130 @@
         $('.modal-backdrop').remove();
     },
 
+    dependenciasMSC: function () {
+        
+        var setMotivo = $("#motivocancelacion").val(); //Guarda el valor de Motivo de Cancelación 
+        
+        //YA ES CLIENTE UNIFIN - LA EMPRESA YA NO EXISTE
+        if ($("#motivocancelacion").val() == "1" || $("#motivocancelacion").val() == "4") {
+            this.showSubmotivo = 0;
+        }
+        //NO ES PERFIL
+        if ($("#motivocancelacion").val() == "2") {            
+            this.showSubmotivo = 1;
+            //Lista desplegable dependiente del Motivo de Cancelación
+            var submotivos = App.lang.getAppListStrings('submotivo_cancelacion_list');
+            var list_smc = '<option value=""></option>';
+            _.each(submotivos, function (value, key) {
+
+                if (key == 1 || key == 2 || key == 3 || key == 4 || key == 8) {
+                    // console.log(key);
+                    list_smc += '<option value="' + key + '">' + submotivos[key] + '</option>';
+                }
+            });
+            self_modal_get.submotivo_list = list_smc;            
+        }
+        //ILOCALIZABLE
+        if ($("#motivocancelacion").val() == "3") {
+            this.showSubmotivo = 1;
+            //Lista desplegable dependiente del Motivo de Cancelación
+            var submotivos = App.lang.getAppListStrings('submotivo_cancelacion_list');
+            var list_smc = '<option value=""></option>';
+            _.each(submotivos, function (value, key) {              
+
+                if (key == 9 || key == 10 || key == 11) {
+                    // console.log(key);                
+                    list_smc += '<option value="' + key + '">' + submotivos[key] + '</option>';
+                }
+            });            
+            self_modal_get.submotivo_list = list_smc;
+        }
+        //SI ES PERFIL, NO ESTA INTERESADO
+        if ($("#motivocancelacion").val() == "5") {
+            this.showSubmotivo = 1;
+            //Lista desplegable dependiente del Motivo de Cancelación
+            var submotivos = App.lang.getAppListStrings('submotivo_cancelacion_list');
+            var list_smc = '<option value=""></option>';
+            _.each(submotivos, function (value, key) {              
+
+                if (key == 5 || key == 6 || key == 7) {
+                    // console.log(key);                
+                    list_smc += '<option value="' + key + '">' + submotivos[key] + '</option>';
+                }
+            });            
+            self_modal_get.submotivo_list = list_smc;            
+        }
+
+        self_modal_get.render();
+        $("#motivocancelacion").select2('val', setMotivo); //Setea el valor de Motivo de Cancelación para que no lo quite el render
+    },
+
     assignedAccount: function () {
         scall = this;
         var keyselect = null;
 
         keyselect = this.$('#motivocancelacion').val();
-        if (keyselect != "" && keyselect != null) {
+        var KeySubmotivoCancel = $("#submotivocancelacion").val();
+        var emptyMotivoSubmotivo = 0;
+
+        ////VALIDACION DE CAMPOS REQUERIDOS EN EL MODAL-MOTIVO DE CANCELACION////
+        if ($("#motivocancelacion").val() == "") {
+            $('#motivos').find('.select2-choice').css('border-color', 'red');
+            emptyMotivoSubmotivo += 1;
+        }
+        if ($("#motivocancelacion").val() == "2" && $("#submotivocancelacion").val() == "") {
+            $('#submotivos').find('.select2-choice').css('border-color', 'red');
+            emptyMotivoSubmotivo += 1;
+        }
+        if ($("#motivocancelacion").val() == "3" && $("#submotivocancelacion").val() == "") {
+            $('#submotivos').find('.select2-choice').css('border-color', 'red');
+            emptyMotivoSubmotivo += 1;
+        }
+        if ($("#motivocancelacion").val() == "5" && $("#submotivocancelacion").val() == "") {
+            $('#submotivos').find('.select2-choice').css('border-color', 'red');
+            emptyMotivoSubmotivo += 1;
+        }
+
+        if (emptyMotivoSubmotivo > 0) {
+            app.alert.show("falta-call-ms-cancelacion", {
+                level: "error",
+                title: 'Debe seleccionar los campos faltantes de Motivo de Cancelación.',
+                autoClose: false
+            });
+        }
+
+        if (keyselect != "" && keyselect != null && emptyMotivoSubmotivo == 0) {
 
             $('.record').removeAttr('style');
             $('.dashboard').removeAttr('style');
             $('.navbar').removeAttr('style');
 
+            app.alert.show('cancel-call-modal', {
+                level: 'process',
+                title: 'Cargando...',
+            });
+
             var lead = app.data.createBean('Leads', { id: this.model.get('parent_id') });
             lead.fetch({
                 success: _.bind(function (model) {
-                    model.set('lead_cancelado_c', true);
-                    model.set('motivo_cancelacion_c', keyselect);
-                    model.save();
+
+                    app.alert.dismiss('cancel-call-modal');
 
                     app.alert.show('message-id', {
                         level: 'success',
                         messages: 'Motivo de cancelación de Lead guardado',
                         autoClose: true
                     });
+
+                    model.set('lead_cancelado_c', true);
+                    model.set('motivo_cancelacion_c', keyselect);
+                    model.set('submotivo_cancelacion_c', KeySubmotivoCancel);
+                    model.save();
+                    
                 }, this)
             });
             scall.closeModal();
-        } else {
-            app.alert.show("Motivo de Cancelación", {
-                level: "error",
-                title: "Debe seleccionar motivo de Cancelación de Lead.",
-                autoClose: false
-            });
-        }
+        } 
     },
     /**Custom method to dispose the view*/
     _disposeView: function () {
@@ -107,9 +205,16 @@
 
     _render: function () {
         this._super("_render");
-        $('#MotivoCancelModal').modal({backdrop: 'static', keyboard: false});
+        // $('#MotivoCancelModal').modal({backdrop: 'static', keyboard: false});
         $('.record').attr('style', 'pointer-events:none');
         $('.dashboard').attr('style', 'pointer-events:none');
         $('.navbar').attr('style', 'pointer-events:none');
+
+        //Muesta el campo de Submotivo
+        if (this.showSubmotivo == 1) {
+            $('#submotivos').show();            
+        } else {
+            $('#submotivos').hide();
+        }
     },
 })
