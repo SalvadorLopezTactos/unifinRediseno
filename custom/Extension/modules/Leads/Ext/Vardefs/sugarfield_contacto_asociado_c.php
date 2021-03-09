409de29365883fb75ccfc1c1071a93c86c622f0f
IRVING DELGADO <irving.delgado@tactos.com.mx>
2021-03-08 18:43:37 -0600

    Ticket-54927,CP-1809-campo contactos asociado enforced vacio

--- custom/Extension/modules/Leads/Ext/Vardefs/sugarfield_contacto_asociado_c.php
+++ custom/Extension/modules/Leads/Ext/Vardefs/sugarfield_contacto_asociado_c.php
@@ -1,7 +1,7 @@
 <?php
  // created: 2021-03-08 18:31:48
 $dictionary['Lead']['fields']['contacto_asociado_c']['labelValue']='Contacto asociado';
-$dictionary['Lead']['fields']['contacto_asociado_c']['enforced']='false';
+$dictionary['Lead']['fields']['contacto_asociado_c']['enforced']='';
 $dictionary['Lead']['fields']['contacto_asociado_c']['dependency']='';
 
  ?>
