9b3a7b30c95a2738a134e29ba4455eadf14033bd
Eduardo Carrasco Beltran <eduardo.carrasco@tactos.com.mx>
2021-03-17 09:16:22 -0600

    53588-Campos CP

---
+++ custom/Extension/modules/Meetings/Ext/Vardefs/sugarfield_invitados_c.php
@@ -0,0 +1,12 @@
+<?php
+ // created: 2021-03-17 00:21:15
+$dictionary['Meeting']['fields']['invitados_c']['labelValue']='Invitados';
+$dictionary['Meeting']['fields']['invitados_c']['full_text_search']=array (
+  'enabled' => '0',
+  'boost' => '1',
+  'searchable' => false,
+);
+$dictionary['Meeting']['fields']['invitados_c']['enforced']='';
+$dictionary['Meeting']['fields']['invitados_c']['dependency']='';
+
+ ?>
