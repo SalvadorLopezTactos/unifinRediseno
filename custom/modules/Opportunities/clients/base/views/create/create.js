b2e94c4ffff8c221afcd9564a2613a5be0736022
erickdjcruz <47753132+erickdjcruz@users.noreply.github.com>
2021-04-13 18:46:00 -0500

    12741_cambio_self_13-04-2021
    CAmbio de render, mejora

--- custom/modules/Opportunities/clients/base/views/create/create.js
+++ custom/modules/Opportunities/clients/base/views/create/create.js
@@ -1955,7 +1955,6 @@
                 this.model.set("assigned_user_name", name_promotor);
 
                 self.render();
-                this.render();
                 }, this)
             });
 
