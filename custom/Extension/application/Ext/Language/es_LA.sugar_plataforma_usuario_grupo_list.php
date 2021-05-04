c9488a5c9a15d2c1d1c014f0cec349f34a01e382
Salvador Lopez <salvador.lopez@tactos.com.mx>
2021-05-04 16:29:12 -0500

    Nuevas plataformas de api y listas de valores

---
+++ custom/Extension/application/Ext/Language/es_LA.sugar_plataforma_usuario_grupo_list.php
@@ -0,0 +1,12 @@
+<?php
+ // created: 2021-05-03 19:36:07
+
+$app_list_strings['plataforma_usuario_grupo_list']=array (
+  'unics' => 'Unics',
+  'cvim' => 'Capa Intermedia',
+  'unicsWebCredit'=> 'Unics Web Credit',
+  'unicsWebUnicredit'=> 'Unics Web Unicredit',
+  'unicsWebCA'=> 'Unics Web CA',
+  'unicsCalidad'=> 'Unics Calidad',
+  'mambu'=>'Mambu',
+);
