637ef321c395bf33c0f9a2adc4f6f2d2b7930b04
Salvador Lopez <salvador.lopez@tactos.com.mx>
2021-06-10 23:09:13 -0500

    Nuevos campos de control sobre CF Quantico

---
+++ custom/Extension/modules/Opportunities/Ext/Vardefs/sugarfield_cf_quantico_c.php
@@ -0,0 +1,12 @@
+<?php
+ // created: 2021-06-10 13:35:10
+$dictionary['Opportunity']['fields']['cf_quantico_c']['labelValue']='Condiciones Financieras Quantico Usuario';
+$dictionary['Opportunity']['fields']['cf_quantico_c']['full_text_search']=array (
+  'enabled' => '0',
+  'boost' => '1',
+  'searchable' => false,
+);
+$dictionary['Opportunity']['fields']['cf_quantico_c']['enforced']='';
+$dictionary['Opportunity']['fields']['cf_quantico_c']['dependency']='';
+
+ ?>
