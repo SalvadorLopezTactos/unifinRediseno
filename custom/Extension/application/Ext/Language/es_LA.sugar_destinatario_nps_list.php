c0a9fb7972ff6097e47787f4da5b217a3b0283e1
IRVING DELGADO <irving.delgado@tactos.com.mx>
2021-06-15 11:03:39 -0500

    Ticket#60266-Ajuste destinatario

--- custom/Extension/application/Ext/Language/es_LA.sugar_destinatario_nps_list.php
+++ custom/Extension/application/Ext/Language/es_LA.sugar_destinatario_nps_list.php
@@ -2,5 +2,5 @@
  // created: 2021-06-14 15:21:38
 
 $app_list_strings['destinatario_nps_list']=array (
-  1 => 'c57e811e-b81a-cde4-d6b4-5626c9961772',
+  1 => '7c4149fa-bfa7-11ea-afb2-00155da0710c',
 );
