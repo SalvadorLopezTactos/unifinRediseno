3a474b08efc67fbec75db6c1c99b464c5ad99388
Salvador Lopez <salvador.lopez@tactos.com.mx>
2021-06-17 16:44:22 -0500

    Fórmula de visibilidad para campo CF Quantico, solo mostrar cuando el switch de uni2 esté apagado

--- custom/Extension/modules/Opportunities/Ext/Vardefs/sugarfield_condiciones_financieras_quantico.php
+++ custom/Extension/modules/Opportunities/Ext/Vardefs/sugarfield_condiciones_financieras_quantico.php
@@ -1,5 +1,5 @@
 <?php
- // created: 2021-06-17 09:31:41
+ // created: 2021-06-17 16:42:24
 $dictionary['Opportunity']['fields']['condiciones_financieras_quantico']['len']='255';
 $dictionary['Opportunity']['fields']['condiciones_financieras_quantico']['audited']=false;
 $dictionary['Opportunity']['fields']['condiciones_financieras_quantico']['massupdate']=false;
@@ -7,5 +7,6 @@ $dictionary['Opportunity']['fields']['condiciones_financieras_quantico']['duplic
 $dictionary['Opportunity']['fields']['condiciones_financieras_quantico']['duplicate_merge_dom_value']='1';
 $dictionary['Opportunity']['fields']['condiciones_financieras_quantico']['merge_filter']='disabled';
 $dictionary['Opportunity']['fields']['condiciones_financieras_quantico']['calculated']=false;
+$dictionary['Opportunity']['fields']['condiciones_financieras_quantico']['dependency']='equal(getDropdownValue("switch_inicia_proceso_list","ejecuta"),"0")';
 
  ?>
