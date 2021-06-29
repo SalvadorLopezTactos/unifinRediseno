a6c2568dbcbed448756f9d99000c8fdb4a47fa36
Salvador Lopez <salvador.lopez@tactos.com.mx>
2021-06-28 22:10:41 -0500

    Ajuste a referecnai para poder crear registros con el mismo producto en origen y referenciado

--- custom/Extension/modules/Ref_Venta_Cruzada/Ext/Vardefs/sugarfield_producto_referenciado.php
+++ custom/Extension/modules/Ref_Venta_Cruzada/Ext/Vardefs/sugarfield_producto_referenciado.php
@@ -1,6 +1,103 @@
 <?php
- // created: 2021-06-22 20:16:19
+ // created: 2021-06-28 22:05:08
 $dictionary['Ref_Venta_Cruzada']['fields']['producto_referenciado']['default']='';
 $dictionary['Ref_Venta_Cruzada']['fields']['producto_referenciado']['options']='producto_referenciado_list';
+$dictionary['Ref_Venta_Cruzada']['fields']['producto_referenciado']['visibility_grid']=array (
+  'trigger' => 'producto_origen',
+  'values' => 
+  array (
+    1 => 
+    array (
+      0 => '',
+      1 => '1',
+      2 => '2',
+      3 => '3',
+      4 => '4',
+      5 => '7',
+      6 => '8',
+      7 => '9',
+    ),
+    2 => 
+    array (
+      0 => '',
+      1 => '1',
+      2 => '2',
+      3 => '3',
+      4 => '4',
+      5 => '7',
+      6 => '8',
+      7 => '9',
+    ),
+    3 => 
+    array (
+      0 => '',
+      1 => '1',
+      2 => '2',
+      3 => '3',
+      4 => '4',
+      5 => '7',
+      6 => '8',
+      7 => '9',
+    ),
+    4 => 
+    array (
+      0 => '',
+      1 => '1',
+      2 => '2',
+      3 => '3',
+      4 => '4',
+      5 => '7',
+      6 => '8',
+      7 => '9',
+    ),
+    5 => 
+    array (
+    ),
+    6 => 
+    array (
+    ),
+    7 => 
+    array (
+      0 => '',
+      1 => '1',
+      2 => '3',
+      3 => '2',
+      4 => '4',
+      5 => '7',
+      6 => '8',
+      7 => '9',
+    ),
+    8 => 
+    array (
+      0 => '',
+      1 => '9',
+      2 => '8',
+    ),
+    9 => 
+    array (
+      0 => '',
+      1 => '8',
+      2 => '9',
+    ),
+    10 => 
+    array (
+    ),
+    11 => 
+    array (
+    ),
+    12 => 
+    array (
+    ),
+    13 => 
+    array (
+    ),
+    14 => 
+    array (
+    ),
+    '' => 
+    array (
+    ),
+  ),
+);
 
  ?>
