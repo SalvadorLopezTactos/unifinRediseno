47cd7490deb49dd53f2b68aaeb641036722b0eaf
Axel Flores <axelflores@192.168.1.5>
2021-07-20 13:23:48 -0500

    Bloqueo de acceso a usuario de integración sin plataforma

---
+++ custom/Extension/application/Ext/Language/es_LA.sugar_usuarios_api_ext_list.php
@@ -0,0 +1,15 @@
+<?php
+ // created: 2021-07-19 14:31:18
+
+$app_list_strings['usuarios_api_ext_list']=array (
+  1 => 'token',
+  2 => 'token1',
+  3 => 'unics',
+  4 => 'uni2',
+  5 => 'uni2crm',
+  6 => 'uni2-analisis',
+  7 => 'uni2-analisis1',
+  8 => 'migracion',
+  9 => 'rest',
+  10 => 'rest1',
+);
