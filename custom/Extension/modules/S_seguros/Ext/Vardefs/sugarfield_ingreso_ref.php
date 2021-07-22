4628fba0712423eefa9483a512d5a7b8d95e3cb6
Eduardo Carrasco Beltran <eduardo.carrasco@tactos.com.mx>
2021-07-22 10:37:37 -0500

    66273-Seguros

--- custom/Extension/modules/S_seguros/Ext/Vardefs/sugarfield_ingreso_ref.php
+++ custom/Extension/modules/S_seguros/Ext/Vardefs/sugarfield_ingreso_ref.php
@@ -1,7 +1,7 @@
 <?php
- // created: 2021-07-12 23:43:31
+ // created: 2021-07-22 10:27:58
 $dictionary['S_seguros']['fields']['ingreso_ref']['dependency']='or(equal($subetapa_c,1),equal($subetapa_c,2))';
-$dictionary['S_seguros']['fields']['ingreso_ref']['formula']='multiply($prima_neta_emitida_c,divide($incentivo,100))';
+$dictionary['S_seguros']['fields']['ingreso_ref']['formula']='multiply($prima_neta,divide($incentivo,100))';
 $dictionary['S_seguros']['fields']['ingreso_ref']['audited']=true;
 
  ?>
