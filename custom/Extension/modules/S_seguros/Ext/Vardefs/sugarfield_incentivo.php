e747652c0674de3347830cdcd777d7dc5919cbea
Axel Flores <axelflores@MacBook-Pro-de-Axel.local>
2021-08-04 17:59:04 -0500

    Seguros ajuste porcentaje a 25

--- custom/Extension/modules/S_seguros/Ext/Vardefs/sugarfield_incentivo.php
+++ custom/Extension/modules/S_seguros/Ext/Vardefs/sugarfield_incentivo.php
@@ -1,6 +1,6 @@
 <?php
  // created: 2021-07-13 16:56:36
-$dictionary['S_seguros']['fields']['incentivo']['default']='20';
+$dictionary['S_seguros']['fields']['incentivo']['default']='25';
 $dictionary['S_seguros']['fields']['incentivo']['audited']=true;
 $dictionary['S_seguros']['fields']['incentivo']['required']=true;
 $dictionary['S_seguros']['fields']['incentivo']['dependency']='or(equal($subetapa_c,1),equal($subetapa_c,2))';
