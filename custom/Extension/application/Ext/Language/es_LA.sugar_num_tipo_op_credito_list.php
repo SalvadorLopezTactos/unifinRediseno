ca1d8358e0215370c1cecad231126841ba0e1f61
erickdjcruz <erick.cruz@tactos.com.mx>
2021-08-18 16:57:23 -0500

    2424_BacklogCredito_campos_20210818
    Detalle de los campos creados

---
+++ custom/Extension/application/Ext/Language/es_LA.sugar_num_tipo_op_credito_list.php
@@ -0,0 +1,8 @@
+<?php
+ // created: 2021-08-18 16:24:52
+
+$app_list_strings['num_tipo_op_credito_list']=array (
+  1 => 'Capital trabajo',
+  2 => 'Simple',
+  3 => 'Estructurado',
+);
---
+++ custom/Extension/modules/lev_Backlog/Ext/Vardefs/sugarfield_num_tipo_op_leasing_c.php
@@ -0,0 +1,7 @@
+<?php
+ // created: 2021-08-18 16:19:39
+$dictionary['lev_Backlog']['fields']['num_tipo_op_leasing_c']['labelValue']='Tipo Operación Producto';
+$dictionary['lev_Backlog']['fields']['num_tipo_op_leasing_c']['dependency']='';
+$dictionary['lev_Backlog']['fields']['num_tipo_op_leasing_c']['visibility_grid']='';
+
+ ?>
