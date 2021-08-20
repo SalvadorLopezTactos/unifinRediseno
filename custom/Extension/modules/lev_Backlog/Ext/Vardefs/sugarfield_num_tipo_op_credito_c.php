0badd584b03780467d153f6f8561633f772283a7
erickdjcruz <erick.cruz@tactos.com.mx>
2021-08-20 08:36:32 -0500

    2424_BacklogCredito_listas_dependencias_20210820
    Cambios depdnencias de listas de valores

--- custom/Extension/modules/lev_Backlog/Ext/Vardefs/sugarfield_num_tipo_op_credito_c.php
+++ custom/Extension/modules/lev_Backlog/Ext/Vardefs/sugarfield_num_tipo_op_credito_c.php
@@ -1,7 +1,87 @@
 <?php
- // created: 2021-08-18 16:26:29
+ // created: 2021-08-20 08:35:15
 $dictionary['lev_Backlog']['fields']['num_tipo_op_credito_c']['labelValue']='Tipo Operación Producto';
-$dictionary['lev_Backlog']['fields']['num_tipo_op_credito_c']['dependency']='isInList($producto_c,createList("2"))';
-$dictionary['lev_Backlog']['fields']['num_tipo_op_credito_c']['visibility_grid']='';
+$dictionary['lev_Backlog']['fields']['num_tipo_op_credito_c']['dependency']='';
+$dictionary['lev_Backlog']['fields']['num_tipo_op_credito_c']['visibility_grid']=array (
+  'trigger' => 'producto_c',
+  'values' => 
+  array (
+    1 => 
+    array (
+    ),
+    2 => 
+    array (
+      0 => '1',
+      1 => '2',
+      2 => '3',
+      3 => '4',
+      4 => '5',
+      5 => '6',
+      6 => '7',
+      7 => '8',
+      8 => '9',
+      9 => '10',
+      10 => '11',
+      11 => '12',
+      12 => '13',
+      13 => '14',
+      14 => '15',
+      15 => '16',
+      16 => '18',
+      17 => '19',
+      18 => '20',
+      19 => '21',
+      20 => '22',
+      21 => '23',
+      22 => '24',
+      23 => '25',
+      24 => '26',
+      25 => '27',
+      26 => '28',
+      27 => '29',
+      28 => '30',
+      29 => '31',
+    ),
+    3 => 
+    array (
+    ),
+    4 => 
+    array (
+    ),
+    5 => 
+    array (
+    ),
+    6 => 
+    array (
+    ),
+    7 => 
+    array (
+    ),
+    8 => 
+    array (
+    ),
+    9 => 
+    array (
+    ),
+    10 => 
+    array (
+    ),
+    11 => 
+    array (
+    ),
+    12 => 
+    array (
+    ),
+    13 => 
+    array (
+    ),
+    14 => 
+    array (
+    ),
+    '' => 
+    array (
+    ),
+  ),
+);
 
  ?>
