3f8edf9c64880c083351b8f46e26db4bb825aada
arauz07 <adrian.arauz@tactos.com.mx>
2021-10-25 18:11:20 -0500

    Ticket#14318- Mejora - Validar usuario inactivo en llamadas ALERTS4

--- custom/modules/Calls/clients/base/views/record/record.js
+++ custom/modules/Calls/clients/base/views/record/record.js
@@ -1099,7 +1099,7 @@
                       }
                       app.alert.show("Usuarios", {
                           level: "error",
-                          messages: "No es posible guardar la llamada el siguiente usuario inactivo:<br>"+nombres,
+                          messages: "No es posible guardar la llamada con lo(s) siguiente(s) usuario(s) inactivo(s):<br>"+nombres,
                           autoClose: false
                       });
                       errors['usuariostatus'] = errors['usuariostatus'] || {};
