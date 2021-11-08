bf97cdff0e3846916fcec27c848180d28fd08b45
IRVING DELGADO <irving.delgado@tactos.com.mx>
2021-11-08 17:01:28 -0600

    Ticket#14319, CP-2691, Ajuste Sincronización LUMO BACKLOG-Cuentas

--- custom/modules/Accounts/lh_Admin_Backlog.php
+++ custom/modules/Accounts/lh_Admin_Backlog.php
@@ -12,16 +12,9 @@ class clase_Admin_Backlog
 
             $lumo = ($bean->lumo_c != '') ? 1 : 0;
 
-            $query = "SELECT id as idBacklog, name, account_id_c, lumo_cuentas_c as lumoBacklog from lev_backlog lb
-            inner join lev_backlog_cstm lbc on lbc.id_c = lb.id
-            where account_id_c = '{$bean->id}'";
-            $result = $db->query($query);
-
-            while ($row = $GLOBALS['db']->fetchByAssoc($result)) {
-
-                $update_Backlog = "UPDATE lev_backlog_cstm lbc SET lbc.lumo_cuentas_c = '{$lumo}' WHERE lbc.id_c ='{$row['idBacklog']}'";
-                $db->query($update_Backlog);
-            }
+            $update_Backlog = "UPDATE lev_backlog_cstm lbc INNER JOIN lev_backlog lb on lb.id = lbc.id_c SET lbc.lumo_cuentas_c = '{$lumo}' WHERE lb.account_id_c ='{$bean->id}'";
+            $db->query($update_Backlog);
+            
         }
     }
 }
--- custom/modules/lev_Backlog/admin_Backlog.php
+++ custom/modules/lev_Backlog/admin_Backlog.php
@@ -2,18 +2,22 @@
 
 class class_Backlog_Admin
 {
-    function func_Backlog_Admin($bean, $event, $arguments)
+    function func_Backlog_Admin($bean = null, $event = null, $args = null)
     {
-        if ($bean->account_id_c != '') {
+        //SOLO SE EJECUTA EN LA CREACIÓN DEL BACKLOG
+        if (!$args['isUpdate']) {
 
-            $rel_cuenta = BeanFactory::retrieveBean('Accounts', $bean->account_id_c);
-            $check_lumo = $rel_cuenta->lumo_c;
+            if ($bean->account_id_c != '') {
 
-            $GLOBALS['log']->fatal("BACKLOG - ACTUALIZA LUMO SI ESTA ACTIVO EN LA CUENTA RELACIONADA: ". $bean->account_id_c . " LUMO: ". $check_lumo);
-            
-            if ($check_lumo == 1) {
-                $bean->lumo_cuentas_c = 1;
-                // $GLOBALS['log']->fatal("LUMO BACKLOG ". $bean->lumo_cuentas_c);
+                $rel_cuenta = BeanFactory::retrieveBean('Accounts', $bean->account_id_c);
+                $check_lumo = $rel_cuenta->lumo_c;
+
+                $GLOBALS['log']->fatal("BACKLOG - ACTUALIZA LUMO SI ESTA ACTIVO EN LA CUENTA RELACIONADA: " . $bean->account_id_c . " LUMO: " . $check_lumo);
+
+                if ($check_lumo == 1) {
+                    $bean->lumo_cuentas_c = 1;
+                    // $GLOBALS['log']->fatal("LUMO BACKLOG ". $bean->lumo_cuentas_c);
+                }
             }
         }
     }
