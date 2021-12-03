a553436c47ad6b0d148c8a833dd88d813b64f7c4
Axel F <axel.flores@tactos.com.mx>
2021-12-02 21:05:21 -0600

    Ajuste para habilitar campo de Monto a Operar

--- custom/Extension/modules/Opportunities/Ext/Vardefs/sugarfield_amount.php
+++ custom/Extension/modules/Opportunities/Ext/Vardefs/sugarfield_amount.php
@@ -1,17 +1,15 @@
 <?php
- // created: 2021-11-10 18:55:10
+ // created: 2021-12-02 20:53:10
 $dictionary['Opportunity']['fields']['amount']['required']=false;
 $dictionary['Opportunity']['fields']['amount']['audited']=false;
 $dictionary['Opportunity']['fields']['amount']['massupdate']=false;
 $dictionary['Opportunity']['fields']['amount']['type']='currency';
 $dictionary['Opportunity']['fields']['amount']['dbType']='currency';
 $dictionary['Opportunity']['fields']['amount']['comments']='Unconverted amount of the opportunity';
-$dictionary['Opportunity']['fields']['amount']['duplicate_merge']='enabled';
-$dictionary['Opportunity']['fields']['amount']['duplicate_merge_dom_value']='1';
+$dictionary['Opportunity']['fields']['amount']['duplicate_merge']='disabled';
+$dictionary['Opportunity']['fields']['amount']['duplicate_merge_dom_value']='0';
 $dictionary['Opportunity']['fields']['amount']['merge_filter']='disabled';
-$dictionary['Opportunity']['fields']['amount']['calculated']=true;
-$dictionary['Opportunity']['fields']['amount']['formula']='rollupConditionalSum($revenuelineitems, "likely_case", "sales_stage", forecastSalesStages(true, false))';
-$dictionary['Opportunity']['fields']['amount']['enforced']=true;
+$dictionary['Opportunity']['fields']['amount']['calculated']=false;
 $dictionary['Opportunity']['fields']['amount']['related_fields']=array (
   0 => 'currency_id',
   1 => 'base_rate',
@@ -22,6 +20,9 @@ $dictionary['Opportunity']['fields']['amount']['validation']=array (
   'max' => 1.0E+18,
 );
 $dictionary['Opportunity']['fields']['amount']['hidemassupdate']=false;
-$dictionary['Opportunity']['fields']['amount']['readonly']=true;
+$dictionary['Opportunity']['fields']['amount']['readonly']=false;
+$dictionary['Opportunity']['fields']['amount']['default']='';
+$dictionary['Opportunity']['fields']['amount']['importable']='false';
+$dictionary['Opportunity']['fields']['amount']['enable_range_search']='1';
 
  ?>
