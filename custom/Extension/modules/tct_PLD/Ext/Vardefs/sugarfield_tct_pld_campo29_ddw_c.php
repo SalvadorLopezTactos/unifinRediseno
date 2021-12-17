7034131dd5fe52c67977e96a81ba104a32b53933
arauz07 <adrian.arauz@tactos.com.mx>
2021-12-17 11:43:47 -0600

    Ticket #91078 -  Ajuste campo CRM PLD multiselección

---
+++ custom/Extension/modules/tct_PLD/Ext/Vardefs/sugarfield_tct_pld_campo29_ddw_c.php
@@ -0,0 +1,9 @@
+<?php
+ // created: 2021-12-16 15:45:42
+$dictionary['tct_PLD']['fields']['tct_pld_campo29_ddw_c']['labelValue']='CR_Preg4';
+$dictionary['tct_PLD']['fields']['tct_pld_campo29_ddw_c']['dependency']='';
+$dictionary['tct_PLD']['fields']['tct_pld_campo29_ddw_c']['required_formula']='';
+$dictionary['tct_PLD']['fields']['tct_pld_campo29_ddw_c']['readonly_formula']='';
+$dictionary['tct_PLD']['fields']['tct_pld_campo29_ddw_c']['visibility_grid']='';
+
+ ?>
