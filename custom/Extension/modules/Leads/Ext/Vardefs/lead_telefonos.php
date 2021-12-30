d3d9ca6201270371d3cef4077b406a08686f0b5c
Eduardo Carrasco Beltran <eduardo.carrasco@tactos.com.mx>
2021-12-30 10:33:38 -0600

    14596-REUS

---
+++ custom/Extension/modules/Leads/Ext/Vardefs/lead_telefonos.php
@@ -0,0 +1,12 @@
+<?php
+
+$dictionary['Lead']['fields']['lead_telefonos'] = array (
+    'name' => 'lead_telefonos',
+    'id_name' => 'lead_telefonos',
+    'type' => 'lead_telefonos',
+    'module' => 'Leads',
+    'source' => 'non-db',
+    'dbType' => 'non-db',
+    'studio' => 'visible',
+    'vname' => 'LBL_LEAD_TELEFONOS',
+);
