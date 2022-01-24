e335162eb7392dd7f08105e084d4b2e0c27fa146
Eduardo Carrasco Beltran <eduardo.carrasco@tactos.com.mx>
2022-01-24 17:11:43 -0600

    101547-Estado Civil

--- custom/Extension/application/Ext/Language/es_LA.sugar_estadocivil_list.php
+++ custom/Extension/application/Ext/Language/es_LA.sugar_estadocivil_list.php
@@ -1,9 +1,16 @@
 <?php
- // created: 2018-01-10 14:10:55
+ // created: 2022-01-24 11:39:52
 
 $app_list_strings['estadocivil_list']=array (
   '' => '',
   'Casado' => 'Casado(a)',
   'Soltero' => 'Soltero(a)',
   'Union Libre' => 'Union Libre',
+  'Union Concubinaria' => 'Unión Concubinaria',
+  'Divorciado' => 'Divorciado(a)',
+  'Desconocido' => 'Desconocido',
+  'Conviviente' => 'Conviviente',
+  'Separado' => 'Separado(a)',
+  'Viudo' => 'Viudo(a)',
+  'Otros' => 'Otros',
 );
