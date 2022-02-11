2fbd7627c6aa6ce0141c61802777632d5becf0f4
erickdjcruz <47753132+erickdjcruz@users.noreply.github.com>
2022-02-11 02:27:41 -0600

    validacion_check_20220211
    validacion regla limpiar el check

--- custom/Extension/modules/Leads/Ext/Vardefs/sugarfield_alianza_soc_chk_c.php
+++ custom/Extension/modules/Leads/Ext/Vardefs/sugarfield_alianza_soc_chk_c.php
@@ -2,8 +2,8 @@
  // created: 2022-02-03 19:19:16
 $dictionary['Lead']['fields']['alianza_soc_chk_c']['duplicate_merge_dom_value']=0;
 $dictionary['Lead']['fields']['alianza_soc_chk_c']['labelValue']='Alianza SOC';
-$dictionary['Lead']['fields']['alianza_soc_chk_c']['calculated']='1';
-$dictionary['Lead']['fields']['alianza_soc_chk_c']['formula']='ifElse(and(equal($origen_c,"12"),equal($detalle_origen_c,"12")),true,$alianza_soc_chk_c)';
+$dictionary['Lead']['fields']['alianza_soc_chk_c']['calculated']='';
+$dictionary['Lead']['fields']['alianza_soc_chk_c']['formula']='';
 $dictionary['Lead']['fields']['alianza_soc_chk_c']['dependency']='not(and(equal($origen_c,"12"),equal($detalle_origen_c,"12")))';
 $dictionary['Lead']['fields']['alianza_soc_chk_c']['enforced']='true';
 $dictionary['Lead']['fields']['alianza_soc_chk_c']['readonly_formula']='';
