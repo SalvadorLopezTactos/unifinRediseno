45714d95b15b23b293eb64a02cf6bec5cf9dcf5d
Axel Flores <axelflores@MacBook-Pro-de-Axel.local>
2022-03-01 17:55:59 -0600

    Ajuste asesor operación

--- custom/modules/Opportunities/asesor_operacion.php
+++ custom/modules/Opportunities/asesor_operacion.php
@@ -10,28 +10,28 @@ class asesor_operacion_class
 {
     function asesor_operacion_function($bean, $event, $arguments)
     {
-        $GLOBALS['log']->fatal("solicitudes --> ".$bean->asesor_operacion_c . " asignado  " .$bean->assigned_user_id ." usuario " . $bean->user_id_c);
-        if(empty($bean->asesor_operacion_c))
-        {
-            //$bean->asesor_operacion_c=$bean->assigned_user_id;
-            $bean->user_id_c=$bean->assigned_user_id;
-        }
-		// Asesor Asignado vacío ECB 24/02/2022
-		if(empty($bean->assigned_user_id))
-        {
-            global $db;
-			$query = "select b.asesor_producto_c from prod_estructura_productos a, prod_estructura_productos_cstm b where a.id = b.id_c and a.tipo_producto = $bean->tipo_producto_c and a.negocio = $bean->negocio_c and producto_financiero = $bean->producto_financiero_c";
-			$result = $db->query($query);
-			$row = $db->fetchByAssoc($result);
-			$asesor = $row['asesor_producto_c'];
-			if(!empty($asesor))
-			{
-				$query = "select assigned_user_id from uni_productos where tipo_producto = $asesor and id in (select accounts_uni_productos_1uni_productos_idb from accounts_uni_productos_1_c where accounts_uni_productos_1accounts_ida = '$bean->account_id')";
-				$result = $db->query($query);
-				$row = $db->fetchByAssoc($result);
-				$asignado = $row['assigned_user_id'];
-				if(!empty($asignado)) $bean->assigned_user_id = $asignado;
-			}
-        }
-	}
+
+  		// Asesor Asignado vacío ECB 24/02/2022
+  		if(empty($bean->assigned_user_id)){
+        global $db;
+  			$query = "select b.asesor_producto_c from prod_estructura_productos a, prod_estructura_productos_cstm b where a.id = b.id_c and a.tipo_producto = $bean->tipo_producto_c and a.negocio = $bean->negocio_c and producto_financiero = $bean->producto_financiero_c";
+  			$result = $db->query($query);
+  			$row = $db->fetchByAssoc($result);
+  			$asesor = $row['asesor_producto_c'];
+  			if(!empty($asesor))
+  			{
+  				$query = "select assigned_user_id from uni_productos where tipo_producto = $asesor and id in (select accounts_uni_productos_1uni_productos_idb from accounts_uni_productos_1_c where accounts_uni_productos_1accounts_ida = '$bean->account_id')";
+  				$result = $db->query($query);
+  				$row = $db->fetchByAssoc($result);
+  				$asignado = $row['assigned_user_id'];
+  				if(!empty($asignado)) $bean->assigned_user_id = $asignado;
+  			}
+      }
+      //$GLOBALS['log']->fatal("solicitudes --> ".$bean->asesor_operacion_c . " asignado  " .$bean->assigned_user_id ." usuario " . $bean->user_id_c);
+      if(empty($bean->asesor_operacion_c))
+      {
+          //$bean->asesor_operacion_c=$bean->assigned_user_id;
+          $bean->user_id_c=$bean->assigned_user_id;
+      }
+  	}
 }
