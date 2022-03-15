c0f712d20cb7f7d52da07be56a697c5c0feb34a9
arauz07 <aa@MacBook-Pro-de-Adrian.local>
2022-03-15 16:21:17 -0600

    Ticket 105223 - Programa Vendors-5

--- custom/modules/Accounts/validate_vendor.php
+++ custom/modules/Accounts/validate_vendor.php
@@ -9,6 +9,7 @@ class class_validate_vendor
 			$queryVendor = "SELECT codigo_vendor_c FROM accounts_cstm c
             INNER JOIN accounts ac ON c.id_c = ac.id
             WHERE codigo_vendor_c = '{$bean->codigo_vendor_c}'
+            AND id_c != '{$bean->id}'
             AND ac.deleted = 0";
 			$queryResult = $db->query($queryVendor);
 			if($db->getRowCount($queryResult) > 0) throw new SugarApiExceptionInvalidParameter('No se puede guardar el registro. El código vendor ingresado '.$bean->codigo_vendor_c.' ya existe, favor de corregir.');
