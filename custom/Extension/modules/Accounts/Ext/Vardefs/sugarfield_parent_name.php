1514ec44f697de26dbf5250c53ca5997b2e571bf
erickdjcruz <47753132+erickdjcruz@users.noreply.github.com>
2022-03-17 11:43:28 -0600

    Texto_cuentas_parent_20220317
    Cambio texto de cuentas parent_name

---
+++ custom/Extension/modules/Accounts/Ext/Vardefs/sugarfield_parent_name.php
@@ -0,0 +1,11 @@
+<?php
+ // created: 2022-03-17 17:23:05
+$dictionary['Account']['fields']['parent_name']['audited']=true;
+$dictionary['Account']['fields']['parent_name']['hidemassupdate']=false;
+$dictionary['Account']['fields']['parent_name']['help']='Las opciones Cuenta primaria y Cuenta Secundaria no son elegibles manualmente. Para usar éstas 2 opciones debes asociar la cuenta a un grupo empresarial.';
+$dictionary['Account']['fields']['parent_name']['duplicate_merge']='disabled';
+$dictionary['Account']['fields']['parent_name']['duplicate_merge_dom_value']=0;
+$dictionary['Account']['fields']['parent_name']['merge_filter']='disabled';
+$dictionary['Account']['fields']['parent_name']['calculated']=false;
+
+ ?>
