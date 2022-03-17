8c6ecfae7b74a2fa67380117c712b718cf6805e9
erickdjcruz <47753132+erickdjcruz@users.noreply.github.com>
2022-03-17 12:31:28 -0600

    texto_comentario_ayuda_20220317
    Texto comentario aydua ortografia

--- custom/Extension/modules/Accounts/Ext/Vardefs/sugarfield_situacion_gpo_empresarial_c.php
+++ custom/Extension/modules/Accounts/Ext/Vardefs/sugarfield_situacion_gpo_empresarial_c.php
@@ -5,6 +5,6 @@ $dictionary['Account']['fields']['situacion_gpo_empresarial_c']['dependency']=''
 $dictionary['Account']['fields']['situacion_gpo_empresarial_c']['required_formula']='';
 $dictionary['Account']['fields']['situacion_gpo_empresarial_c']['readonly_formula']='';
 $dictionary['Account']['fields']['situacion_gpo_empresarial_c']['visibility_grid']='';
-$dictionary['Account']['fields']['situacion_gpo_empresarial_c']['help']='Las opciones Cuenta primaria y Cuenta Secundaria no son elegibles manualmente. Para usar éstas 2 opciones debes asociar la cuenta a un grupo empresarial.';
+$dictionary['Account']['fields']['situacion_gpo_empresarial_c']['help']='Las opciones Cuenta Primaria y Cuenta Secundaria no son elegibles manualmente. Para usar éstas 2 opciones debes asociar la cuenta a un grupo empresarial.';
 
  ?>
