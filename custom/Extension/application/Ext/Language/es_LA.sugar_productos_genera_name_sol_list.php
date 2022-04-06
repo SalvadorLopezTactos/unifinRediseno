3ba98a5d90c2398d3e3b620cba054c37bac07465
Axel Flores <axelflores@MacBook-Pro-de-Axel.local>
2022-04-06 12:42:09 -0500

    Actualiza gestión de nombre para solicitudes

---
+++ custom/Extension/application/Ext/Language/es_LA.sugar_productos_genera_name_sol_list.php
@@ -0,0 +1,14 @@
+<?php
+ // created: 2022-04-06 12:41:09
+
+$app_list_strings['productos_genera_name_sol_list']=array (
+  39 => 'UNICREDIT',
+  41 => 'UNILEASING',
+  48 => 'UNICARD',
+  49 => 'UNIPREMIUM',
+  50 => 'UNIFACTOR',
+  51 => 'UNIREVOLVING',
+  77 => 'CRÉDITO PYME',
+  83 => 'UNIFACTORING',
+  85 => 'UNIFIN CARD',
+);
---
+++ custom/Extension/application/Ext/Language/es_LA.sugar_productos_uniclick_list.php
@@ -0,0 +1,11 @@
+<?php
+ // created: 2022-04-06 12:38:13
+
+$app_list_strings['productos_uniclick_list']=array (
+  39 => 'UNICREDIT',
+  41 => 'UNILEASING',
+  48 => 'UNICARD',
+  49 => 'UNIPREMIUM',
+  50 => 'UNIFACTOR',
+  51 => 'UNIREVOLVING',
+);
