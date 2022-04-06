0e8708d9e45f8df7583f7f8019daa8994e6d1b6f
arauz07 <aa@MacBook-Pro-de-Adrian.local>
2022-04-06 10:21:32 -0500

    Ticket 109240 - Ajuste a Cuestionario PLD Tarjeta de Crédito3

--- custom/modules/Accounts/clients/base/views/record/record.js
+++ custom/modules/Accounts/clients/base/views/record/record.js
@@ -8320,7 +8320,7 @@ validaPropRealCR: function (fields, errors, callback) {
                                    }
                                    
                                    if((!esPropietario && esTercero && !tienePR) || (esCLiente && esTercero && !tienePR)){
-                                           $('.campo9rel-ap').find('.select2-choice').css('border-color', 'red');
+                                           $('.campo9rel-ce').find('.select2-choice').css('border-color', 'red');
    
                                        app.alert.show("existen_relaciones_PR", {
                                        level: "error",
