485b0944bd459f785aa39aedc4c032b0292f8938
erickdjcruz <47753132+erickdjcruz@users.noreply.github.com>
2022-04-13 12:38:13 -0500

    5042022_quantico_cancel_13042022
    Alerta correcta level error

--- custom/modules/Opportunities/clients/base/views/record/record.js
+++ custom/modules/Opportunities/clients/base/views/record/record.js
@@ -1638,7 +1638,7 @@
 
         if(!cancelar){
             app.alert.show("CancelcacSol", {
-                level: "alert",
+                level: "error",
                 title: respuesta.ErrorMessage,
                 autoClose: false
             });
