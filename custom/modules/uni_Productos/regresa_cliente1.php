cd7f5394301597a084906bb069e60f343b350e82
Eduardo Carrasco Beltran <eduardo.carrasco@tactos.com.mx>
2022-04-13 12:59:22 -0500

    131-06042022-Tarjeta de Crédito

--- custom/modules/uni_Productos/regresa_cliente1.php
+++ custom/modules/uni_Productos/regresa_cliente1.php
@@ -1,8 +1,8 @@
 <?php
 // Regresa a Cliente si cambia a Prospecto
-class regresa_cliente
+class regresa_cliente1
 {
-    function regresa_cliente($bean, $event, $arguments)
+    function regresa_cliente1($bean, $event, $arguments)
     {
 		if($bean->fetched_row['tipo_cuenta'] == 3 && $bean->tipo_cuenta == 2) {
 			$bean->tipo_cuenta = $bean->fetched_row['tipo_cuenta'];
