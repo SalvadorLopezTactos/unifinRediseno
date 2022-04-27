c74ba5c172ef930bf05266ab1988b3c461c43c0d
Axel Flores <axelflores@192.168.1.7>
2022-04-27 14:06:36 -0500

    Ticket#109661, CP-3198 Habilitación campos en vista PO

--- custom/Extension/modules/Prospects/Ext/Vardefs/prospects_clasf_sectorial.php
+++ custom/Extension/modules/Prospects/Ext/Vardefs/prospects_clasf_sectorial.php
@@ -1,9 +1,9 @@
 <?php
-$dictionary['Prospects']['fields']['prospects_clasf_sectorial'] = array (
+$dictionary['Prospect']['fields']['prospects_clasf_sectorial'] = array (
 'name' => 'prospects_clasf_sectorial',
 'id_name' => 'prospects_clasf_sectorial',
 'type' => 'prospects_clasf_sectorial',
-'module' => 'prospectss',
+'module' => 'prospects',
 'source' => 'non-db',
 'dbType' => 'non-db',
 'studio' => 'visible',
--- custom/Extension/modules/Prospects/Ext/Vardefs/prospects_direcciones.php
+++ custom/Extension/modules/Prospects/Ext/Vardefs/prospects_direcciones.php
@@ -1,6 +1,6 @@
 <?php
 
-$dictionary['Prospects']['fields']['prospects_direcciones'] = array (
+$dictionary['Prospect']['fields']['prospects_direcciones'] = array (
     'name' => 'prospects_direcciones',
     'id_name' => 'prospects_direcciones',
     'type' => 'prospects_direcciones',
