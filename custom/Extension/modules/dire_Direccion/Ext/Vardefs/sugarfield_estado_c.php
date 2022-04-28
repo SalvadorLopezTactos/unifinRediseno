5732c3e5ba2aa565563da4247b585d9d0cd66455
Salvador Lopez <salvador.lopez@tactos.com.mx>
2022-04-27 21:51:09 -0500

    Nuevos campos de direcciones

---
+++ custom/Extension/modules/dire_Direccion/Ext/Vardefs/sugarfield_estado_c.php
@@ -0,0 +1,17 @@
+<?php
+ // created: 2022-04-27 21:27:20
+$dictionary['dire_Direccion']['fields']['estado_c']['duplicate_merge_dom_value']=0;
+$dictionary['dire_Direccion']['fields']['estado_c']['labelValue']='Estado';
+$dictionary['dire_Direccion']['fields']['estado_c']['full_text_search']=array (
+  'enabled' => '0',
+  'boost' => '1',
+  'searchable' => false,
+);
+$dictionary['dire_Direccion']['fields']['estado_c']['calculated']='true';
+$dictionary['dire_Direccion']['fields']['estado_c']['formula']='related($dir_sepomex_dire_direccion,"estado")';
+$dictionary['dire_Direccion']['fields']['estado_c']['enforced']='true';
+$dictionary['dire_Direccion']['fields']['estado_c']['dependency']='';
+$dictionary['dire_Direccion']['fields']['estado_c']['required_formula']='';
+$dictionary['dire_Direccion']['fields']['estado_c']['readonly_formula']='';
+
+ ?>
