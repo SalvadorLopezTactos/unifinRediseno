80f1508f07ef695d8ef067410ab639521861bd88
arauz07 <aa@MacBook-Pro-de-Adrian.local>
2022-05-02 20:41:38 -0500

    Ticket#109661 - Público Objetivo y Leads3

---
+++ custom/Extension/application/Ext/TableDictionary/prospects_tel_telefonos_1.php
@@ -0,0 +1,5 @@
+<?php 
+ //WARNING: The contents of this file are auto-generated
+include('custom/metadata/prospects_tel_telefonos_1MetaData.php');
+
+?>
---
+++ custom/Extension/modules/Prospects/Ext/clients/mobile/layouts/subpanels/prospects_dire_direccion_1_Prospects.php
@@ -0,0 +1,10 @@
+<?php
+// created: 2022-05-03 01:30:00
+$viewdefs['Prospects']['mobile']['layout']['subpanels']['components'][] = array (
+  'layout' => 'subpanel',
+  'label' => 'LBL_PROSPECTS_DIRE_DIRECCION_1_FROM_DIRE_DIRECCION_TITLE',
+  'context' => 
+  array (
+    'link' => 'prospects_dire_direccion_1',
+  ),
+);
