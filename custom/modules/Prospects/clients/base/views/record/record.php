d5d556675478d5c62c2819b80f22d63d28cd90e2
Axel Flores <axelflores@MacBook-Pro-de-Axel.local>
2022-05-09 15:55:56 -0500

    Ticket#109661, CP-3198 - Ajuste PO

--- custom/modules/Prospects/clients/base/views/record/record.php
+++ custom/modules/Prospects/clients/base/views/record/record.php
@@ -49,20 +49,13 @@ array (
               ),
               1 => 
               array (
-                'type' => 'shareaction',
-                'name' => 'share',
-                'label' => 'LBL_RECORD_SHARE_BUTTON',
-                'acl_action' => 'view',
-              ),
-              2 => 
-              array (
                 'type' => 'pdfaction',
                 'name' => 'download-pdf',
                 'label' => 'LBL_PDF_VIEW',
                 'action' => 'download',
                 'acl_action' => 'view',
               ),
-              3 => 
+              2 => 
               array (
                 'type' => 'pdfaction',
                 'name' => 'email-pdf',
@@ -70,11 +63,11 @@ array (
                 'action' => 'email',
                 'acl_action' => 'view',
               ),
-              4 => 
+              3 => 
               array (
                 'type' => 'divider',
               ),
-              5 => 
+              4 => 
               array (
                 'name' => 'convert_po_to_Lead',
                 'type' => 'rowaction',
@@ -83,18 +76,25 @@ array (
                 'event' => 'button:convert_po_to_Lead:click',
                 'class' => 'btn_convertLeads',
               ),
+              5 => 
+              array (
+                'type' => 'divider',
+              ),
               6 => 
               array (
-                'type' => 'manage-subscription',
-                'name' => 'manage_subscription_button',
-                'label' => 'LBL_MANAGE_SUBSCRIPTIONS',
+                'type' => 'rowaction',
+                'event' => 'button:historical_summary_button:click',
+                'name' => 'historical_summary_button',
+                'label' => 'LBL_HISTORICAL_SUMMARY',
+                'acl_action' => 'view',
               ),
               7 => 
               array (
-                'type' => 'vcard',
-                'name' => 'vcard_button',
-                'label' => 'LBL_VCARD_DOWNLOAD',
-                'acl_action' => 'edit',
+                'type' => 'rowaction',
+                'event' => 'button:audit_button:click',
+                'name' => 'audit_button',
+                'label' => 'LNK_VIEW_CHANGE_LOG',
+                'acl_action' => 'view',
               ),
               8 => 
               array (
@@ -103,63 +103,10 @@ array (
               9 => 
               array (
                 'type' => 'rowaction',
-                'event' => 'button:find_duplicates_button:click',
-                'name' => 'find_duplicates_button',
-                'label' => 'LBL_DUP_MERGE',
-                'acl_action' => 'edit',
-              ),
-              10 => 
-              array (
-                'type' => 'rowaction',
-                'event' => 'button:duplicate_button:click',
-                'name' => 'duplicate_button',
-                'label' => 'LBL_DUPLICATE_BUTTON_LABEL',
-                'acl_module' => 'Prospects',
-                'acl_action' => 'create',
-              ),
-              11 => 
-              array (
-                'type' => 'rowaction',
-                'event' => 'button:historical_summary_button:click',
-                'name' => 'historical_summary_button',
-                'label' => 'LBL_HISTORICAL_SUMMARY',
-                'acl_action' => 'view',
-              ),
-              12 => 
-              array (
-                'type' => 'rowaction',
-                'event' => 'button:audit_button:click',
-                'name' => 'audit_button',
-                'label' => 'LNK_VIEW_CHANGE_LOG',
-                'acl_action' => 'view',
-              ),
-              13 => 
-              array (
-                'type' => 'divider',
-              ),
-              14 => 
-              array (
-                'type' => 'rowaction',
                 'event' => 'button:delete_button:click',
                 'name' => 'delete_button',
                 'label' => 'LBL_DELETE_BUTTON_LABEL',
                 'acl_action' => 'delete',
-              ),
-              15 => 
-              array (
-                'name' => 'send_survey',
-                'type' => 'rowaction',
-                'label' => 'Send Survey',
-                'acl_action' => 'send_survey',
-                'event' => 'button:send_survey:click',
-              ),
-              16 => 
-              array (
-                'name' => 'send_poll',
-                'type' => 'rowaction',
-                'label' => 'Send Poll',
-                'acl_action' => 'send_poll',
-                'event' => 'button:send_poll:click',
               ),
             ),
           ),
@@ -417,15 +364,12 @@ array (
               36 => 
               array (
                 'readonly' => false,
-                'name' => 'persona_c',
-                'studio' => 'visible',
-                'label' => 'LBL_PERSONA',
+                'name' => 'status_management_c',
+                'label' => 'LBL_STATUS_MANAGEMENT_C',
               ),
               37 => 
               array (
-                'readonly' => false,
-                'name' => 'status_management_c',
-                'label' => 'LBL_STATUS_MANAGEMENT_C',
+                'name' => 'prospects_prospects_1_name',
               ),
               38 => 
               array (
@@ -440,13 +384,6 @@ array (
                 'readonly' => false,
                 'name' => 'fecha_asignacion_c',
                 'label' => 'LBL_FECHA_ASIGNACION',
-              ),
-              41 => 
-              array (
-                'name' => 'prospects_prospects_1_name',
-              ),
-              42 => 
-              array (
               ),
             ),
           ),
@@ -754,5 +691,3 @@ array (
     ),
   ),
 );
-
-
