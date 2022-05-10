c40df6433b27b7232ec3292470965c033d6f3fdc
Eduardo Carrasco Beltran <eduardo.carrasco@tactos.com.mx>
2022-05-10 10:53:39 -0500

    8-06052022-CFDI

---
+++ custom/Extension/modules/Accounts/Ext/Vardefs/sugarfield_regimen_fiscal_sat_c.php
@@ -0,0 +1,56 @@
+<?php
+ // created: 2022-05-09 14:11:27
+$dictionary['Account']['fields']['regimen_fiscal_sat_c']['labelValue']='Régimen fiscal SAT';
+$dictionary['Account']['fields']['regimen_fiscal_sat_c']['required_formula']='';
+$dictionary['Account']['fields']['regimen_fiscal_sat_c']['readonly_formula']='';
+$dictionary['Account']['fields']['regimen_fiscal_sat_c']['visibility_grid']=array (
+  'trigger' => 'tipodepersona_c',
+  'values' => 
+  array (
+    'Persona Fisica' => 
+    array (
+      0 => '605',
+      1 => '606',
+      2 => '607',
+      3 => '608',
+      4 => '610',
+      5 => '611',
+      6 => '612',
+      7 => '614',
+      8 => '615',
+      9 => '616',
+      10 => '621',
+      11 => '625',
+      12 => '626',
+    ),
+    'Persona Fisica con Actividad Empresarial' => 
+    array (
+      0 => '605',
+      1 => '606',
+      2 => '607',
+      3 => '608',
+      4 => '610',
+      5 => '611',
+      6 => '612',
+      7 => '614',
+      8 => '615',
+      9 => '616',
+      10 => '621',
+      11 => '625',
+      12 => '626',
+    ),
+    'Persona Moral' => 
+    array (
+      0 => '601',
+      1 => '603',
+      2 => '610',
+      3 => '620',
+      4 => '622',
+      5 => '623',
+      6 => '624',
+      7 => '626',
+    ),
+  ),
+);
+
+ ?>
