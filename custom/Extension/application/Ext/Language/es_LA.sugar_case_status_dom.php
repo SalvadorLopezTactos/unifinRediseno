8ab6d93445781b083b8c9d9b63a90cd233d1532a
erickdjcruz <47753132+erickdjcruz@users.noreply.github.com>
2022-05-17 14:16:26 -0500

    3283_casos_incidencias1_20220517
    Archivos incidencias 1

--- custom/Extension/application/Ext/Language/es_LA.sugar_case_status_dom.php
+++ custom/Extension/application/Ext/Language/es_LA.sugar_case_status_dom.php
@@ -1,9 +1,10 @@
 <?php
- // created: 2022-05-08 23:31:35
+ // created: 2022-05-17 13:54:40
 
 $app_list_strings['case_status_dom']=array (
   1 => 'No iniciada',
   2 => 'En curso',
   3 => 'Completada',
   4 => 'Atrasada',
+  5 => 'Escalada',
 );
