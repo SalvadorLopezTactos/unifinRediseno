d837e4d11f97fbe680bd250d699adddd89910795
Salvador Lopez <salvador.lopez@tactos.com.mx>
2022-05-22 22:24:45 -0500

    Nuevos campos CFDI 4.0

---
+++ custom/Extension/modules/Accounts/Ext/Vardefs/sugarfield_fin_regimen_c.php
@@ -0,0 +1,9 @@
+<?php
+ // created: 2022-05-22 22:20:59
+$dictionary['Account']['fields']['fin_regimen_c']['labelValue']='Fecha fin del régimen';
+$dictionary['Account']['fields']['fin_regimen_c']['enforced']='';
+$dictionary['Account']['fields']['fin_regimen_c']['dependency']='';
+$dictionary['Account']['fields']['fin_regimen_c']['required_formula']='';
+$dictionary['Account']['fields']['fin_regimen_c']['readonly_formula']='';
+
+ ?>
---
+++ custom/Extension/modules/Accounts/Ext/Vardefs/sugarfield_ultimo_cambio_edo_c.php
@@ -0,0 +1,9 @@
+<?php
+ // created: 2022-05-22 22:19:27
+$dictionary['Account']['fields']['ultimo_cambio_edo_c']['labelValue']='Fecha del último cambio de estado';
+$dictionary['Account']['fields']['ultimo_cambio_edo_c']['enforced']='';
+$dictionary['Account']['fields']['ultimo_cambio_edo_c']['dependency']='';
+$dictionary['Account']['fields']['ultimo_cambio_edo_c']['required_formula']='';
+$dictionary['Account']['fields']['ultimo_cambio_edo_c']['readonly_formula']='';
+
+ ?>
