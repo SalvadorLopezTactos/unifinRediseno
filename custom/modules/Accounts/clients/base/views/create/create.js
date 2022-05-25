e5ca33693e2871ceb6645e30e4135789dfff44ec
Eduardo Carrasco Beltran <eduardo.carrasco@tactos.com.mx>
2022-05-25 16:59:40 -0500

    8-06052022-CFDI

--- custom/modules/Accounts/clients/base/views/create/create.js
+++ custom/modules/Accounts/clients/base/views/create/create.js
@@ -340,6 +340,14 @@
 
         //Oculta fecha de bloqueo para saber si el Origen se habilita
         $('[data-name="fecha_bloqueo_origen_c"]').hide();
+		
+		//Campos Denominación y Régimen Fiscal SAT
+        Object.entries(app.lang.getAppListStrings('actualiza_sat_list')).forEach(([key, value]) => {
+            if(value!=app.user.attributes.id) {
+				this.$('div[data-name=denominacion_c]').css("pointer-events", "none");
+				this.$('div[data-name=regimen_fiscal_sat_c]').css("pointer-events", "none");
+			}
+		});
     },
 
     initialize: function (options) {
