4bc2f943e64e31529ec43a5af3cd2deff5ceee10
arauz07 <aa@MacBook-Pro-de-Adrian.local>
2022-05-27 13:04:43 -0500

    Ticket 138-03052022 Ajuste para CFDI 4.0 -1

--- custom/Extension/modules/ANLZT_analizate/Ext/Language/es_LA.lang.php
+++ custom/Extension/modules/ANLZT_analizate/Ext/Language/es_LA.lang.php
@@ -2,3 +2,4 @@
 // WARNING: The contents of this file are auto-generated.
 $mod_strings['LBL_TIPO_REGISTRO_CUENTA_C'] = 'Tipo de Cuenta';
 $mod_strings['LBL_JSON_ROBIN_C'] = 'JSON ROBINA';
+$mod_strings['LBL_JSON_ROBINA_C'] = 'JSON ROBINA';
---
+++ custom/Extension/modules/ANLZT_analizate/Ext/Vardefs/sugarfield_json_robina_c.php
@@ -0,0 +1,14 @@
+<?php
+ // created: 2022-05-25 22:16:13
+$dictionary['ANLZT_analizate']['fields']['json_robina_c']['labelValue']='JSON ROBINA';
+$dictionary['ANLZT_analizate']['fields']['json_robina_c']['full_text_search']=array (
+  'enabled' => '0',
+  'boost' => '1',
+  'searchable' => false,
+);
+$dictionary['ANLZT_analizate']['fields']['json_robina_c']['enforced']='';
+$dictionary['ANLZT_analizate']['fields']['json_robina_c']['dependency']='';
+$dictionary['ANLZT_analizate']['fields']['json_robina_c']['required_formula']='';
+$dictionary['ANLZT_analizate']['fields']['json_robina_c']['readonly_formula']='';
+
+ ?>
