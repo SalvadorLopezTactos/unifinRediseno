ae4bcc8c4d2a55d799a0cdb95e3f9ff33c52dd0b
Axel Flores <axelflores@MacBook-Pro-de-Axel.local>
2022-05-31 03:03:18 -0500

    Ticket #138-03052022, CP-3293 - Actualización json robina

--- custom/modules/ANLZT_analizate/analizate_hooks.php
+++ custom/modules/ANLZT_analizate/analizate_hooks.php
@@ -170,4 +170,91 @@ class analizate_hooks  {
             }
         }
     }
+
+    public function ActualizaRobina($bean = null, $event = null, $args = null) {
+        //Valida si existe información de Robina
+        if(!empty($bean->json_robina_c)){
+            //Recupera cuenta
+            //$GLOBALS['log']->fatal($bean->json_robina_c);
+            $jsonObject = [];
+            $cambios = 0;
+            $guardarCambios = 0;
+            try{
+                $jsonObject = json_decode($bean->json_robina_c);
+                //Fecha: Inicio Operaciones
+                if(isset($jsonObject->startedOperationsAt) && !empty($jsonObject->startedOperationsAt)){
+                    $inicioOperacionesString = strtotime($jsonObject->startedOperationsAt);
+                    $inicioOperacionesDate = date('Y-m-d',$inicioOperacionesString);
+                    $cambios++;
+                    //$GLOBALS['log']->fatal($inicioOperacionesDate);
+                }
+                //Fecha: Inicio Régimen
+                if(isset($jsonObject->taxRegimes[0]->startDate) && !empty($jsonObject->taxRegimes[0]->startDate)){
+                    $inicioRegimenString = strtotime($jsonObject->taxRegimes[0]->startDate);
+                    $inicioRegimenDate = date('Y-m-d',$inicioRegimenString);
+                    $cambios++;
+                    //$GLOBALS['log']->fatal($inicioRegimenDate);
+                }
+                //Fecha: Fin Régimen
+                if(isset($jsonObject->taxRegimes[0]->startDate) && !empty($jsonObject->taxRegimes[0]->startDate)){
+                    $finRegimenString = strtotime($jsonObject->taxRegimes[0]->startDate);
+                    $finRegimenDate = date('Y-m-d',$finRegimenString);
+                    $cambios++;
+                    //$GLOBALS['log']->fatal($finRegimenDate);
+                }
+                //Fecha: Último cambio Edo
+                if(isset($jsonObject->statusUpdatedAt) && !empty($jsonObject->statusUpdatedAt)){
+                    $ultimoCambioEdoString = strtotime($jsonObject->statusUpdatedAt);
+                    $ultimoCambioEdoDate = date('Y-m-d',$ultimoCambioEdoString);
+                    $cambios++;
+                    //$GLOBALS['log']->fatal($ultimoCambioEdoDate);
+                }
+                //Status
+                if(isset($jsonObject->status) && !empty($jsonObject->status)){
+                    $status = $jsonObject->status;
+                    $cambios++;
+                    //$GLOBALS['log']->fatal($status);
+                }
+            } catch (Exception $e) {
+                $GLOBALS['log']->fatal($e->getMessage());
+            }
+            if($cambios>0){
+                $beanCuenta = BeanFactory::retrieveBean('Accounts', $bean->anlzt_analizate_accountsaccounts_ida, array('disable_row_level_security' => true));
+                //Valida cambios
+                if(isset($inicioOperacionesDate) && !empty($inicioOperacionesDate) && $beanCuenta->inicio_operaciones_c!= $inicioOperacionesDate ){
+                    $beanCuenta->inicio_operaciones_c = $inicioOperacionesDate;
+                    $guardarCambios ++;
+                    // $GLOBALS['log']->fatal('GC 1');
+                }
+                if(isset($inicioRegimenDate) && !empty($inicioRegimenDate) && $beanCuenta->inicio_regimen_c!= $inicioRegimenDate ){
+                    $beanCuenta->inicio_regimen_c = $inicioRegimenDate;
+                    $guardarCambios ++;
+                    // $GLOBALS['log']->fatal('GC 2');
+                }
+                if(isset($finRegimenDate) && !empty($finRegimenDate) && $beanCuenta->fin_regimen_c!= $finRegimenDate ){
+                    $beanCuenta->fin_regimen_c = $finRegimenDate;
+                    $guardarCambios ++;
+                    // $GLOBALS['log']->fatal('GC 3');
+                }
+                if(isset($ultimoCambioEdoDate) && !empty($ultimoCambioEdoDate) && $beanCuenta->ultimo_cambio_edo_c!= $ultimoCambioEdoDate ){
+                    $beanCuenta->ultimo_cambio_edo_c = $ultimoCambioEdoDate;
+                    $guardarCambios ++;
+                    // $GLOBALS['log']->fatal('GC 4');
+                }
+                if(isset($status) && !empty($status) && strtoupper($beanCuenta->estatus_padron_c)!= strtoupper($status) ){
+                    $beanCuenta->estatus_padron_c = $status;
+                    $guardarCambios ++;
+                    // $GLOBALS['log']->fatal('GC 5');
+                }
+
+                if($guardarCambios>0){
+                    $GLOBALS['log']->fatal('Guarda cambios Robina');
+                    $beanCuenta->save();
+
+                }
+            }
+
+        }
+
+    }
 }
