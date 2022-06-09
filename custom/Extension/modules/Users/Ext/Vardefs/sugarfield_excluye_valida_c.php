3edef194bb7755581143787836ad52e6ba784da5
Eduardo Carrasco Beltran <eduardo.carrasco@tactos.com.mx>
2022-06-09 13:16:10 -0500

    176-16052022-Estructura Productos

--- custom/Extension/modules/Users/Ext/Vardefs/sugarfield_excluye_valida_c.php
+++ custom/Extension/modules/Users/Ext/Vardefs/sugarfield_excluye_valida_c.php
@@ -1,6 +1,6 @@
 <?php
- // created: 2022-06-09 12:36:46
-$dictionary['User']['fields']['excluye_valida_c']['labelValue']='Alta sol. Op - No negocio';
+ // created: 2022-06-09 13:13:18
+$dictionary['User']['fields']['excluye_valida_c']['labelValue']='Alta Sol. Op - No negocio';
 $dictionary['User']['fields']['excluye_valida_c']['enforced']='';
 $dictionary['User']['fields']['excluye_valida_c']['dependency']='';
 $dictionary['User']['fields']['excluye_valida_c']['readonly_formula']='';
