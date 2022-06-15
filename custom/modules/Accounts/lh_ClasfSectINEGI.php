ff4725870e3411f4bdcb8bb76325aaa94fdda3b9
Axel Flores <axelflores@MacBook-Pro-de-Axel.local>
2022-06-14 19:17:11 -0500

    Fix20220616 - Ajuste para seteo de clasificación sectorial

--- custom/modules/Accounts/lh_ClasfSectINEGI.php
+++ custom/modules/Accounts/lh_ClasfSectINEGI.php
@@ -9,22 +9,23 @@ class clase_ClasfSectorialINEGI
         $actEco = $bean->actividadeconomica_c;
         if (!empty($actEco) && !empty($idCuenta)) {
             $GLOBALS['log']->fatal("INEGI: " . $idCuenta);
-			global $db;
-			$query = <<<SQL
-SELECT * FROM catalogo_clasificacion_sectorial WHERE id_actividad_economica_cnbv = '{$actEco}'
-SQL;
-			$queryResult = $db->query($query);
-			$row = $db->fetchByAssoc($queryResult);
-			$bean->subsectoreconomico_c = $row['id_subsector_economico_cnbv'];
-			$bean->sectoreconomico_c = $row['id_sector_economico_cnbv'];
-			$bean->tct_macro_sector_ddw_c = $row['id_macro_sector_cnbv'];
-			$beanINEGI = BeanFactory::retrieveBean('tct02_Resumen', $idCuenta);
-			$beanINEGI->inegi_clase_c = $row['id_clase_inegi'];
-			$beanINEGI->inegi_subrama_c = $row['id_subrama_inegi'];
-			$beanINEGI->inegi_rama_c = $row['id_rama_inegi'];
-			$beanINEGI->inegi_subsector_c = $row['id_subsector_inegi'];
-			$beanINEGI->inegi_sector_c = $row['id_sector_inegi'];
-			$beanINEGI->inegi_macro_c = $row['id_macro_inegi'];
+	          global $db;
+            $query ="SELECT * FROM catalogo_clasificacion_sectorial WHERE id_actividad_economica_cnbv = '{$actEco}';";
+      			$queryResult = $db->query($query);
+      			$row = $db->fetchByAssoc($queryResult);
+      			$bean->subsectoreconomico_c = $row['id_subsector_economico_cnbv'];
+      			$bean->sectoreconomico_c = $row['id_sector_economico_cnbv'];
+      			$bean->tct_macro_sector_ddw_c = $row['id_macro_sector_cnbv'];
+            $updateS = "UPDATE accounts_cstm SET subsectoreconomico_c='{$bean->subsectoreconomico_c}', sectoreconomico_c='{$bean->sectoreconomico_c}', tct_macro_sector_ddw_c='{$bean->tct_macro_sector_ddw_c}' WHERE id_c='{$bean->id}';";
+            //$GLOBALS['log']->fatal("UPDATE: " . $updateS);
+            $updateResult = $db->query($updateS);
+      			$beanINEGI = BeanFactory::retrieveBean('tct02_Resumen', $idCuenta);
+      			$beanINEGI->inegi_clase_c = $row['id_clase_inegi'];
+      			$beanINEGI->inegi_subrama_c = $row['id_subrama_inegi'];
+      			$beanINEGI->inegi_rama_c = $row['id_rama_inegi'];
+      			$beanINEGI->inegi_subsector_c = $row['id_subsector_inegi'];
+      			$beanINEGI->inegi_sector_c = $row['id_sector_inegi'];
+      			$beanINEGI->inegi_macro_c = $row['id_macro_inegi'];
             $beanINEGI->save();
             $GLOBALS['log']->fatal("FINALIZA HOOK CLASIFICACION SECTORIAL ");
         }
