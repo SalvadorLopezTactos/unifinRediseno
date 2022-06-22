47da4822dfbcfb96cad696fc826868a85d4f5e30
Salvador Lopez <salvador.lopez@tactos.com.mx>
2022-06-21 22:32:02 -0500

    Vista Kanban: Suma de Solicitudes

--- custom/clients/base/api/ClientManager.php
+++ custom/clients/base/api/ClientManager.php
@@ -40,9 +40,12 @@ class ClientManager extends SugarApi
     public function getInfoKanban($api, $args){
         global $sugar_config,$db,$current_user;
         $id_usuario=$current_user->id;
+        //$id_usuario='cb6dfd0a-257a-5977-db84-599b31c3e22b';
 
         $query = <<<SQL
-			SELECT lc.nombre_c nombre,
+			SELECT l.id id_registro,
+        'Leads' modulo,
+        lc.nombre_c nombre,
         lc.apellido_paterno_c apellido,
         lc.nombre_empresa_c nombre_empresa,
         lc.tipo_registro_c tipo_registro,
@@ -56,7 +59,9 @@ class ClientManager extends SugarApi
         AND l.assigned_user_id ='{$id_usuario}'
         AND l.deleted=0
         UNION
-        SELECT ac.primernombre_c,
+        SELECT a.id,
+        'Accounts' modulo,
+        ac.primernombre_c,
         ac.apellidopaterno_c,
         ac.razonsocial_c,
         ac.tipo_registro_cuenta_c,
@@ -81,24 +86,32 @@ SQL;
 
         $registros_interesado=array();
         $total_prospecto_interesado=0;
+        $monto_prospecto_interesado=0;
 
         $registros_int_expediente=array();
         $total_int_expediente=0;
+        $monto_int_expediente=0;
 
         $registros_credito=array();
         $total_prospecto_credito=0;
+        $monto_prospecto_credito=0;
 
         $registros_cliente_linea_sin_operar=array();
         $total_cliente_linea_sin_operar=0;
+        $monto_cliente_linea_sin_operar=0;
 
         $registros_cliente_activo=array();
         $total_cliente_activo=0;
+        $monto_cliente_activo=0;
 
         $registros_cliente_perdido=array();
         $total_cliente_perdido=0;
+        $monto_cliente_perdido=0;
         
         while ($row = $db->fetchByAssoc($result)) {
-          $tipo=$row['tipo_registro']; 
+          $id=$row['id_registro'];
+          $modulo=$row['modulo'];
+          $tipo=$row['tipo_registro'];
           $subtipo=$row['subtipo_registro'];
           $nombre=$row['nombre']." ".$row['apellido'];
           $date_modified=$row['date_modified'];
@@ -107,6 +120,7 @@ SQL;
             if($tipo=='1' && $subtipo=='1'){
                 $total_leads_sin_contactar+=1;
                 $array_lead_sin_contactar=array(
+                    "Id"=>$id,
                     "Nombre"=>($nombre_empresa=="" || $nombre_empresa==null) ? $nombre:$nombre_empresa,
                     "Dias_Etapa"=>"2",
                     "Favorito"=>1,
@@ -121,6 +135,7 @@ SQL;
             if($tipo=='2' && $subtipo=='2'){
                 $total_prospecto_contactado+=1;
                 $array_prospecto_contactado=array(
+                    "Id"=>$id,
                     "Nombre"=>($nombre_empresa=="" || $nombre_empresa==null) ? $nombre:$nombre_empresa,
                     "Dias_Etapa"=>"2",
                     "Favorito"=>1,
@@ -134,8 +149,15 @@ SQL;
             //Prospecto Interesado
             if($tipo=='2' && $subtipo=='7'){
                 $total_prospecto_interesado+=1;
+                //Obteniendo solicitudes relacionadas al usuario logueado sin tomar en cuenta las canceladas ni rechazadas
+                // tct_etapa_ddw_c - R, estatus_c - R, K, CM
+                //array('monto_total'=>$monto,'monto_cuenta'=>$monto_cuenta)
+                $montos_prospecto_interesado=$this->getSolicitudes($modulo,$id_usuario,$id,$monto_prospecto_interesado);
+                $monto_prospecto_interesado=$montos_prospecto_interesado['monto_total'];
                 $array_prospecto_interesado=array(
+                    "Id"=>$id,
                     "Nombre"=>($nombre_empresa=="" || $nombre_empresa==null) ? $nombre:$nombre_empresa,
+                    "Monto_Cuenta"=>$montos_prospecto_interesado['monto_cuenta'],
                     "Dias_Etapa"=>"2",
                     "Favorito"=>1,
                     "Date_Modified"=>$date_modified,
@@ -148,13 +170,19 @@ SQL;
             //Prospecto Integración de Expediente
             if($tipo=='2' && $subtipo=='8'){
                 $total_int_expediente+=1;
+                //Obteniendo solicitudes relacionadas al usuario logueado sin tomar en cuenta las canceladas ni rechazadas
+                // tct_etapa_ddw_c - R, estatus_c - R, K, CM
+                $montos_int_expediente=$this->getSolicitudes($modulo,$id_usuario,$id,$monto_int_expediente);
+                $monto_int_expediente=$montos_int_expediente['monto_total'];
                 $array_prospecto_int_expediente=array(
+                    "Id"=>$id,
                     "Nombre"=>($nombre_empresa=="" || $nombre_empresa==null) ? $nombre:$nombre_empresa,
+                    "Monto_Cuenta"=>$montos_int_expediente['monto_cuenta'],
                     "Dias_Etapa"=>"2",
                     "Favorito"=>1,
                     "Date_Modified"=>$date_modified,
                     "Preview"=>array("Info Preview")
-                ); 
+                );
                 
                 $registros_int_expediente[]=$array_prospecto_int_expediente;
             }
@@ -162,13 +190,20 @@ SQL;
             //Prospecto en Crédito
             if($tipo=='2' && $subtipo=='9'){
                 $total_prospecto_credito+=1;
+                //Obteniendo solicitudes relacionadas al usuario logueado sin tomar en cuenta las canceladas ni rechazadas
+                // tct_etapa_ddw_c - R, estatus_c - R, K, CM
+                $montos_prospecto_credito=$this->getSolicitudes($modulo,$id_usuario,$id,$monto_prospecto_credito);
+                $monto_prospecto_credito=$montos_prospecto_credito['monto_total'];
                 $array_prospecto_credito=array(
+                    "Id"=>$id,
                     "Nombre"=>($nombre_empresa=="" || $nombre_empresa==null) ? $nombre:$nombre_empresa,
+                    "Monto_Cuenta"=>$montos_prospecto_credito['monto_cuenta'],
                     "Dias_Etapa"=>"2",
                     "Favorito"=>1,
                     "Date_Modified"=>$date_modified,
                     "Preview"=>array("Info Preview")
-                ); 
+                );
+                
                 
                 $registros_credito[]=$array_prospecto_credito;
             }
@@ -193,13 +228,19 @@ SQL;
             //Cliente Activo
             if($tipo=='2' && $subtipo=='11'){
                 $total_cliente_activo+=1;
+                //Obteniendo solicitudes relacionadas al usuario logueado sin tomar en cuenta las canceladas ni rechazadas
+                // tct_etapa_ddw_c - R, estatus_c - R, K, CM
+                $montos_cliente_activo=$this->getSolicitudes($modulo,$id_usuario,$id,$monto_cliente_activo);
+                $monto_cliente_activo=$montos_cliente_activo['monto_total'];
                 $array_cliente_activo=array(
+                    "Id"=>$id,
                     "Nombre"=>($nombre_empresa=="" || $nombre_empresa==null) ? $nombre:$nombre_empresa,
+                    "Monto_Cuenta"=>$montos_cliente_activo['monto_cuenta'],
                     "Dias_Etapa"=>"2",
                     "Favorito"=>1,
                     "Date_Modified"=>$date_modified,
                     "Preview"=>array("Info Preview")
-                ); 
+                );
                 
                 $registros_cliente_activo[]=$array_cliente_activo;
             }
@@ -207,13 +248,17 @@ SQL;
             //Cliente Perdido
             if($tipo=='2' && $subtipo=='17'){
                 $total_cliente_perdido+=1;
+                $montos_cliente_perdido=$this->getSolicitudes($modulo,$id_usuario,$id,$monto_cliente_perdido);
+                $monto_cliente_perdido=$montos_cliente_perdido['monto_total'];
                 $array_cliente_perdido=array(
+                    "Id"=>$id,
                     "Nombre"=>($nombre_empresa=="" || $nombre_empresa==null) ? $nombre:$nombre_empresa,
+                    "Monto_Cuenta"=>$montos_cliente_perdido['monto_cuenta'],
                     "Dias_Etapa"=>"2",
                     "Favorito"=>1,
                     "Date_Modified"=>$date_modified,
                     "Preview"=>array("Info Preview")
-                ); 
+                );
                 
                 $registros_cliente_perdido[]=$array_cliente_perdido;
             }
@@ -232,81 +277,79 @@ SQL;
             ),
             "Prospecto_Interesado"=>array(
                 "Total_Registros"=>$total_prospecto_interesado,
-                "Total_Monto"=>0,
+                "Total_Monto"=>$monto_prospecto_interesado,
                 "Registros"=>$registros_interesado
             ),
             "Prospecto_Integracion_Expediente"=>array(
                 "Total_Registros"=>$total_int_expediente,
-                "Total_Monto"=>0,
+                "Total_Monto"=>$monto_int_expediente,
                 "Registros"=>$registros_int_expediente
             ),
             "Prospecto_Credito"=>array(
                 "Total_Registros"=>$total_prospecto_credito,
-                "Total_Monto"=>0,
+                "Total_Monto"=>$monto_prospecto_credito,
                 "Registros"=>$registros_credito
             ),
             "Cliente_Linea_Sin_Operar"=>array(
                 "Total_Registros"=>$total_cliente_linea_sin_operar,
-                "Total_Monto"=>0,
+                "Total_Monto"=>$monto_cliente_linea_sin_operar,
                 "Registros"=>$registros_cliente_linea_sin_operar
             ),
             "Cliente_Activo"=>array(
                 "Total_Registros"=>$total_cliente_activo,
-                "Total_Monto"=>0,
+                "Total_Monto"=>$monto_cliente_activo,
                 "Registros"=>$registros_cliente_activo
             ),
             "Cliente_Perdido"=>array(
                 "Total_Registros"=>$total_cliente_perdido,
-                "Total_Monto"=>0,
+                "Total_Monto"=>$monto_cliente_perdido,
                 "Registros"=>$registros_cliente_perdido
             ),
         );
 
-
-        /*
-        $response=array(
-            "Lead_Sin_Contactar"=>array(
-                "Total_Registros"=>"180",
-                "Total_Monto"=>"20000",
-                "Registros"=>array(
-                    array(
-                        "Nombre"=>"Lead 1",
-                        "Dias_Etapa"=>"2",
-                        "Favorito"=>1,
-                        "Preview"=>array("Info Preview")
-                    ),
-                    array(
-                        "Nombre"=>"Lead 2",
-                        "Dias_Etapa"=>"2",
-                        "Favorito"=>0,
-                        "Preview"=>array("Info Preview")
-                    )
-                )
-            ),
-            "Prospecto_Contactado"=>array(
-                "Total_Registros"=>"180",
-                "Total_Monto"=>"20000",
-                "Registros"=>array(
-                    array(
-                        "Nombre"=>"Lead 1",
-                        "Dias_Etapa"=>"2",
-                        "Favorito"=>"true",
-                        "Preview"=>array("Info Preview")
-                    ),
-                    array(
-                        "Nombre"=>"Lead 2",
-                        "Dias_Etapa"=>"2",
-                        "Favorito"=>"true",
-                        "Preview"=>array("Info Preview")
-                    )
-                )
-            )
-            
-        );
-        */
-
         return $response;
 
+    }
+
+    public function getSolicitudes($modulo,$id_usuario,$id_cuenta,$monto){
+        global $db;
+        $array_montos=array();
+        $monto_cuenta=0;
+
+        if($modulo=='Accounts'){
+            $querySolicitudes= <<<SQL
+        SELECT ac.primernombre_c, 
+ac.apellidopaterno_c,
+ac.razonsocial_c,
+opp.name,
+oppc.monto_c,
+oppc.tct_etapa_ddw_c,
+oppc.estatus_c
+FROM accounts a
+INNER JOIN accounts_cstm ac ON a.id=ac.id_c
+INNER JOIN accounts_opportunities ao ON a.id=ao.account_id
+INNER JOIN opportunities opp ON opp.id=ao.opportunity_id
+INNER JOIN opportunities_cstm oppc ON opp.id=oppc.id_c
+WHERE opp.assigned_user_id='{$id_usuario}'
+AND a.id='{$id_cuenta}'
+AND oppc.tct_etapa_ddw_c != 'R' AND oppc.estatus_c NOT IN('R','K','CM')
+AND opp.deleted=0 AND ao.deleted=0 AND a.deleted=0
+ORDER BY opp.date_modified DESC;
+SQL;
+            $resultSolicitudes = $db->query($querySolicitudes);
+            $GLOBALS['log']->fatal('La cuenta: '.$id_cuenta.' tiene '.$resultSolicitudes->num_rows);
+            if($resultSolicitudes->num_rows>0){
+
+                while ($fila = $db->fetchByAssoc($resultSolicitudes)) {
+                    $monto_cuenta+=floatval($fila['monto_c']);
+                    $monto+=floatval($fila['monto_c']);
+                }
+            }
+        }
+
+        $GLOBALS['log']->fatal(print_r(array('monto_total'=>$monto,'monto_cuenta'=>$monto_cuenta),true));
+
+        return array('monto_total'=>$monto,'monto_cuenta'=>$monto_cuenta);
     }
 
 }
