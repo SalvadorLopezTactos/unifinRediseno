cb0d30e6ffed3da6392a96cd2841336aae49a763
Eduardo Carrasco Beltran <eduardo.carrasco@tactos.com.mx>
2022-07-14 14:27:55 -0500

    167-06072022-Relación Seguros

--- custom/Extension/modules/Rel_Relaciones/Ext/Vardefs/sugarfield_tipodecontacto.php
+++ custom/Extension/modules/Rel_Relaciones/Ext/Vardefs/sugarfield_tipodecontacto.php
@@ -1,5 +1,6 @@
 <?php
- // created: 2019-04-15 17:19:11
-$dictionary['Rel_Relaciones']['fields']['tipodecontacto']['dependency'] = false;
-$dictionary['Rel_Relaciones']['fields']['tipodecontacto']['full_text_search']['boost'] = 1;
+ // created: 2022-07-13 10:02:14
+$dictionary['Rel_Relaciones']['fields']['tipodecontacto']['dependency']=false;
+$dictionary['Rel_Relaciones']['fields']['tipodecontacto']['hidemassupdate']=false;
 
+ ?>
