59500868aac7afce04afa4fad5cdf693b002ce77
arauz07 <aa@MacBook-Pro-de-Adrian.local>
2022-07-14 18:26:20 -0500

    Ticket 135-27052022 Seguros - Migracion Inter a Dynamics CRM

--- custom/Extension/application/Ext/Language/es_LA.sugar_dynamics_tipo_list.php
+++ custom/Extension/application/Ext/Language/es_LA.sugar_dynamics_tipo_list.php
@@ -1,8 +1,8 @@
 <?php
- // created: 2022-07-14 19:10:45
+ // created: 2022-07-14 23:25:44
 
 $app_list_strings['dynamics_tipo_list']=array (
   '' => '',
-  1 => 'Venta Nueva',
+  1 => 'Venta nueva',
   2 => 'Venta Cruzada',
 );
