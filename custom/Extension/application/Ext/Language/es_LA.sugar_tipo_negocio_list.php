65ce279d56de6b670d18197eec7fad3f6eca53d1
arauz07 <aa@MacBook-Pro-de-Adrian.local>
2022-07-14 17:44:17 -0500

    Ticket 135-27052022 Seguros - Migracion Inter a Dynamics CRM

---
+++ custom/Extension/application/Ext/Language/es_LA.sugar_tipo_negocio_list.php
@@ -0,0 +1,12 @@
+<?php
+ // created: 2022-07-14 19:53:47
+
+$app_list_strings['tipo_negocio_list']=array (
+  1 => 'Ramo / Autos',
+  2 => 'Beneficios',
+  3 => 'Daños',
+  4 => 'Fianzas',
+  '' => '',
+  5 => 'Accidentes y enfermedades',
+  6 => 'Vida',
+);
--- custom/Extension/modules/S_seguros/Ext/Vardefs/sugarfield_subramos_c.php
+++ custom/Extension/modules/S_seguros/Ext/Vardefs/sugarfield_subramos_c.php
@@ -1,7 +1,9 @@
 <?php
- // created: 2021-02-15 12:12:07
+ // created: 2022-07-14 20:31:37
 $dictionary['S_seguros']['fields']['subramos_c']['labelValue']='Subramos';
 $dictionary['S_seguros']['fields']['subramos_c']['dependency']='';
+$dictionary['S_seguros']['fields']['subramos_c']['required_formula']='';
+$dictionary['S_seguros']['fields']['subramos_c']['readonly_formula']='';
 $dictionary['S_seguros']['fields']['subramos_c']['visibility_grid']='';
 
  ?>
