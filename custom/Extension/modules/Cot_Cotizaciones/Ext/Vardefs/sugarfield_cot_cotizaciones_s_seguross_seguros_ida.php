39f1940162ecd120d0f7af458a59cd02e1ad00fd
arauz07 <aa@MacBook-Pro-de-Adrian.local>
2022-07-19 10:37:46 -0500

    Ticket 135-27052022 Seguros - Migracion Inter a Dynamics CRM-OBSERVACIONES2

---
+++ custom/Extension/modules/Cot_Cotizaciones/Ext/Vardefs/sugarfield_cot_cotizaciones_s_seguross_seguros_ida.php
@@ -0,0 +1,19 @@
+<?php
+ // created: 2022-07-19 15:33:42
+$dictionary['Cot_Cotizaciones']['fields']['cot_cotizaciones_s_seguross_seguros_ida']['name']='cot_cotizaciones_s_seguross_seguros_ida';
+$dictionary['Cot_Cotizaciones']['fields']['cot_cotizaciones_s_seguross_seguros_ida']['type']='id';
+$dictionary['Cot_Cotizaciones']['fields']['cot_cotizaciones_s_seguross_seguros_ida']['source']='non-db';
+$dictionary['Cot_Cotizaciones']['fields']['cot_cotizaciones_s_seguross_seguros_ida']['vname']='LBL_COT_COTIZACIONES_S_SEGUROS_FROM_COT_COTIZACIONES_TITLE_ID';
+$dictionary['Cot_Cotizaciones']['fields']['cot_cotizaciones_s_seguross_seguros_ida']['id_name']='cot_cotizaciones_s_seguross_seguros_ida';
+$dictionary['Cot_Cotizaciones']['fields']['cot_cotizaciones_s_seguross_seguros_ida']['link']='cot_cotizaciones_s_seguros';
+$dictionary['Cot_Cotizaciones']['fields']['cot_cotizaciones_s_seguross_seguros_ida']['table']='s_seguros';
+$dictionary['Cot_Cotizaciones']['fields']['cot_cotizaciones_s_seguross_seguros_ida']['module']='S_seguros';
+$dictionary['Cot_Cotizaciones']['fields']['cot_cotizaciones_s_seguross_seguros_ida']['rname']='id';
+$dictionary['Cot_Cotizaciones']['fields']['cot_cotizaciones_s_seguross_seguros_ida']['reportable']=false;
+$dictionary['Cot_Cotizaciones']['fields']['cot_cotizaciones_s_seguross_seguros_ida']['side']='right';
+$dictionary['Cot_Cotizaciones']['fields']['cot_cotizaciones_s_seguross_seguros_ida']['massupdate']=false;
+$dictionary['Cot_Cotizaciones']['fields']['cot_cotizaciones_s_seguross_seguros_ida']['duplicate_merge']='disabled';
+$dictionary['Cot_Cotizaciones']['fields']['cot_cotizaciones_s_seguross_seguros_ida']['hideacl']=true;
+$dictionary['Cot_Cotizaciones']['fields']['cot_cotizaciones_s_seguross_seguros_ida']['audited']=false;
+
+ ?>
