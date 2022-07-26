d9c481fead4604d9046545a717a0ca3e418549f5
Eduardo Carrasco Beltran <eduardo.carrasco@tactos.com.mx>
2022-07-26 11:57:03 -0500

    47-11052022-Lenia

---
+++ custom/Extension/modules/Users/Ext/Vardefs/sugarfield_lenia_c.php
@@ -0,0 +1,8 @@
+<?php
+ // created: 2022-07-26 09:15:20
+$dictionary['User']['fields']['lenia_c']['labelValue']='Videoconferencia Lenia';
+$dictionary['User']['fields']['lenia_c']['enforced']='';
+$dictionary['User']['fields']['lenia_c']['dependency']='';
+$dictionary['User']['fields']['lenia_c']['readonly_formula']='';
+
+ ?>
