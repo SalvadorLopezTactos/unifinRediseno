cb287b3a17841256a20139824998d187bd1048a1
erickdjcruz <47753132+erickdjcruz@users.noreply.github.com>
2022-08-03 09:40:05 -0500

    3250_clintm_complmento_20220803
    pba

--- custom/clients/base/views/planned-activities-ctc/planned-activities-ctc.php
+++ custom/clients/base/views/planned-activities-ctc/planned-activities-ctc.php
@@ -142,7 +142,7 @@ $viewdefs['base']['view']['planned-activities-ctc'] = array(
             'order_by' => 'date_start:asc',
             'record_date' => 'date_start',
             'row_actions' => array(
-                array(
+                /*array(
                     'type' => 'rowaction',
                     'icon' => 'fa-times-circle',
                     'css_class' => 'btn btn-mini',
@@ -150,7 +150,7 @@ $viewdefs['base']['view']['planned-activities-ctc'] = array(
                     'target' => 'view',
                     'tooltip' => 'LBL_PLANNED_ACTIVITIES_DASHLET_HELD_ACTIVITY',
                     'acl_action' => 'edit',
-                ),
+                ),*/
                 array(
                     'type' => 'unlink-action',
                     'icon' => 'fa-chain-broken',
