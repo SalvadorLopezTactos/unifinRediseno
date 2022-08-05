af9ce5668d66ab1eeef82a6f8a6fd5c04e6ed8de
Salvador Lopez <salvador.lopez@tactos.com.mx>
2022-08-05 14:42:38 -0500

    Pendiente, deshabilitación de integración con Mambu

--- custom/Extension/modules/cta_cuentas_bancarias/Ext/LogicHooks/mambu_hooks.php
+++ custom/Extension/modules/cta_cuentas_bancarias/Ext/LogicHooks/mambu_hooks.php
@@ -6,13 +6,15 @@
  * Time: 03:23 PM
  */
 
-$hook_array['after_save'][] = Array(
+/*
+ $hook_array['after_save'][] = Array(
     1,
     'Habilitar el envío y actualización de cuentas bancarias a Cuentas en Mambu',//Just a quick comment about the logic of it
     'custom/modules/cta_cuentas_bancarias/CBMambu_hooks.php', //path to the logic hook
     'CBMambu_hook', // name of the class
     'Envia_mambu' // name of the function.
 );
+*/
 
 $hook_array['before_save'][] = Array(
     1,
