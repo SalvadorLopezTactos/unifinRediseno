f843a9ea5b7a23f5666f1e889dc157682514ca6a
erickdjcruz <47753132+erickdjcruz@users.noreply.github.com>
2022-08-24 02:07:12 -0500

    3671_val_banca_servicio_20220824
    campos nuevos y servicio nuevo

---
+++ custom/Extension/modules/cta_cuentas_bancarias/Ext/Vardefs/sugarfield_validada_c.php
@@ -0,0 +1,8 @@
+<?php
+ // created: 2022-08-23 12:51:20
+$dictionary['cta_cuentas_bancarias']['fields']['validada_c']['labelValue']='Cuenta validada';
+$dictionary['cta_cuentas_bancarias']['fields']['validada_c']['enforced']='';
+$dictionary['cta_cuentas_bancarias']['fields']['validada_c']['dependency']='';
+$dictionary['cta_cuentas_bancarias']['fields']['validada_c']['readonly_formula']='';
+
+ ?>
