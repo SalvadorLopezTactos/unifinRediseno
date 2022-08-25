f786c5434bfba422830e3c8401e3a933176ec522
Eduardo Carrasco Beltran <eduardo.carrasco@tactos.com.mx>
2022-08-25 10:57:52 -0500

    122-24082022-Quitar validación para Seguros

--- custom/Extension/application/Ext/Language/es_LA.sugar_full_access_accounts_list.php
+++ custom/Extension/application/Ext/Language/es_LA.sugar_full_access_accounts_list.php
@@ -1,5 +1,5 @@
 <?php
- // created: 2022-08-25 10:09:00
+ // created: 2022-08-25 10:56:05
 
 $app_list_strings['full_access_accounts_list']=array (
   '' => '',
@@ -7,7 +7,7 @@ $app_list_strings['full_access_accounts_list']=array (
   2 => 'Acceso Total Consulta',
   3 => 'Admin CRM',
   4 => 'Rol Crédito',
-  5 => 'centro de prospección',
-  6 => 'centro de prospección - Total',
+  5 => 'Centro Prospección',
+  6 => 'Centro Prospección - Total',
   7 => 'Seguros',
 );
