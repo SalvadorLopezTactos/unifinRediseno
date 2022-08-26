edf6cad152ab7a1fc153582d73fc46955f2a266b
erickdjcruz <47753132+erickdjcruz@users.noreply.github.com>
2022-08-26 16:12:44 -0500

    3671_valbanca_20220826
    sin accionespara el subpanel

---
+++ custom/modules/cta_cuentas_bancarias/clients/base/views/subpanel-list/subpanel-list.php
@@ -0,0 +1,52 @@
+<?php
+// created: 2020-07-15 20:23:33
+$viewdefs['cta_cuentas_bancarias']['base']['view']['subpanel-list'] = array (
+  'panels' => 
+  array (
+    0 => 
+    array (
+      'name' => 'panel_header',
+      'label' => 'LBL_PANEL_1',
+      'fields' => 
+      array (
+        0 => 
+        array (
+          'label' => 'LBL_NAME',
+          'enabled' => true,
+          'default' => true,
+          'name' => 'name',
+          'link' => true,
+        ),
+        1 => 
+        array (
+          'label' => 'LBL_DATE_MODIFIED',
+          'enabled' => true,
+          'default' => true,
+          'name' => 'date_modified',
+        ),
+      ),
+    ),
+  ),
+  'orderBy' => 
+  array (
+    'field' => 'date_modified',
+    'direction' => 'desc',
+  ),
+  'rowactions' => 
+  array (
+    'actions' => 
+    array (
+      0 => 
+      array (
+        'type' => 'rowaction',
+        'css_class' => 'btn',
+        'tooltip' => 'LBL_PREVIEW',
+        'event' => 'list:preview:fire',
+        'icon' => 'fa-eye',
+        'acl_action' => 'view',
+        'allow_bwc' => false,
+      ),
+    ),
+  ),
+  'type' => 'subpanel-list',
+);
