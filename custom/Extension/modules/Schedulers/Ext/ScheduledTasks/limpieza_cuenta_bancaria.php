12c2304d9e2957e8d8497eff32ced336f4c62d61
Axel Flores <axelflores@MacBook-Pro-de-Axel.local>
2022-09-01 09:52:03 -0500

    Servicio cuenta bancaria - Actualización Job

--- custom/Extension/modules/Schedulers/Ext/ScheduledTasks/limpieza_cuenta_bancaria.php
+++ custom/Extension/modules/Schedulers/Ext/ScheduledTasks/limpieza_cuenta_bancaria.php
@@ -5,25 +5,26 @@ function limpieza_cuenta_bancaria()
 {
     global $db;
     $error = false;
-        
+
     //Inicia ejecución
     $GLOBALS['log']->fatal('Job limpieza_cuenta_bancaria: Inicia');
 
-    $sql = "SELECT  ccb.id id_tarjeta, ccb.name, ccbc.idcorto_c, ccb.banco, 
+    $sql = "SELECT  ccb.id id_tarjeta, ccb.name, ccbc.idcorto_c, ccb.banco,
     ccb.cuenta, ccb.estado, ccb.clabe , ccbc.validada_c , DATE_FORMAT(ccbc.vigencia_c, '%Y-%m-%d') vigencia,
     DATE_FORMAT(date_add(NOW(), INTERVAL 1 DAY) , '%Y-%m-%d') tope
-    from cta_cuentas_bancarias ccb 
-    inner join cta_cuentas_bancarias_cstm ccbc on ccb.id = ccbc.id_c 
-      WHERE DATE_FORMAT(ccbc.vigencia_c, '%Y-%m-%d') < DATE_FORMAT(date_add(NOW(), INTERVAL 1 DAY) , '%Y-%m-%d') 
-      AND ccbc.validada_c = 0 ; ";
+    from cta_cuentas_bancarias ccb
+    inner join cta_cuentas_bancarias_cstm ccbc on ccb.id = ccbc.id_c
+      WHERE DATE_FORMAT(ccbc.vigencia_c, '%Y-%m-%d') < DATE_FORMAT(date_add(NOW(), INTERVAL 1 DAY) , '%Y-%m-%d')
+      AND ccbc.validada_c = 1 ; ";
     $result = $db->query($sql);
     while ($row = $db->fetchByAssoc($result)) {
         $bean = null;
-        $GLOBALS['log']->fatal('Cuenta bancaria -atrasada: '. $row['id_tarjeta']);
+        //$GLOBALS['log']->fatal('Cuenta bancaria -atrasada: '. $row['id_tarjeta']);
         $bean = BeanFactory::retrieveBean('cta_cuentas_bancarias', $row['id_tarjeta'] ,array('disable_row_level_security' => true));
-        $bean->validada_c = 1;
+        $bean->validada_c = 0;
         $bean->save();
     }
-  
+
     $GLOBALS['log']->fatal('Job limpieza_cuenta_bancaria: Fin');
+    return true;
 }
