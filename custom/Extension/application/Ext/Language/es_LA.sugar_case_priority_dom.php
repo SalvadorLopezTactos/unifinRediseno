9c4d4b71256e89d6077e4f6e9ebe111f8e22ce65
Salvador Lopez <salvador.lopez@tactos.com.mx>
2022-09-12 21:52:45 -0500

    Corrección listas de valores para casos

--- custom/Extension/application/Ext/Language/es_LA.sugar_case_priority_dom.php
+++ custom/Extension/application/Ext/Language/es_LA.sugar_case_priority_dom.php
@@ -1,9 +1,9 @@
 <?php
- // created: 2022-08-30 00:09:32
+ // created: 2022-09-12 21:50:18
 
 $app_list_strings['case_priority_dom']=array (
+  'P4' => 'Urgente',
   'P1' => 'Importante',
   'P2' => 'Media',
   'P3' => 'Baja',
-  'P4' => 'Urgente',
 );
