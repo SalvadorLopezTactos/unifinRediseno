880b4a13024117496c89cdd1f18e7afa415a1820
Salvador Lopez <salvador.lopez@tactos.com.mx>
2022-09-29 23:10:39 -0500

    Ordenamiento de Kanban Backlogs por los diferentes tipos

--- custom/clients/base/views/cm_ranking_bl/cm_ranking_bl.js
+++ custom/clients/base/views/cm_ranking_bl/cm_ranking_bl.js
@@ -2,7 +2,8 @@
     plugins: ['Dashlet'],
 
     events: {
-        
+        'click .dropdown-field':'muestraOpcionesOrdenamiento',
+        'click .ordenamiento_bl':'ordenamientoBacklogs'
     },
 
     initialize: function (options) {
@@ -40,6 +41,117 @@
                 throw e;
             }
         });
+    },
+
+    muestraOpcionesOrdenamiento:function(e){
+        
+        var $this = $(e.currentTarget);
+        $this.children('ul').slideToggle(100);
+        var $target = $(e.target);
+        if ($target.is('li')) {
+            $this.children('span').html($target.text());
+            $this.find('input[type="hidden"]').val($target.attr('data-value'));
+        }
+    },
+
+    ordenamientoBacklogs:function(e){
+
+        var tipo_ordenamiento=$(e.currentTarget).attr('data-value');
+        var columna=$(e.currentTarget).attr('data-columna');
+        var registros=this.ordenaRegistrosBL(contextoRanking.registrosRankingBL,columna,tipo_ordenamiento);
+
+        contextoRanking.registrosRankingBL[columna].Registros=registros;
+        this.render();
+    },
+
+    ordenaRegistrosBL:function(arreglo,columna, tipo){
+        var return_registros=[];
+        //BA : Backlog Ascendente, BD: Backlog Descendente
+        //MA : Monto Ascendente, MD: Monto Descendente
+        //CA : Nombre Cliente Ascendente, CD: Nombre Cliente Descendente
+        //EA : Nombre Equipo Ascendente, ED: Nombre Equipo Descendente
+        switch (tipo) {
+            case 'BA':
+                return_registros=arreglo[columna].Registros.sort((a, b) => {
+                    return parseInt(a.NoBacklog.replace(/,/g, '')) - parseInt(b.NoBacklog.replace(/,/g, ''));
+                });
+                break;
+            
+            case 'BD':
+                return_registros=arreglo[columna].Registros.sort((a, b) => {
+                    return parseInt(b.NoBacklog.replace(/,/g, '')) - parseInt(a.NoBacklog.replace(/,/g, ''));
+                });
+                break;
+            
+            case 'MA':
+                return_registros=arreglo[columna].Registros.sort((a, b) => {
+                    return parseFloat(a.Monto.replace(/,/g, '')) - parseFloat(b.Monto.replace(/,/g, ''));
+                });
+                break;
+            case 'MD':
+                return_registros=arreglo[columna].Registros.sort((a, b) => {
+                    return parseFloat(b.Monto.replace(/,/g, '')) - parseFloat(a.Monto.replace(/,/g, ''));
+                });
+                break;
+            case 'CA':
+                return_registros=arreglo[columna].Registros.sort((a,b) =>{
+                    const nameA = a.Cliente.toUpperCase().trim();
+                    const nameB = b.Cliente.toUpperCase().trim();
+                    if (nameA < nameB) {
+                        return -1;
+                    }
+                    if (nameA > nameB) {
+                        return 1;
+                    }
+                    return 0;
+                });
+                break;
+            case 'CD':
+                return_registros=arreglo[columna].Registros.sort((a, b) => {
+                    const nameA = a.Cliente.toUpperCase().trim();
+                    const nameB = b.Cliente.toUpperCase().trim();
+                    if (nameA < nameB) {
+                        return 1;
+                    }
+                    if (nameA > nameB) {
+                        return -1;
+                    }
+                    return 0;
+                });
+                break;
+            case 'EA':
+                return_registros=arreglo[columna].Registros.sort((a,b) =>{
+                    const nameA = a.Equipo.toUpperCase().trim();
+                    const nameB = b.Equipo.toUpperCase().trim();
+                    if (nameA < nameB) {
+                        return -1;
+                    }
+                    if (nameA > nameB) {
+                        return 1;
+                    }
+                    return 0;
+                });
+                break;
+            case 'ED':
+                return_registros=arreglo[columna].Registros.sort((a, b) => {
+                    const nameA = a.Equipo.toUpperCase().trim();
+                    const nameB = b.Equipo.toUpperCase().trim();
+                    if (nameA < nameB) {
+                        return 1;
+                    }
+                    if (nameA > nameB) {
+                        return -1;
+                    }
+                    return 0;
+                });
+                break;
+            default:
+                return_registros=arreglo[columna].Registros;
+                break;
+        }
+
+        return return_registros;
+
     }
 
 })
