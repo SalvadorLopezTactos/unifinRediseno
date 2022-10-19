b4ca2bb1c14192f3fc42ab13622ab1949e6c73fe
Salvador Lopez <salvador.lopez@tactos.com.mx>
2022-10-19 16:03:32 -0500

    Nueva opción en Sepomex para dar de alta nueva colonia

---
+++ custom/clients/base/views/Alta-Sepomex/Alta-Sepomex.js
@@ -0,0 +1,23 @@
+/**
+ * Created by Jorge on 7/17/2015.
+ */
+/**
+ * Created by Jorge on 6/1/2015.
+ */
+({
+
+    className: 'Alta-Sepomex',
+
+    events: {
+    },
+
+    initialize: function(options){
+        this._super("initialize", [options]);
+
+    },
+
+    _render: function () {
+        this._super("_render");
+    },
+
+})
