d54d424a29d98c764c9d99ad06f7bb333d5b2fc4
Salvador Lopez <salvador.lopez@tactos.com.mx>
2022-11-21 03:01:35 -0600

    Ajuste cuentas bloqueadas, envío de notificación a múltiles usuarios

--- custom/clients/base/api/CuentasNoContactar.php
+++ custom/clients/base/api/CuentasNoContactar.php
@@ -105,6 +105,7 @@ AND (accounts_cstm.user_id_c='{$user_id}' OR accounts_cstm.user_id1_c='{$user_id
 			global $db, $current_user;
 			$cuentas = $args['data']['cuentas'];
 			$parame = $args['data']['parame'];
+			$GLOBALS['log']->fatal(print_r($parame,true));
 			$selected = $args['data']['selected'];
 			$cuentas_resumen['actualizados']=array();
 			$cuentas_resumen['no_actualizados']=array();
@@ -177,13 +178,14 @@ AND (accounts_cstm.user_id_c='{$user_id}' OR accounts_cstm.user_id1_c='{$user_id
 							require_once 'include/SugarPHPMailer.php';
 							require_once 'modules/Administration/Administration.php';
 							$ingesta = BeanFactory::retrieveBean('Users', $parame["ingesta"]);
-							$valida = BeanFactory::retrieveBean('Users', $parame["valida"]);
+							$lista_usuarios=$parame["valida_users"];
+							
+							$GLOBALS['log']->fatal(print_r($lista_usuarios_notificar,true));
+							//$valida = BeanFactory::retrieveBean('Users', $parame["valida"]);
 							$linkCuenta=$GLOBALS['sugar_config']['site_url'].'/#Accounts/'.$idcuenta;
-							$mailHTML = '<p align="justify"><font face="verdana" color="#635f5f">Hola <b>'.$valida->nombre_completo_c.'</b>
-							<br><br>Se le informa que la cuenta <b><a id="linkCuenta" href="'.$linkCuenta.'">'.$cuenta.'</a></b> ha sido bloqueada por <b>'.$ingesta->nombre_completo_c.'</b>
-							<br><br>La razón de bloqueo es: <b>'.$app_list_strings['razon_list'][$parame["razon"]].'</b>
-							<br><br>y el detalle: <b>'.$parame["detalle"].'</b>
-							<br><br>Se requiere de su aprobación para bloquear definitivamente la cuenta.
+							$mailHTML = '<p align="justify"><font face="verdana" color="#635f5f">
+							Se le informa que el asesor '.$ingesta->nombre_completo_c.' ha solicitado el bloqueo de la cuenta'.$cuenta.' en CRM.<br>
+							Para autorizar el bloqueo dé click en el siguiente enlace <b><a id="linkCuenta" href="'.$linkCuenta.'">'.$cuenta.'</a></b>
 							<br><br>Saludos.
 							<br><br>Atentamente Unifin</font></p>
 							<br><p class="imagen"><img border="0" id="bannerUnifin" src="https://www.unifin.com.mx/ri/front/img/logo.png"></span></p>		
@@ -194,11 +196,20 @@ AND (accounts_cstm.user_id_c='{$user_id}' OR accounts_cstm.user_id1_c='{$user_id
 							Asimismo, los datos personales, que en su caso UNIFIN pudiera recibir a través de este medio, mantendrán la seguridad y privacidad en los términos de la Ley Federal de Protección de Datos Personales; para más información consulte nuestro &nbsp;</span><span style="font-size: 7.5pt; font-family: \'Arial\',sans-serif; color: #2f96fb;"><a href="https://www.unifin.com.mx/aviso-de-privacidad.php" target="_blank" rel="noopener"><span style="color: #2f96fb; text-decoration: none;">Aviso de Privacidad</span></a></span><span style="font-size: 7.5pt; font-family: \'Arial\',sans-serif; color: #212121;">&nbsp; publicado en&nbsp;</span><span style="font-size: 7.5pt; font-family: \'Arial\',sans-serif; color: #0b5195;"><a href="http://www.unifin.com.mx/" target="_blank" rel="noopener"><span style="color: #0b5195; text-decoration: none;">www.unifin.com.mx</span></a></span><u></u><u></u></p>';
 							$mailer = MailerFactory::getSystemDefaultMailer();
 							$mailTransmissionProtocol = $mailer->getMailTransmissionProtocol();
-							$mailer->setSubject('Cuenta '.$cuenta.' bloqueada por '.$ingesta->nombre_completo_c);
+							$mailer->setSubject('Solicitud de bloqueo de Cuenta en CRM');
 							$body = trim($mailHTML);
 							$mailer->setHtmlBody($body);
 							$mailer->clearRecipients();
-							$mailer->addRecipientsTo(new EmailIdentity($valida->email1, $valida->first_name . ' ' . $valida->last_name));
+							if(count($lista_usuarios)>0){
+								for ($i=0; $i < count($lista_usuarios); $i++) { 
+									$usuario_validador = BeanFactory::retrieveBean('Users', $lista_usuarios[$i]);
+									//$info_usuario=array("email"=>$usuario_validador->email1,"name"=>$usuario_validador->first_name . ' ' . $usuario_validador->last_name);
+									//array_push($lista_usuarios_notificar,$info_usuario);
+									$GLOBALS['log']->fatal("AGREGANDO EL EMAIL: ".$usuario_validador->email1." A LA LISTA");
+									$mailer->addRecipientsTo(new EmailIdentity($usuario_validador->email1, $usuario_validador->first_name . ' ' . $usuario_validador->last_name));
+								}
+							}
+							//$mailer->addRecipientsTo(new EmailIdentity($valida->email1, $valida->first_name . ' ' . $valida->last_name));
 							$result = $mailer->send();
 						}else{
 							//Notifica desbloqueo al Resposable de validación
