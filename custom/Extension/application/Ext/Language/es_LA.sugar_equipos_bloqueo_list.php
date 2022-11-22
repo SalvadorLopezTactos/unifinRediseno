245799a8a618764b9920b610725a861f34fbed0d
Salvador Lopez <salvador.lopez@tactos.com.mx>
2022-11-22 14:28:11 -0600

    Ajuste para notificar confirmación de bloqueo a los usuarios del equipo correspondiente

---
+++ custom/Extension/application/Ext/Language/es_LA.sugar_equipos_bloqueo_list.php
@@ -0,0 +1,8 @@
+<?php
+ // created: 2022-11-22 13:29:41
+
+$app_list_strings['equipos_bloqueo_list']=array (
+  'Cartera' => 'af7b5d44-cae4-11eb-aae9-00155da0710c',
+  'Credito' => 'c735bb58-d298-11eb-bef9-00155da0710c',
+  'Cumplimiento' => 'd15cacb8-d298-11eb-b283-00155da0710c',
+);
