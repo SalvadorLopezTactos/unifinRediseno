571dffd06b7ab57e3916afc7dc723f4f0222abe7
Salvador Lopez <salvador.lopez@tactos.com.mx>
2022-11-24 21:09:34 -0600

    Ajuste modificación de dependencia FCR y HD, Solo Lectura Fecha de conclusión

--- custom/Extension/modules/Cases/Ext/Vardefs/sugarfield_case_fcr_c.php
+++ custom/Extension/modules/Cases/Ext/Vardefs/sugarfield_case_fcr_c.php
@@ -1,8 +1,8 @@
 <?php
- // created: 2022-08-30 18:30:44
+ // created: 2022-11-24 20:55:35
 $dictionary['Case']['fields']['case_fcr_c']['labelValue']='FCR';
 $dictionary['Case']['fields']['case_fcr_c']['enforced']='';
-$dictionary['Case']['fields']['case_fcr_c']['dependency']='or(equal($subtipo_c,"22"),equal($subtipo_c,"48"),equal($subtipo_c,"36"),equal($subtipo_c,"42"),equal($subtipo_c,"45"),equal($subtipo_c,"60"),equal($subtipo_c,"62"),equal($subtipo_c,"30"),equal($subtipo_c,"32"),equal($subtipo_c,"43"),equal($subtipo_c,"44"),equal($subtipo_c,"56"),equal($subtipo_c,"18"),equal($subtipo_c,"20"),equal($subtipo_c,"21"),equal($subtipo_c,"35"),equal($subtipo_c,"38"),equal($subtipo_c,"19"),equal($subtipo_c,"37"),equal($subtipo_c,"39"),equal($subtipo_c,"40"),equal($subtipo_c,"58"),equal($subtipo_c,"28"))';
+$dictionary['Case']['fields']['case_fcr_c']['dependency']='or(equal($subtipo_c,"22"),equal($subtipo_c,"27"),equal($subtipo_c,"29"),equal($subtipo_c,"36"),equal($subtipo_c,"42"),equal($subtipo_c,"45"),equal($subtipo_c,"60"),equal($subtipo_c,"62"),equal($subtipo_c,"30"),equal($subtipo_c,"32"),equal($subtipo_c,"43"),equal($subtipo_c,"44"),equal($subtipo_c,"18"),equal($subtipo_c,"20"),equal($subtipo_c,"21"),equal($subtipo_c,"35"),equal($subtipo_c,"38"),equal($subtipo_c,"19"),equal($subtipo_c,"37"),equal($subtipo_c,"39"),equal($subtipo_c,"40"),equal($subtipo_c,"58"),equal($subtipo_c,"28"))';
 $dictionary['Case']['fields']['case_fcr_c']['readonly_formula']='';
 
  ?>
