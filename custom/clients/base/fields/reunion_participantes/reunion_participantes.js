531ba24f2a5589a0b1d8f9f4c239151776a7fae6
axelflores <fj.axe21@gmail.com>
2023-01-20 13:29:59 -0600

    Lenia - Ajuste para obtener participantes

--- custom/clients/base/fields/reunion_participantes/reunion_participantes.js
+++ custom/clients/base/fields/reunion_participantes/reunion_participantes.js
@@ -44,7 +44,7 @@
 					throw e;
 				}
 			});
-		} else {
+		} else if (this.action!='disabled') {
 			if(this.model.get('parent_id') != undefined && this.model.get('parent_id') != '' && this.model.get('parent_type') == 'Accounts') {
 				//Recupera datos para vista de creación
 				app.alert.show('alert_participants', {
