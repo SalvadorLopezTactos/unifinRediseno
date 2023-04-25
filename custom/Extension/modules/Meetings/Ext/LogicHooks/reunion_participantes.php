4da3f1ced22e9a1a9729041c7d24b9fb7a3d0e5a
Axel F <axel.flores@tactos.com.mx>
2023-04-24 19:29:13 -0600

    Lenia - Ajuste clase reunion_participantes

--- custom/Extension/modules/Meetings/Ext/LogicHooks/reunion_participantes.php
+++ custom/Extension/modules/Meetings/Ext/LogicHooks/reunion_participantes.php
@@ -8,6 +8,6 @@ $hook_array['after_save'][] = Array(
     7,
     'Crea Relación entre Reunión y Participantes',
     'custom/modules/Meetings/reunion_participantes.php',
-    'reunion_participantes',
+    'reunion_participantes_class',
     'reunion_participantes'
 );
