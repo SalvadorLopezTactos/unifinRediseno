8b3d09dd74f199ceb251e43f85b3e1c1bfa4da45
Salvador Lopez <salvador.lopez@tactos.com.mx>
2023-05-08 01:53:41 -0600

    Nuevos campos para saber el usuaario que rechazó/aprobó cambios

---
+++ custom/Extension/modules/Accounts/Ext/Vardefs/sugarfield_fecha_aprueba_rechaza_c.php
@@ -0,0 +1,9 @@
+<?php
+ // created: 2023-05-08 01:56:09
+$dictionary['Account']['fields']['fecha_aprueba_rechaza_c']['labelValue']='Fecha de aprobación/rechazo';
+$dictionary['Account']['fields']['fecha_aprueba_rechaza_c']['enforced']='';
+$dictionary['Account']['fields']['fecha_aprueba_rechaza_c']['dependency']='';
+$dictionary['Account']['fields']['fecha_aprueba_rechaza_c']['required_formula']='';
+$dictionary['Account']['fields']['fecha_aprueba_rechaza_c']['readonly_formula']='';
+
+ ?>
---
+++ custom/Extension/modules/Accounts/Ext/Vardefs/sugarfield_usr_aprueba_rechaza_c.php
@@ -0,0 +1,8 @@
+<?php
+ // created: 2023-05-08 01:53:23
+$dictionary['Account']['fields']['usr_aprueba_rechaza_c']['labelValue']='Usuario que aprobó/rechazó';
+$dictionary['Account']['fields']['usr_aprueba_rechaza_c']['dependency']='';
+$dictionary['Account']['fields']['usr_aprueba_rechaza_c']['required_formula']='';
+$dictionary['Account']['fields']['usr_aprueba_rechaza_c']['readonly_formula']='';
+
+ ?>
