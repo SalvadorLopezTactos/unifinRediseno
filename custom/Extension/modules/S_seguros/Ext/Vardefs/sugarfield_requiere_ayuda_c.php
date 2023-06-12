d98a90d0b8d1f44b138131304f315f1f42091c40
Eduardo Carrasco Beltran <eduardo.carrasco@tactos.com.mx>
2023-06-12 16:47:27 -0600

    18-19042023-Creditaria

--- custom/Extension/modules/S_seguros/Ext/Vardefs/sugarfield_requiere_ayuda_c.php
+++ custom/Extension/modules/S_seguros/Ext/Vardefs/sugarfield_requiere_ayuda_c.php
@@ -1,9 +1,23 @@
 <?php
- // created: 2023-06-07 19:27:39
+ // created: 2023-06-12 12:33:59
 $dictionary['S_seguros']['fields']['requiere_ayuda_c']['labelValue']='Requiere Ayuda';
-$dictionary['S_seguros']['fields']['requiere_ayuda_c']['dependency']='and(equal($tipo_registro_sf_c,2),equal($creditaria_c,"Seguros"))';
 $dictionary['S_seguros']['fields']['requiere_ayuda_c']['required_formula']='';
 $dictionary['S_seguros']['fields']['requiere_ayuda_c']['readonly_formula']='';
-$dictionary['S_seguros']['fields']['requiere_ayuda_c']['visibility_grid']='';
+$dictionary['S_seguros']['fields']['requiere_ayuda_c']['visibility_grid']=array (
+  'trigger' => 'creditaria_c',
+  'values' => 
+  array (
+    'Creditaria' => 
+    array (
+      0 => '1',
+    ),
+    'Seguros' => 
+    array (
+      0 => '',
+      1 => '1',
+      2 => '2',
+    ),
+  ),
+);
 
  ?>
