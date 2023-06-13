eb80ba6bc09218c1ef325ee587176a9efb2a4c6c
Eduardo Carrasco Beltran <eduardo.carrasco@tactos.com.mx>
2023-06-13 08:44:06 -0600

    18-19042023-Creditaria

--- custom/Extension/application/Ext/Language/es_LA.sugar_ejecutivo_list.php
+++ custom/Extension/application/Ext/Language/es_LA.sugar_ejecutivo_list.php
@@ -1,5 +1,5 @@
 <?php
- // created: 2021-04-14 13:46:41
+ // created: 2023-06-12 22:16:56
 
 $app_list_strings['ejecutivo_list']=array (
   1 => 'CON-0000000004',
@@ -7,4 +7,5 @@ $app_list_strings['ejecutivo_list']=array (
   3 => 'CON-0000000653',
   4 => 'N/A',
   5 => 'CON-0000000589',
+  6 => 'CON-0000002180',
 );
