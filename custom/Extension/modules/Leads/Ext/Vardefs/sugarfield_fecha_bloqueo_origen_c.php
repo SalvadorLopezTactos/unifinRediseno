46edb54ac15530299643a353d001e93e73870372
Eduardo Carrasco Beltran <eduardo.carrasco@tactos.com.mx>
2023-06-16 10:16:23 -0600

    18-19042023-Creditaria

--- custom/Extension/modules/Leads/Ext/Vardefs/sugarfield_fecha_bloqueo_origen_c.php
+++ custom/Extension/modules/Leads/Ext/Vardefs/sugarfield_fecha_bloqueo_origen_c.php
@@ -1,8 +1,8 @@
 <?php
- // created: 2022-03-13 16:31:44
+ // created: 2023-06-16 11:07:02
 $dictionary['Lead']['fields']['fecha_bloqueo_origen_c']['labelValue']='Fecha Bloqueo Origen';
 $dictionary['Lead']['fields']['fecha_bloqueo_origen_c']['enforced']='';
-$dictionary['Lead']['fields']['fecha_bloqueo_origen_c']['dependency']='';
+$dictionary['Lead']['fields']['fecha_bloqueo_origen_c']['dependency']='not(equal($subtipo_registro_c,4))';
 $dictionary['Lead']['fields']['fecha_bloqueo_origen_c']['required_formula']='';
 $dictionary['Lead']['fields']['fecha_bloqueo_origen_c']['readonly_formula']='';
 
