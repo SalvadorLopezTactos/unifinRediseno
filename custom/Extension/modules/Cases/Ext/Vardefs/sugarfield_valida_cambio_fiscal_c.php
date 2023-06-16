6a0a037d849226def9e4bd30a3109ceb1e3d66fb
Salvador Lopez <salvador.lopez@tactos.com.mx>
2023-06-16 16:00:27 -0600

    Fix: Asignación de nuevos casos solo para cambio fiscal

---
+++ custom/Extension/modules/Cases/Ext/Vardefs/sugarfield_valida_cambio_fiscal_c.php
@@ -0,0 +1,8 @@
+<?php
+ // created: 2023-06-16 16:15:18
+$dictionary['Case']['fields']['valida_cambio_fiscal_c']['labelValue']='Valida cambio fiscal';
+$dictionary['Case']['fields']['valida_cambio_fiscal_c']['enforced']='';
+$dictionary['Case']['fields']['valida_cambio_fiscal_c']['dependency']='';
+$dictionary['Case']['fields']['valida_cambio_fiscal_c']['readonly_formula']='';
+
+ ?>
