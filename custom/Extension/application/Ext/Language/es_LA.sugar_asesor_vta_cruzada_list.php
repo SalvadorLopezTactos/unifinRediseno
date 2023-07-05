97277066e94109fc95263c101768a4edd1ee5230
Eduardo Carrasco Beltran <eduardo.carrasco@tactos.com.mx>
2023-07-05 17:14:55 -0600

    39-19062023-Creditaria Fase2

--- custom/Extension/application/Ext/Language/es_LA.sugar_asesor_vta_cruzada_list.php
+++ custom/Extension/application/Ext/Language/es_LA.sugar_asesor_vta_cruzada_list.php
@@ -1,5 +1,5 @@
 <?php
- // created: 2023-07-05 14:12:29
+ // created: 2023-07-05 18:12:06
 
 $app_list_strings['asesor_vta_cruzada_list']=array (
   '' => '',
@@ -7,4 +7,5 @@ $app_list_strings['asesor_vta_cruzada_list']=array (
   2 => 'Paola Jímenez',
   3 => 'Claudia Cardos',
   4 => 'Humberto Ruiz',
+  5 => 'Veronica Gutierrez',
 );
