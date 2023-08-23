7c8a8b1e1630278e13ad691b8fc13150eca8f227
Salvador Lopez <salvador.lopez@tactos.com.mx>
2023-08-23 15:20:12 -0600

    Nuevo valor 1er Contacto y muestra panel para creación de nueva llamada

--- custom/Extension/application/Ext/Language/es_LA.sugar_tct_resultado_llamada_ddw_list.php
+++ custom/Extension/application/Ext/Language/es_LA.sugar_tct_resultado_llamada_ddw_list.php
@@ -1,5 +1,5 @@
 <?php
- // created: 2022-04-28 10:43:05
+ // created: 2023-08-21 15:15:18
 
 $app_list_strings['tct_resultado_llamada_ddw_list']=array (
   '' => '',
@@ -14,4 +14,5 @@ $app_list_strings['tct_resultado_llamada_ddw_list']=array (
   'Llamada_preventiva' => 'Llamada Preventiva',
   'Encuesta_Exitosa' => 'Encuesta Exitosa',
   'Encuesta_no_Exitosa' => 'Encuesta no Exitosa',
+  '1er Contacto' => '1er Contacto',
 );
