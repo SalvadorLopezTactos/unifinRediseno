da1870c7f4f25d9fe1c9aaa80abf1d2c5aa21bca
Axel F <axel.flores@tactos.com.mx>
2023-08-29 14:30:53 -0600

    Agrega valor vacío a la dependencia de resultado sobre seguimiento de po

--- custom/Extension/modules/Meetings/Ext/Vardefs/sugarfield_resultado_c.php
+++ custom/Extension/modules/Meetings/Ext/Vardefs/sugarfield_resultado_c.php
@@ -1,5 +1,5 @@
 <?php
- // created: 2023-08-28 13:09:57
+ // created: 2023-08-29 20:28:09
 $dictionary['Meeting']['fields']['resultado_c']['labelValue']='Resultado de Cita';
 $dictionary['Meeting']['fields']['resultado_c']['dependency']='';
 $dictionary['Meeting']['fields']['resultado_c']['required_formula']='';
@@ -152,11 +152,12 @@ $dictionary['Meeting']['fields']['resultado_c']['visibility_grid']=array (
     ),
     14 => 
     array (
-      0 => '26',
-      1 => '27',
-      2 => '28',
-      3 => '29',
-      4 => '30',
+      0 => '',
+      1 => '26',
+      2 => '27',
+      3 => '28',
+      4 => '29',
+      5 => '30',
     ),
     'Planned' => 
     array (
