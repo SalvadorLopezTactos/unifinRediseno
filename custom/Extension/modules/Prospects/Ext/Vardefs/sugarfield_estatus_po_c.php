354591be7acb93d2282f4ad554f293339049b315
Salvador Lopez <salvador.lopez@tactos.com.mx>
2023-09-01 12:50:10 -0600

    Omita dependencia de visibilidad Estatus de PO

--- custom/Extension/modules/Prospects/Ext/Vardefs/sugarfield_estatus_po_c.php
+++ custom/Extension/modules/Prospects/Ext/Vardefs/sugarfield_estatus_po_c.php
@@ -1,31 +1,9 @@
 <?php
- // created: 2022-05-17 02:28:48
+ // created: 2023-08-31 21:12:31
 $dictionary['Prospect']['fields']['estatus_po_c']['labelValue']='Estatus';
+$dictionary['Prospect']['fields']['estatus_po_c']['dependency']='';
 $dictionary['Prospect']['fields']['estatus_po_c']['required_formula']='';
 $dictionary['Prospect']['fields']['estatus_po_c']['readonly_formula']='';
-$dictionary['Prospect']['fields']['estatus_po_c']['visibility_grid']=array (
-  'trigger' => 'estatus_po_c',
-  'values' =>
-  array (
-    1 =>
-    array (
-      0 => '1',
-      1 => '4',
-    ),
-    2 =>
-    array (
-      0 => '2',
-      1 => '4',
-    ),
-    3 =>
-    array (
-      0 => '3',
-    ),
-    4 =>
-    array (
-      0 => '4',
-    ),
-  ),
-);
+$dictionary['Prospect']['fields']['estatus_po_c']['visibility_grid']='';
 
  ?>
