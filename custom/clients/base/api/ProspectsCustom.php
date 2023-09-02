581b38759b08c35869cb442803ee6e3661412abf
axelflores <fj.axe21@gmail.com>
2023-09-01 20:19:21 -0600

    Se agrega validación de Match por email y se solventa origen

--- custom/clients/base/api/ProspectsCustom.php
+++ custom/clients/base/api/ProspectsCustom.php
@@ -34,26 +34,57 @@ class ProspectsCustom extends SugarApi
     public function customProspectInsert($api, $args)
     {
         $GLOBALS['log']->fatal('Prospects Custom Endpoint');
-        //Genera nuevo bean para Prospects
-        $beanProspect = BeanFactory::newBean("Prospects");
-        //Generar objeto para respuesta
-        $beanResult = [];
         
-        //Iterar $args obtenidos y setea bean
-        foreach ($args as $clave => $valor) {
-            $beanProspect->$clave = $valor;
+        //Recupera email
+        $email = isset($args['email1']) ? $args['email1'] : '';
+        if (empty($email)) {
+            require_once 'include/api/SugarApiException.php';
+            throw new SugarApiExceptionInvalidParameter("Es necesario enviar un Email");
         }
-        //Guarda bean y devuelve estructura
-        $beanProspect->save();
         
-        //Setar objeto de resultado con valores guardados
+        //Consulta existencia de PO con mismo Email
+        $idProspect = '';
+        global $db;
+        $query="select p.id, e.email_address
+          from prospects p
+          inner join prospects_cstm pc on pc.id_c=p.id
+          inner join email_addr_bean_rel eb on eb.bean_id=p.id and eb.deleted=0
+          inner join email_addresses e on e.id = eb.email_address_id and e.deleted=0
+          where 
+          p.deleted=0 and pc.excluye_campana_c=0 -- Exluye campana sirve para descartar registros cargados por importación masiva para campañas
+          and e.email_address='{$email}'
+          order by p.date_entered asc
+          limit 1;";
+        $queryResult = $db->query($query);
+        while($row = $db->fetchByAssoc($queryResult)){
+             $idProspect = $row['id'];
+         }
+        
+        //Valida existenticia
+        if(empty($idProspect)){
+            //Genera nuevo bean para Prospects
+            $beanProspect = BeanFactory::newBean("Prospects");
+            //Generar objeto para respuesta
+            $beanResult = [];
+            
+            //Iterar $args obtenidos y setea bean
+            foreach ($args as $clave => $valor) {
+                if (!empty($valor)) {
+                    $beanProspect->$clave = $valor;
+                }
+            }
+            //Guarda bean y devuelve estructura
+            $beanProspect->save();
+        }else{
+            //Recupera bean existente
+            $beanProspect = BeanFactory::retrieveBean('Prospects', $idProspect, array('disable_row_level_security' => true));
+        }
+        
+        //Setear objeto de resultado con valores guardados
         foreach ($beanProspect->column_fields as $elemento) {
             $beanResult[$elemento] = $beanProspect->$elemento;
         }
         
-        //$GLOBALS['log']->fatal(print_r($beanProspect,true));
-        //$GLOBALS['log']->fatal($beanResult);
-
         //Regresa estructura de bean
         return $beanResult;
 
