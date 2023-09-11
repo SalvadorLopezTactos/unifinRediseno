6f4bafb7c6cddac8be9c268ad3539831f8082cfd
Salvador Lopez <salvador.lopez@tactos.com.mx>
2023-09-11 12:47:34 -0600

    Ajuste en validaciòn de email duplicado en PO

--- custom/modules/Prospects/Validate_Email.php
+++ custom/modules/Prospects/Validate_Email.php
@@ -7,13 +7,21 @@ class Validate_Email
         global $db;
         $email = $bean->email1;
 
-        $qEmailExists = "SELECT p.id,pc.clean_name_c,e.email_address from prospects p
+        $qEmailExists = "SELECT p.id id_po,pc.clean_name_c,e.email_address from prospects p
         inner join prospects_cstm pc on p.id=pc.id_c
         left join email_addr_bean_rel er on er.bean_id = p.id and er.deleted=0
         left join email_addresses e on e.id=er.email_address_id and e.deleted =0
         WHERE e.email_address = '{$email}'";
         
         $queryResultEmail = $db->query($qEmailExists);
-        if($db->getRowCount($queryResultEmail) > 0) throw new SugarApiExceptionInvalidParameter('No se puede guardar el registro. El correo electrónico '.$email.' ya exite en Público Objetivo, favor de corregir'); 
+        $id_po = "";
+        if ($queryResultEmail->num_rows > 0) {
+
+            while ($row = $GLOBALS['db']->fetchByAssoc($queryResultEmail)) {
+                $id_po = $row['id_po'];
+            }
+        }
+        
+        if($db->getRowCount($queryResultEmail) > 0 && $id_po != $bean->id) throw new SugarApiExceptionInvalidParameter('No se puede guardar el registro. El correo electrónico '.$email.' ya existe en Público Objetivo, favor de corregir'); 
     }
 }
