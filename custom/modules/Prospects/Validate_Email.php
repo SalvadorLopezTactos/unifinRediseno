355014172d23c6e84e83afbebf2da468e072897b
Salvador Lopez <salvador.lopez@tactos.com.mx>
2023-09-11 14:45:21 -0600

    Ajuste en msj de duplicado en PO

--- custom/modules/Prospects/Validate_Email.php
+++ custom/modules/Prospects/Validate_Email.php
@@ -5,6 +5,7 @@ class Validate_Email
     public function existsEmail($bean = null, $event = null, $args = null)
     {
         global $db;
+        $id_publico_objetivo = $bean->id;
         $email = $bean->email1;
 
         $qEmailExists = "SELECT p.id id_po,pc.clean_name_c,e.email_address from prospects p
@@ -14,14 +15,24 @@ class Validate_Email
         WHERE e.email_address = '{$email}'";
         
         $queryResultEmail = $db->query($qEmailExists);
-        $id_po = "";
+        $ids_po = array();
         if ($queryResultEmail->num_rows > 0) {
 
             while ($row = $GLOBALS['db']->fetchByAssoc($queryResultEmail)) {
                 $id_po = $row['id_po'];
+                $name_po = $row['clean_name_c'];
+                if( $id_po != $id_publico_objetivo ){
+                    array_push( $ids_po, array( "id_po" => $id_po,"name_po" => $name_po ) );
+                }
             }
         }
-        
-        if($db->getRowCount($queryResultEmail) > 0 && $id_po != $bean->id) throw new SugarApiExceptionInvalidParameter('No se puede guardar el registro. El correo electrónico '.$email.' ya existe en Público Objetivo, favor de corregir'); 
+        $str_link_po = "";
+        if( count($ids_po) > 0 ){
+            for ($i=0; $i < count($ids_po); $i++) { 
+                $str_link_po .= $ids_po[$i]["name_po"].',';
+            }
+            
+        }
+        if($db->getRowCount($queryResultEmail) > 0 && count($ids_po) > 0) throw new SugarApiExceptionInvalidParameter('No se puede guardar el registro. El correo electrónico '.$email.' ya existe en Público Objetivo en los siguientes registros: ' .$str_link_po. ' favor de corregir'); 
     }
 }
