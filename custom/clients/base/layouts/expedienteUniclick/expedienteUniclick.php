b647524eb776df1910b605f3552d7fc9a82e3479
axelflores <fj.axe21@gmail.com>
2023-09-20 17:46:34 -0600

    Ajuste Expediente Uniclick - Habilitación layout

---
+++ custom/clients/base/layouts/expedienteUniclick/expedienteUniclick.php
@@ -0,0 +1,9 @@
+<?php
+$viewdefs['base']['layout']['expedienteUniclick'] = array(
+    'type' => 'simple',
+    'components' => array(
+        array(
+            'view' => 'expedienteUniclick',
+        ),
+    ),
+);
