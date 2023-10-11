cefdc8c723140d1dee54b106c4f0a9fceb34104f
Salvador Lopez <salvador.lopez@tactos.com.mx>
2023-10-10 20:19:12 -0600

    Nuevos campos Razón de Rechazo y Régimen Capital

---
+++ custom/Extension/modules/Accounts/Ext/Vardefs/sugarfield_regimen_capital_c.php
@@ -0,0 +1,14 @@
+<?php
+ // created: 2023-10-10 17:30:41
+$dictionary['Account']['fields']['regimen_capital_c']['labelValue']='Régimen capital';
+$dictionary['Account']['fields']['regimen_capital_c']['full_text_search']=array (
+  'enabled' => '0',
+  'boost' => '1',
+  'searchable' => false,
+);
+$dictionary['Account']['fields']['regimen_capital_c']['enforced']='';
+$dictionary['Account']['fields']['regimen_capital_c']['dependency']='';
+$dictionary['Account']['fields']['regimen_capital_c']['required_formula']='';
+$dictionary['Account']['fields']['regimen_capital_c']['readonly_formula']='';
+
+ ?>
---
+++ custom/Extension/modules/tct02_Resumen/Ext/Vardefs/sugarfield_razon_rechazo_regimen_c.php
@@ -0,0 +1,14 @@
+<?php
+ // created: 2023-10-10 17:43:33
+$dictionary['tct02_Resumen']['fields']['razon_rechazo_regimen_c']['labelValue']='Razón de Rechazo';
+$dictionary['tct02_Resumen']['fields']['razon_rechazo_regimen_c']['full_text_search']=array (
+  'enabled' => '0',
+  'boost' => '1',
+  'searchable' => false,
+);
+$dictionary['tct02_Resumen']['fields']['razon_rechazo_regimen_c']['enforced']='';
+$dictionary['tct02_Resumen']['fields']['razon_rechazo_regimen_c']['dependency']='';
+$dictionary['tct02_Resumen']['fields']['razon_rechazo_regimen_c']['required_formula']='';
+$dictionary['tct02_Resumen']['fields']['razon_rechazo_regimen_c']['readonly_formula']='';
+
+ ?>
