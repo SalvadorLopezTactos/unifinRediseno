8ffdfd056a93ec26d1a6a4fa111d2e19add9cdea
Axel F <axel.flores@tactos.com.mx>
2024-02-23 18:49:28 -0600

    Ajuste para envío de Documento Quantico

--- custom/modules/Documents/clients/base/views/create/create.js
+++ custom/modules/Documents/clients/base/views/create/create.js
@@ -12,6 +12,8 @@
         //Ocultar campo que descarga Documento Quantico
         $('[data-type="document_download_quantico"]').hide();
 
+        //Oculta campo que guarda información del Documento de Quantico
+        $('[data-name="data_document_quantico_c"]').hide();
     },
 
 })
