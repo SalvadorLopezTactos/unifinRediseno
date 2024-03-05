8ede985efb090a2059654cb98a263b23371a5837
Salvador Lopez <salvador.lopez@tactos.com.mx>
2024-03-04 22:21:26 -0600

    Ajuste para habilitar opción Realizada a Reuniones que no pertenecen a Cuentas

--- custom/modules/Meetings/clients/base/fields/enum/enum.js
+++ custom/modules/Meetings/clients/base/fields/enum/enum.js
@@ -1,18 +1,15 @@
 ({
     extendsFrom: 'EnumField',
 
-    initialize: function (options)
-    {
+    initialize: function (options){
         this._super('initialize',[options]);
     },
 
-    render : function()
-    {
-    	if(this.name === 'status')
-	    {
+    render : function(){
+    	if(this.name === 'status'){
     		var lista = app.lang.getAppListStrings('meeting_status_dom');
-        if(this.model.get('parent_name') !== '' && this.model.get('status') === 'Planned')
-        {
+        
+        if( this.model.get('status') === 'Planned' && this.model.get('parent_type') == 'Accounts'){
           lista = app.lang.getAppListStrings('meeting_status_list');
         }
 		    this.items = lista;
