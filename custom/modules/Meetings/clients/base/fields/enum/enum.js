e0bc6339a616f142e95604430544806d94439391
Salvador Lopez <salvador.lopez@tactos.com.mx>
2024-03-05 18:13:23 -0600

    Agrega valor Realizada a Reuniones que no son de Cuentas

--- custom/modules/Meetings/clients/base/fields/enum/enum.js
+++ custom/modules/Meetings/clients/base/fields/enum/enum.js
@@ -7,11 +7,23 @@
 
     render : function(){
     	if(this.name === 'status'){
-    		var lista = app.lang.getAppListStrings('meeting_status_dom');
-        
-        if( this.model.get('status') === 'Planned' && this.model.get('parent_type') == 'Accounts'){
+    		var lista = {};
+
+        var fechaActual = new Date(); //obtiene fecha actual
+        var fechainicio = new Date(this.model.get("date_start"));
+        var d = fechainicio.getDate();
+        var m = fechainicio.getMonth() + 1;
+        var y = fechainicio.getFullYear();
+        var fechafin= new Date(y,m-1,d+1, 2,0); //Fecha final
+
+        if( fechaActual>fechainicio && this.model.get('status') === 'Planned' && this.model.get('parent_type') != 'Accounts' ){
+          lista = app.lang.getAppListStrings('meeting_status_dom');
+
+        }else {
+          //( this.model.get('status') === 'Planned' && this.model.get('parent_type') == 'Accounts')
           lista = app.lang.getAppListStrings('meeting_status_list');
         }
+        
 		    this.items = lista;
       }
       this._super('render');
