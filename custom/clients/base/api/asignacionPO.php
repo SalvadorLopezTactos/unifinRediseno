d00d927fc17e0c7e243bccbc9ec742a5d9ef19a9
salvadorLopez10 <jslb_cafcb10@hotmail.com>
2024-10-09 00:16:09 -0600

    Ajuste a vista para Gestión de Asignación PO

--- custom/clients/base/api/asignacionPO.php
+++ custom/clients/base/api/asignacionPO.php
@@ -25,6 +25,20 @@ class asignacionPO extends SugarApi
                 'method' => 'upAsignacionPOMethod',
                 'shortHelp' => 'Obtiene la lista de asignación de PO unifin_asignacion_po',
             ),
+            'getAsignacionSetAsignado' => array(
+                'reqType' => 'GET',
+                'path' => array('getAsignacionPoUsers'),
+                'pathVars' => array(''),
+                'method' => 'getRecordsAsignacionPOusers',
+                'shortHelp' => 'Obtiene la lista de asignación de PO unifin_asignacion_po que no tenga equipos',
+            ),
+            'updateAsignados' => array(
+                'reqType' => 'POST',
+                'path' => array('updateAsignadosPO'),
+                'pathVars' => array(''),
+                'method' => 'updateAsignadoId',
+                'shortHelp' => 'Actualiza asignado id en tabla de unifin_asignacion_po',
+            ),
         );
     }
 
@@ -36,7 +50,7 @@ class asignacionPO extends SugarApi
             $query = "select a.*, concat(u.first_name, ' ', u.last_name)uName
               from unifin_asignacion_po a
               inner join users u on u.id=a.modified_by
-              limit 100;";
+              WHERE municipio IS NULL or municipio = '';";
             $resultado = $db->query($query);
             $restultado_list = [];
 
@@ -77,6 +91,66 @@ class asignacionPO extends SugarApi
           }
         }else{
           return '400';
+        }
+
+    }
+
+    public function getRecordsAsignacionPOusers($api, $args){
+
+        global $db, $current_user;
+
+        try
+        {
+            $query = "SELECT a.*, m.name nMunicipio , concat(u.first_name, ' ', u.last_name)uName FROM unifin_asignacion_po a
+            INNER JOIN users u on u.id=a.modified_by
+            INNER join dire_municipio m on a.municipio = m.id 
+            WHERE (zona_geografica != '' AND zona_geografica IS NOT NULL) AND
+            (municipio != '' AND municipio IS NOT NULL) AND (equipos IS NULL OR equipos = '' )";
+            $resultado = $db->query($query);
+            $restultado_list = [];
+
+            while ($row = $db->fetchByAssoc($resultado)) {
+                $restultado_list[] = $row;
+            }
+
+            return $restultado_list;
+
+        }catch (Exception $e){
+            error_log(__FILE__." - ".__CLASS__."->".__FUNCTION__." <".$current_user->user_name."> : Error: ".$e->getMessage());
+            $GLOBALS['log']->fatal(__FILE__." - ".__CLASS__."->".__FUNCTION__." <".$current_user->user_name."> : Error ".$e->getMessage());
+        }
+    }
+
+    public function updateAsignadoId($api, $args){
+
+        $arrNewAsignados = $args['newAsignados'];
+
+        $GLOBALS['log']->fatal("QUERYS ACTUALIZACIÓN");
+        try{
+
+            for( $i=0; $i < count($arrNewAsignados); $i++ ){
+
+                $idRegistro = $arrNewAsignados[$i]['idRegistro'];
+                $idAsignado = $arrNewAsignados[$i]['asignado'];
+                
+                $queryUpdate = "UPDATE unifin_asignacion_po SET asignado_id = '{$idAsignado}' WHERE id = '{$idRegistro}';";
+                $GLOBALS['log']->fatal($queryUpdate);
+                
+                $GLOBALS['db']->query($queryUpdate);
+            }
+
+            return array(
+                "status"=> "éxito",
+                "msj"=> "Los registros se han actualizado correctamente"
+            );
+
+        }catch(Exception $ex) {
+            $GLOBALS['log']->fatal("Error al aplicar actualización de esignado " . $ex);
+            
+            return array(
+                "status"=> "error",
+                "msj"=> "Ocurrió un error al actualizar asignado: ".$ex,
+            );
         }
 
     }
--- custom/clients/base/views/asignacion_po/asignacion_po.js
+++ custom/clients/base/views/asignacion_po/asignacion_po.js
@@ -4,6 +4,9 @@
     events: {
         'click #btn_guardar': 'guardar',
         'change .oficinas_atiende': 'actualizaOficinasAtiende',
+        'click #btnGuardarAsignados': 'saveAsignados',
+        'change .usersActive': 'updateAsignado',
+        'click #btnRevertir': 'revertirCambios',
     },
 
     initialize: function(options){
@@ -13,8 +16,17 @@
           this.loadView = true;
           asignacionPO = this;
           asignacionPO.lista_equipo = app.lang.getAppListStrings('equipo_list');
+
+          asignacionPO.listAsignadosActualizados = [];
+
+          this.getUsuariosActivos();
+          app.alert.show('loadRecords', {
+            level: 'process',
+            title: 'Cargando...',
+          });
           app.api.call("read", app.api.buildURL('getAsignacionPO'), null, {
             success: _.bind(function (data) {
+              app.alert.dismiss('loadRecords');
               if(data.length > 0) {
 
                 asignacionPO.listaAsignacionPO = [];
@@ -49,12 +61,87 @@
                 asignacionPO.Total = 0;
               }
               asignacionPO.render();
+
+              asignacionPO.getRecordsSinEquipo();
             }, this)
           });
+
+
         }else{
             var route = app.router.buildRoute(this.module, null, '');
             app.router.navigate(route, {trigger: true});
         }
+    },
+
+    getUsuariosActivos: function(){
+      app.alert.show('loadActiveUsers', {
+        level: 'process',
+        title: 'Cargando...',
+      });
+      app.api.call('GET', app.api.buildURL('Users?filter[0][status][$equals]=Active&fields=id,full_name,first_name&order_by=first_name:ASC&max_num=-1'), null, {
+        success: function(data){
+          app.alert.dismiss('loadActiveUsers');
+          asignacionPO.listUsersActive = data.records;
+          
+        },
+        error: function (e) {
+          //console.log(e);
+        }
+      });
+    },
+
+    getRecordsSinEquipo: function(){
+      //selfAsignacion = this;
+      app.alert.show('loadRegistrosAsignacionPoUsers', {
+        level: 'process',
+        title: 'Cargando...',
+      });
+      app.api.call("read", app.api.buildURL('getAsignacionPoUsers'), null, {
+
+        success: _.bind(function (data) {
+          app.alert.dismiss('loadRegistrosAsignacionPoUsers');
+          if(data.length > 0) {
+
+            asignacionPO.listaAsignacionSinEquipo = [];
+            asignacionPO.listaAsignacionPOSinEquipoPrevio = [];
+
+            for(var i = 0; i < data.length; i++) {
+
+              var actual = {
+                "id": data[i].id,
+                "zona_geografica": data[i].zona_geografica,
+                "municipio": data[i].municipio,
+                "nMunicipio": data[i].nMunicipio,
+                "asignado": data[i].asignado_id,
+                "uName": data[i].uName,
+                "date_modified": data[i].date_modified
+              };
+
+              var previo = {
+                "id": data[i].id,
+                "zona_geografica": data[i].zona_geografica,
+                "municipio": data[i].municipio,
+                "nMunicipio": data[i].nMunicipio,
+                "asignado": data[i].asignado_id,
+                "uName": data[i].uName,
+                "date_modified": data[i].date_modified
+              };
+
+              asignacionPO.listaAsignacionSinEquipo.push(actual);
+              asignacionPO.listaAsignacionPOSinEquipoPrevio.push(previo);
+
+            }
+          }else {
+            asignacionPO.listaAsignacionSinEquipo = [];
+            asignacionPO.listaAsignacionPOSinEquipoPrevio = [];
+            
+          }
+
+          asignacionPO.render();
+
+        }, this)
+
+      });
     },
 
     actualizaOficinasAtiende: function(evt) {
@@ -71,7 +158,7 @@
       $('#btn_guardar').attr('style', 'pointer-events:auto;');
     },
     
-    guardar: function (){
+    guardar: function(){
         $('#btn_guardar').attr('style', 'pointer-events:none;');
         for(var i = 0; i < asignacionPO.listaAsignacionPO.length; i++) {
           if(asignacionPO.listaAsignacionPO[i].equipos != asignacionPO.listaAsignacionPO_previo[i].equipos) {
@@ -111,4 +198,65 @@
           }
         }
     },
+
+    updateAsignado: function(evt){
+      console.log("updateAsignado");
+      var idAsignado = $(evt.currentTarget).val();
+      var idRegistro = $(evt.currentTarget).closest('tr').attr('data-id');
+
+      var objetoActualizado = { "asignado": idAsignado, "idRegistro": idRegistro };
+
+      asignacionPO.listAsignadosActualizados.push(objetoActualizado);
+
+      $('#btnRevertir').show();
+    },
+
+    revertirCambios: function(){
+      asignacionPO.listaAsignacionSinEquipo = asignacionPO.listaAsignacionPOSinEquipoPrevio;
+
+      asignacionPO.listAsignadosActualizados = [];
+
+      asignacionPO.render();
+
+      $('#btnRevertir').hide();
+    },
+
+    saveAsignados: function(){
+      if( asignacionPO.listAsignadosActualizados.length > 0 ){
+
+        console.log("ENVIAR OBJETO DE TODOS LOS ACTUALIZADOS");
+
+        app.alert.show('saveAsignados', {
+          level: 'process',
+          title: 'Cargando...',
+        });
+        $('#btnGuardarAsignados').attr('style', 'pointer-events:none;');
+        $('#btnGuardarAsignados').addClass('disabled');
+        app.api.call('create', app.api.buildURL('updateAsignadosPO'),  { newAsignados : asignacionPO.listAsignadosActualizados } , {
+          success: _.bind(function (data) {
+            $('#btnGuardarAsignados').attr('style', 'pointer-events:block;');
+            $('#btnGuardarAsignados').removeClass('disabled');
+            
+            app.alert.dismiss('saveAsignados');
+            
+            app.alert.show('successAsignados', {
+              level: 'success',
+              title: "Éxito",
+              messages: data.msj,
+              autoClose: true
+            });
+
+            asignacionPO.getRecordsSinEquipo();
+
+          }, this),
+        });
+
+      }else{
+        app.alert.show('sin-registros', {
+          level: 'error',
+          messages: 'No hay registros por actualizar',
+          autoClose: true
+      });
+      }
+    }
 })
