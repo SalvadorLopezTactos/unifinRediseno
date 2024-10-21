3dc8f1edd48e9802442e02f7943bdc093af6cf58
salvadorLopez10 <jslb_cafcb10@hotmail.com>
2024-10-20 21:33:06 -0600

    Se agrega validationTask para comprobar llenado de dirección sin sepomex

---
+++ custom/Extension/application/Ext/Language/es_LA.sugar_valida_match_sepomex_relacion_list.php
@@ -0,0 +1,6 @@
+<?php
+ // created: 2024-10-20 15:57:20
+
+$app_list_strings['valida_match_sepomex_relacion_list']=array (
+  'Aval' => 'Aval',
+);
---
+++ custom/Extension/application/Ext/Language/es_LA.sugar_valida_match_sepomex_tipo_list.php
@@ -0,0 +1,6 @@
+<?php
+ // created: 2024-10-20 15:56:24
+
+$app_list_strings['valida_match_sepomex_tipo_list']=array (
+  3 => 'Cliente',
+);
